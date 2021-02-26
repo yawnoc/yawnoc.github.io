b756d0d488380a5c09e26c82dca866bae59ae351
Conway <yawnoc@pm.me>
2021-02-26 13:24:14 +0800

    Lexicon: cull lead

--- lexicon.cmd
+++ lexicon.cmd
@@ -7,7 +7,7 @@
 %%
   %title Conway's Lexicon for Literary Chinese
   %date-created 2020-08-05
-  %date-modified 2021-02-08
+  %date-modified 2021-02-26
   \resources:rendering
   %description
     Conway's Lexicon for translating Literary Chinese into English.
@@ -240,36 +240,8 @@ which I have rendered into English somewhere amidst my translations.
 Compiling a dictionary takes a lot of work.
 **This lexicon is not a dictionary:**
 the listings of meanings, parts of speech, and pronunciations
-are not complete, nor could they ever be;
-**neither is this lexicon a concordance:**
-the entries are not indexed to my translations.
+are not complete, nor could they ever be.
 ----
-
-----
-Sorting Chinese characters is a nightmare.
-[K'ang-hi's dictionary][康熙] uses radical-stroke,
-but amongst characters with the *same* radical \& number of strokes
-the ordering appears to be completely arbitrary.
-Unicode has a [sorting algorithm]
-which assigns each character a collation key by
-Radical \> Strokes \> First residual stroke \> Simplified \
-  \> Block \> Code point,
-but here I simply sort by Radical \> Strokes \> Code point.
-----
-
-@@[康熙]
-  https://en.wikipedia.org/wiki/Kangxi_Dictionary
-@@
-@@[sorting algorithm]
-  https://www.unicode.org/reports/tr38/#SortingAlgorithm
-@@
-
-----
-See also: [Unihan Radical-Stroke Index][unihan]
-----
-@@[unihan]
-  https://www.unicode.org/charts/unihanrsindex.html
-@@
 
 
 ////////////////////////////////////////////////////////////////
