babbd0ad8b9cea09ae868ceaf71324d4f4d3e55b
Conway <yawnoc@pm.me>
2021-04-14 00:39:50 +0800

    Use archaic 'was gone' in lion-eating
    Genesis 44:4: [And] when they were gone ...

--- lit/lion-eating.cmd
+++ lit/lion-eating.cmd
@@ -7,7 +7,7 @@
 %%
   %title "[An] History of Mr Shih's Eating Lions" 《施氏食獅史》
   %date-created 2019-04-07
-  %date-modified 2021-02-27
+  %date-modified 2021-04-14
   \resources:rendering
   %description
     An English translation of the Mandarin "lion tongue twister" \
@@ -97,8 +97,8 @@ The Chinese source text is from Page~143 of Chao's
 <<
   十時，適十獅適市。是時，適施氏適市。
 ||
-  .[At] .[the] tenth hour, ten lions .[had] just gone unto market.
-  .[At] this time, Mr~((Shih | ~~施~~)) .[had] just gone unto market.
+  .[At] .[the] tenth hour, ten lions .[were] just gone unto market.
+  .[At] this time, Mr~((Shih | ~~施~~)) .[was] just gone unto market.
 >>
 
 <<
