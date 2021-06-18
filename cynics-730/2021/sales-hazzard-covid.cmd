aa5b3b97eb0ffade5b6900156ba41dbc7e2216b9
Conway <yawnoc@pm.me>
2021-06-18 16:23:27 +0800

    Create 7.30 Sales & Hazzard COVID interview

---
+++ cynics-730/2021/sales-hazzard-covid.cmd
@@ -0,0 +1,217 @@
+{+ resources/syntax/general.cmd +}
+
+{+ resources/syntax/cynics-730.cmd +}
+
+%%
+  %title A cynic's 7.30: \short-title
+  %date-created 2021-06-18
+  %date-modified 2021-06-18
+  \resources
+  %footer-copyright-remark
+    except the [original interview][original],
+    whose copyright belongs to the ABC
+  %footer-remark
+    [:{Curly-bracketed words}:] inserted by me.
+%%
+
+{: \short-title : Brad Hazzard on the NSW COVID outbreak (17 Jun 2021) :}
+
+[[====
+* \header-link:home
+* \header-link:top
+* \header-link:cynicism
+====]]
+
+
+# %title #
+
+
+[||||
+[Original 7.30 interview][original] ([archived version])
+||||]
+
+@@[original]
+  https://www.abc.net.au/7.30/\
+    nsw-covid-outbreak-started-by-an-unvaccinated/\
+    13396682
+@@
+
+@@[archived version]
+  https://web.archive.org/web/20210618053329/\
+    https://www.abc.net.au/7.30/\
+      nsw-covid-outbreak-started-by-an-unvaccinated/\
+      13396682
+@@
+
+##
+  NSW COVID outbreak started by an unvaccinated driver
+##
+
+
+{: \leigh-sales : ![LEIGH SALES:] :}
+@@[leigh sales:]{.h}
+  leigh-sales-2021-06-17.jpg
+@@
+
+{: \brad-hazzard : ![BRAD HAZZARD:] :}
+@@[brad hazzard:]{.g}
+  brad-hazzard-2021-06-17.jpg
+@@
+
+||||{.interview}
+  
+  \leigh-sales
+  ----{.h}
+  Minister, why was an unvaccinated driver allowed
+  anywhere near an international flight crew?
+  ----
+  
+  \brad-hazzard
+  ----{.gt}
+  {But first, let me preface with a non-answer about saliva:}
+  ----
+  ----{.g}
+  The current arrangements [...] are that there should be,
+  every day, a saliva test undertaken [etc. etc.].
+  ----
+  ----{.g}
+  It would appear that uh, he *may* have *had* the saliva test
+  [blah blah etc. etc.].
+  ----
+  ----{.g}
+  [Having vaccines] hasn't been mandatory to date, but at any rate,
+  we've announced today that we'll be reviewing that situation
+  because of the current circumstances.
+  ----
+  
+  \leigh-sales
+  ----{.h}
+  How come every time [...] the minister responsible,
+  regardless of the state, says,
+  "Oh yep, okay, we'll have to review it to make sure it doesn't happen again."
+  Cause [vaccinated drivers and daily testing] seem really obvious [...].
+  It's not rocket science.
+  ----
+  
+  \brad-hazzard
+  ----{.g}
+  Yeah uhh... thank you for that, Leigh,
+  but the reality is that we are in a 1-in-100-year pandemic...
+  and it's the "unknown unknowns".
+  [...]
+  ----
+  ----{.g}
+  There has been a reluctance through National Cabinet
+  to have mandatory testing
+  ----
+  ----{.gt}
+  {so blame them}.
+  ----
+  ----{.g}
+  [...]
+  ----
+  
+  \leigh-sales
+  ----{.h}
+  Do you reckon that the audience would buy that an unvaccinated driver
+  picking up an infection from an international flight crew
+  is an "unknown unknown"?
+  ----
+  
+  \brad-hazzard
+  ----{.g}
+  Well, I just said it was...
+  ----
+  
+  \leigh-sales
+  ----{.ht}
+  {Yeah nah.}
+  ----
+  ----{.h}
+  But how can that be?
+  ----
+  
+  \brad-hazzard
+  ----{.g}
+  Well... it *was*.
+  ----
+  ----{.gt}
+  {And it helps run down the clock in this interview.}
+  ----
+  ----{.g}
+  [...] There are other factors.
+  [...] the companies that actually engage these drivers,
+  often as employees or subcontractors
+  ----
+  ----{.gt}
+  {who are cheaper},
+  ----
+  ----{.g}
+  have expressed concerns [...]
+  that they may not be able to get drivers to actually do the job
+  if we actually force them to have the vaccine
+  ----
+  ----{.gt}
+  {unless we pay them more, which we won't}.
+  ----
+  
+  \leigh-sales
+  ----{.h}
+  New South Wales has a higher appetite for risk than other states [...].
+  ----
+  ----{.h}
+  What assurance can you give [...] that it isn't just luck
+  that this is the first incident of its kind?
+  ----
+  
+  \brad-hazzard
+  ----{.g}
+  [...] We take the majority of people coming back from overseas
+  ----
+  ----{.gt}
+  {but don't you dare talk to me about NSW's larger population}.
+  ----
+  ----{.g}
+  [...] we've taken over 190,000 people.
+  That's about 10~times what WA's taken
+  ----
+  ----{.gt}
+  {which is just begging for a fact-check, because
+  [Dr~Tarun Weeramanthri's Interim Advice #1 from 4~Feb 2021][wa #1]
+  ([archived version][wa #1 archived])
+  says that """Over 37,000 people have been through the HQ system in WA"""}.
+  ----
+  @@[wa #1]
+    https://www.wa.gov.au/sites/default/files/2021-04/\
+      Review-of-Hotel-Quarantine-Arrangements-in-Western-Australia-\
+      Interim-Advice-1.pdf
+  @@
+  @@[wa #1 archived]
+    https://web.archive.org/web/20210618080052/\
+      https://www.wa.gov.au/sites/default/files/2021-04/\
+        Review-of-Hotel-Quarantine-Arrangements-in-Western-Australia-\
+        Interim-Advice-1.pdf
+  @@
+  
+  ----{.h}
+  [...]
+  ----
+  ----{.g}
+  [...]
+  ----
+  
+  \leigh-sales
+  ----{.h}
+  Brad Hazzard, thanks for your time.
+  ----
+  
+  \brad-hazzard
+  ----{.g}
+  Pleasure.
+  ----
+  
+  \END
+  
+||||
+
+%footer-element
