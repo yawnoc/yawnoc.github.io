c846fabf8f0f98f1bfb120181cf084f84dd856f1
Conway <yawnoc@pm.me>
2022-05-29 18:00:55 +0800

    Use titles to explain abbreviated alternative markers

--- lexicon.cmd
+++ lexicon.cmd
@@ -177,7 +177,7 @@ Lookup by stroke count of radical:
   .音-homophones are based on modern Cantonese.
 
 1.
-  Non-literal renderings are marked by \met (for "metaphorical").
+  Non-literal renderings are marked by \met ("metaphorically rendered").
 ++++
 
 [Government-regulated .統讀-readings]:
