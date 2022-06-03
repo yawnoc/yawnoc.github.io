fc2b9b92a6d33588b3aefeee019bdea49c3c5e86
Conway <yawnoc@pm.me>
2022-06-03 21:55:27 +0800

    Convert /code/termux to CMDv4

--- code/termux.cmd
+++ code/termux.cmd
@@ -1,40 +1,45 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
 
-%%
-  %title Termux configuration
-  %date-created 2020-10-04
-  %date-modified 2020-10-04
-  %footer-remark
-    The above `termux.properties` is released under the
-    [MIT No Attribution (MIT-0) license][MIT-0].
-  \resources
-%%
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
+* %post-copyright-remark -->
+  "
+    --
+    The above `termux.properties` is released under [MIT No Attribution (MIT-0)].
+    --
+    [MIT No Attribution (MIT-0)]: https://spdx.org/licenses/MIT-0
+  "
 
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %title --> Termux configuration
+* %date-created --> 2020-10-04
+* %date-modified --> 2022-xx-xx
 
-[[====
-* \header-link:home
-* \header-link:top
-====]]
-
-
-# %title #
-
-[||||
-||||]
+%%%
 
 
-----
+^^^^
+- !home
+- !top
+^^^^
+
+
+# %title
+
+--
 Create the following `~/.termux/termux.properties` in Termux
 to disable phone vibration and add more keys to the touch keyboard:
-----
+--
 
-````
+``
 bell-character = ignore
 extra-keys = [[ \
   'TAB', 'CTRL', 'ALT', '-', '/', \
   'LEFT', 'DOWN', 'UP', 'RIGHT', \
   'DEL', 'HOME', 'END' \
 ]]
-````
+``
 
-%footer-element
+%%footer
