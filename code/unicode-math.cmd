24463dc93a11b92a03b91e14d128dff7f3edd923
Conway <yawnoc@pm.me>
2022-06-03 22:14:16 +0800

    Convert /code/unicode-math to CMDv4

--- code/unicode-math.cmd
+++ code/unicode-math.cmd
@@ -1,41 +1,44 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
 
-%%
-  %title Unicode mathematical symbols
-  %date-created 2019-03-30
-  %date-modified 2020-08-27
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-[[====
-* \header-link:home
-* \header-link:top
-====]]
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %title --> Unicode mathematical symbols
+* %date-created --> 2019-03-30
+* %date-modified --> 2022-xx-xx
 
+%%%
 
-# %title #
-
-\noscript:maths
-
-[||||
-||||]
+^^^^
+- !home
+- !top
+^^^^
 
 
-[====
-* [Constants](#constants)
-* [Subscripts](#subscripts)
-* [Superscripts](#superscripts)
-* [Arithmetic](#arithmetic)
-* [Relations](#relations)
-* [Calculus](#calculus)
-* [Greek](#greek)
-====]
+# %title
 
-----
+%noscript-equation
+
+::::
+- [Constants](#constants)
+- [Subscripts](#subscripts)
+- [Superscripts](#superscripts)
+- [Arithmetic](#arithmetic)
+- [Relations](#relations)
+- [Calculus](#calculus)
+- [Greek](#greek)
+::::
+
+--
 Convenient for use in emails.
-----
+--
 
-````
+``
 ° % ∞ ½ ⅓ ⅔ ¼ ¾ ⅕ ⅖ ⅗ ⅘ ⅙ ⅚ ⅛ ⅜ ⅝ ⅞
 ₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ ₍ ₎ ₊ ₋ ₌
 ⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ⁽ ⁾ ⁺ ⁻ ⁼
@@ -43,409 +46,414 @@ Convenient for use in emails.
 ∂ ∇ ∫
 Α α Β β Γ γ Δ δ Ε ϵ ε Ζ ζ Η η Θ θ Ι ι Κ κ Λ λ Μ μ
 Ν ν Ξ ξ Ο ο Π π Ρ ρ Σ σ ς Τ τ Υ υ Φ ϕ φ Χ χ Ψ ψ Ω ω
-````
+``
 
 
-<## Header row for tables ##>
-{::
-  \header-row
-::
-  |^
-    ==
-      ; Unicode
-      ; KaTeX
-      ; KaTeX code
-      ; Description
-  |:
-::}
+##{#constants} Constants
 
-
-
-##{#constants}
-  Constants
-##
-
-
-||||{.overflowing}
-''''
-  \header-row
-  ==
+||{.wide}
+''
+|^
+  //
+    ; Unicode
+    ; KaTeX
+    ; KaTeX code
+    ; Description
+|:
+  //
     , °
     , $  \degree  $
     , `  \degree  `
     , degree
-  ==
+  //
     , %
     , $  \%  $
     , `  \%  `
     , percent
-  ==
+  //
     , ∞
     , $  \infty  $
     , `  \infty  `
     , infinity
-  ==
+  //
     , ½
     , $  \frac{1}{2}  $
     , `  \frac{1}{2}  `
     , half
-  ==
+  //
     , ⅓ ⅔
     , $  \frac{1}{3} \frac{2}{3}  $
     , `  \frac{1}{3} \frac{2}{3}  `
     , thirds
-  ==
+  //
     , ¼ ¾
     , $  \frac{1}{4} \frac{3}{4}  $
     , `  \frac{1}{4} \frac{3}{4}  `
     , quarters
-  ==
+  //
     , ⅕ ⅖ ⅗ ⅘
     , $  \frac{1}{5} \frac{2}{5} \frac{3}{5} \frac{4}{5}  $
     , `  \frac{1}{5} \frac{2}{5} \frac{3}{5} \frac{4}{5}  `
     , fifths
-  ==
+  //
     , ⅙ ⅚
     , $  \frac{1}{6} \frac{5}{6}  $
     , `  \frac{1}{6} \frac{5}{6}  `
     , sixths
-  ==
+  //
     , ⅛ ⅜ ⅝ ⅞
     , $  \frac{1}{8} \frac{3}{8} \frac{5}{8} \frac{7}{8}  $
     , `  \frac{1}{8} \frac{3}{8} \frac{5}{8} \frac{7}{8}  `
     , eighths
-''''
-||||
+''
+||
 
 
-##{#subscripts}
-  Subscripts
-##
+##{#subscripts} Subscripts
 
-
-||||{.overflowing}
-''''
-  \header-row
-  ==
+||{.wide}
+''
+|^
+  //
+    ; Unicode
+    ; KaTeX
+    ; KaTeX code
+    ; Description
+|:
+  //
     , ₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉
     , $  {_0} {_1} {_2} {_3} {_4} {_5} {_6} {_7} {_8} {_9}  $
     , `  {_0} {_1} {_2} {_3} {_4} {_5} {_6} {_7} {_8} {_9}  `
     , digits
-  ==
+  //
     , ₍ ₎ ₊ ₋ ₌
     , $  {_(} {_)} {_+} {_-} {_=}  $
     , `  {_(} {_)} {_+} {_-} {_=}  `
     , operators etc.
-''''
-||||
+''
+||
 
 
-##{#superscripts}
-  Superscripts
-##
+##{#superscripts} Superscripts
 
-
-||||{.overflowing}
-''''
-  \header-row
-  ==
+||{.wide}
+''
+|^
+  //
+    ; Unicode
+    ; KaTeX
+    ; KaTeX code
+    ; Description
+|:
+  //
     , ⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹
     , $  {^0} {^1} {^2} {^3} {^4} {^5} {^6} {^7} {^8} {^9}  $
     , `  {^0} {^1} {^2} {^3} {^4} {^5} {^6} {^7} {^8} {^9}  `
     , digits
-  ==
+  //
     , ⁽ ⁾ ⁺ ⁻ ⁼
     , $  {^(} {^)} {^+} {^-} {^=}  $
     , `  {^(} {^)} {^+} {^-} {^=}  `
     , operators etc.
-''''
-||||
+''
+||
 
 
-##{#arithmetic}
-  Arithmetic operators
-##
+##{#arithmetic} Arithmetic operators
 
-
-||||{.overflowing}
-''''
-  \header-row
-  ==
+||{.wide}
+''
+|^
+  //
+    ; Unicode
+    ; KaTeX
+    ; KaTeX code
+    ; Description
+|:
+  //
     , +
     , $  +  $
     , `  +  `
     , plus
-  ==
+  //
     , −
     , $  -  $
     , `  -  `
     , minus
-  ==
+  //
     , ±
     , $  \pm  $
     , `  \pm  `
     , plus or minus
-  ==
+  //
     , ∓
     , $  \mp  $
     , `  \mp  `
     , minus or plus
-  ==
+  //
     , ×
     , $  \times  $
     , `  \times  `
     , times
-  ==
+  //
     , ÷
     , $  \div  $
     , `  \div  `
     , divide
-  ==
+  //
     , ·
     , $  \cdot  $
     , `  \cdot  `
     , dot
-''''
-||||
+''
+||
 
 
-##{#relations}
-  Relations
-##
+##{#relations} Relations
 
-
-||||{.overflowing}
-''''
-  \header-row
-  ==
+||{.wide}
+''
+|^
+  //
+    ; Unicode
+    ; KaTeX
+    ; KaTeX code
+    ; Description
+|:
+  //
     , =
     , $  =  $
     , `  =  `
     , equals
-  ==
+  //
     , ≐
     , $  \doteq  $
     , `  \doteq  `
     , approximately equals
-  ==
+  //
     , ≈
     , $  \approx  $
     , `  \approx  `
     , approximately
-  ==
+  //
     , ≃
     , $  \simeq  $
     , `  \simeq  `
     , asymptotically equals
-  ==
-    , \~
+  //
+    , <`` ~ ``>
     , $  \sim  $
     , `  \sim  `
     , asymptotically
-  ==
+  //
     , ≠
     , $  \ne  $
     , `  \ne  `
     , does not equal
-  ==
+  //
     , \<
     , $  <  $
     , `  &lt;  `
     , less than
-  ==
+  //
     , \>
     , $  >  $
     , `  &gt;  `
     , greater than
-  ==
+  //
     , ≤
     , $  \le  $
     , `  \le  `
     , less than or equal to
-  ==
+  //
     , ≥
     , $  \ge  $
     , `  \ge  `
     , greater than or equal to
-  ==
+  //
     , ≪
     , $  \ll  $
     , `  \ll  `
     , much less than
-  ==
+  //
     , ≫
     , $  \gg  $
     , `  \gg  `
     , much greater than
-''''
-||||
+''
+||
 
 
-##{#calculus}
-  Calculus
-##
+##{#calculus} Calculus
 
-
-||||{.overflowing}
-''''
-  \header-row
-  ==
+||{.wide}
+''
+|^
+  //
+    ; Unicode
+    ; KaTeX
+    ; KaTeX code
+    ; Description
+|:
+  //
     , ∂
     , $  \pd  $
     , `  \pd  `
     , partial differential
-  ==
+  //
     , ∇
     , $  \del  $
     , `  \del  `
     , del
-  ==
+  //
     , ∫
     , $  \int  $
     , `  \int  `
     , integral
-''''
-||||
+''
+||
 
-----
+--
 For the definitions of `\pd` and `\del`,
 see `macros` in [`rendering.js`].
-----
-@@[`rendering.js`]
-  https://github.com/yawnoc/yawnoc.github.io/blob/master/\
-    resources/scripts/rendering.js
-@@
+--
+
+[`rendering.js`]: https://github.com/yawnoc/yawnoc.github.io/blob/master/resources/scripts/rendering.js
 
 
-##{#greek}
-  Greek alphabet
-##
+##{#greek} Greek alphabet
 
-
-||||{.overflowing}
-''''
-  \header-row
-  ==
+||{.wide}
+''
+|^
+  //
+    ; Unicode
+    ; KaTeX
+    ; KaTeX code
+    ; Description
+|:
+  //
     , Α α
     , $  \Alpha \alpha  $
     , `  \Alpha \alpha  `
     , alpha
-  ==
+  //
     , Β β
     , $  \Beta \beta  $
     , `  \Beta \beta  `
     , beta
-  ==
+  //
     , Γ γ
     , $  \Gamma \gamma  $
     , `  \Gamma \gamma  `
     , gamma
-  ==
+  //
     , Δ δ
     , $  \Delta \delta  $
     , `  \Delta \delta  `
     , delta
-  ==
+  //
     , Ε ϵ ε
     , $  \Epsilon \epsilon \varepsilon  $
     , `  \Epsilon \epsilon \varepsilon  `
     , epsilon
-  ==
+  //
     , Ζ ζ
     , $  \Zeta \zeta  $
     , `  \Zeta \zeta  `
     , zeta
-  ==
+  //
     , Η η
     , $  \Eta \eta  $
     , `  \Eta \eta  `
     , eta
-  ==
+  //
     , Θ θ
     , $  \Theta \theta  $
     , `  \Theta \theta  `
     , theta
-  ==
+  //
     , Ι ι
     , $  \Iota \iota  $
     , `  \Iota \iota  `
     , iota
-  ==
+  //
     , Κ κ
     , $  \Kappa \kappa  $
     , `  \Kappa \kappa  `
     , kappa
-  ==
+  //
     , Λ λ
     , $  \Lambda \lambda  $
     , `  \Lambda \lambda  `
     , lambda
-  ==
+  //
     , Μ μ
     , $  \Mu \mu  $
     , `  \Mu \mu  `
     , mu
-  ==
+  //
     , Ν ν
     , $  \Nu \nu  $
     , `  \Nu \nu  `
     , nu
-  ==
+  //
     , Ξ ξ
     , $  \Xi \xi  $
     , `  \Xi \xi  `
     , xi
-  ==
+  //
     , Ο ο
     , $  \Omicron \omicron  $
     , `  \Omicron \omicron  `
     , omicron
-  ==
+  //
     , Π π
     , $  \Pi \pi  $
     , `  \Pi \pi  `
     , pi
-  ==
+  //
     , Ρ ρ
     , $  \Rho \rho  $
     , `  \Rho \rho  `
     , rho
-  ==
+  //
     , Σ σ ς
     , $  \Sigma \sigma \varsigma  $
     , `  \Sigma \sigma \varsigma  `
     , sigma
-  ==
+  //
     , Τ τ
     , $  \Tau \tau  $
     , `  \Tau \tau  `
     , tau
-  ==
+  //
     , Υ υ
     , $  \Upsilon \upsilon  $
     , `  \Upsilon \upsilon  `
     , upsilon
-  ==
+  //
     , Φ ϕ φ
     , $  \Phi \phi \varphi  $
     , `  \Phi \phi \varphi  `
     , phi
-  ==
+  //
     , Χ χ
     , $  \Xi \xi  $
     , `  \Xi \xi  `
     , xi
-  ==
+  //
     , Ψ ψ
     , $  \Psi \psi  $
     , `  \Psi \psi  `
     , psi
-  ==
+  //
     , Ω ω
     , $  \Omega \omega  $
     , `  \Omega \omega  `
     , omega
-''''
-||||
+''
+||
 
-%footer-element
+
+%%footer
