ce0a26be8100cbc2d74ebf9d4b385a007ef0176c
Conway <yawnoc@pm.me>
2022-06-04 14:54:26 +0800

    Convert sales-albanese-donation to CMDv4

--- cynics-730/2019/sales-albanese-donation.cmd
+++ cynics-730/2019/sales-albanese-donation.cmd
@@ -1,282 +1,260 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/cynicism.cmdr
 
-{+ /resources/syntax/cynics-730.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title A cynic's 7.30: \short-title
-  %date-created 2019-09-06
-  %date-modified 2020-02-16
-  \resources
-  %footer-copyright-remark
-    except the [original interview][original],
-    whose copyright belongs to the ABC
-  %footer-remark
-    [:{Curly-bracketed words}:] inserted by me.
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %title --> A cynic's 7.30: Albanese on donations (3~Sep 2019)
+* %date-created --> 2019-09-06
+* %date-modified --> 2022-xx-xx
+- concluding_replacements: #.yawnoc.typography
 
-{: \short-title : Albanese on donations (3 Sep 2019) :}
-
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:cynicism
-====]]
+%%%
 
 
-# %title #
+^^^^
+- !home
+- !top
+^^^^
 
 
-[||||
-[Original 7.30 interview][original] ([archived version])
-||||]
+# %title
 
-@@[original]
-  https://www.abc.net.au/7.30/\
-    anthony-albanese-discusses-the-nsw-labor-donation/\
-    11477046
-@@
+--
+([Original 7.30 interview], [archived version])
+--
 
-@@[archived version]
-  https://web.archive.org/web/20190905152759/\
-    https://www.abc.net.au/7.30/\
-      anthony-albanese-discusses-the-nsw-labor-donation/\
-      11477046
-@@
+[Original 7.30 interview]:
+  https://www.abc.net.au/7.30/anthony-albanese-discusses-the-nsw-labor-donation/11477046
+[archived version]:
+  https://web.archive.org/web/20190905152759/https://www.abc.net.au/7.30/anthony-albanese-discusses-the-nsw-labor-donation/11477046
 
 
-||||{.important .statement}
-"Money is good."
-||||
+## Anthony Albanese discusses the NSW Labor donation scandal.
 
 
-## Anthony Albanese discusses the NSW Labor donation scandal. ##
+[Leigh Sales:]{.interview-host}: leigh-sales-2019-09-03.jpg
+[Anthony Albanese:]{.interview-guest}: anthony-albanese-2019-09-03.jpg
 
-
-{: \leigh-sales : ![LEIGH SALES:] :}
-@@[leigh sales:]{.h}
-  leigh-sales-2019-09-03.jpg
-@@
-
-{: \anthony-albanese : ![ANTHONY ALBANESE:] :}
-@@[anthony albanese:]{.g}
-  anthony-albanese-2019-09-03.jpg
-@@
 
 ||||{.interview}
-  
-  \leigh-sales
-  ----{.h}
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Anthony Albanese, thank you for coming in.
-  ----
-  
-  \anthony-albanese
-  ----{.g}
+  --
+
+  ![Anthony Albanese:]
+  --{.interview-guest}
   Good to be with you, Leigh.
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   On another matter, the corruption hearings into donations
   to the New South Wales Labor Party are continuing.
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   The reaction of federal MPs, including yourself,
   has been shock and disbelief,
   as if the party's headquarters and its elected MPs are church and state.
   That's a bit hard for the average voter to believe, isn't it,
   that Labor MPs have no idea about the culture or operation
   of your party headquarters and the way it gets money?
-  ----
-  
-  \anthony-albanese
-  ----{.g}
+  --
+
+  ![Anthony Albanese:]
+  --{.interview-guest}
   Well, quite clearly, people didn't know about it, quite frankly, Leigh.
-  ----
-  ----{.gt}
-  {Except Kaila Murnain of course, but she's dead to us now,
-  so she doesn't count as a person.}
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _Except Kaila Murnain of course, but she's dead to us now,
+  so she doesn't count as a person._
+  --
+  --{.interview-guest}
   If you knew about it you would have been reporting on it,
   on this very program.
-  ----
-  
-  \leigh-sales
-  ----{.ht}
-  {All right Mr~Smarty-pants it's just the first question.}
-  ----
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host .thought}
+  _All right Mr~Smarty-pants it's just the first question._
+  --
+  --{.interview-host}
   But I'm not a key elected official of the Labor Party.
-  ----
-  
-  \anthony-albanese
-  ----{.g}
+  --
+
+  ![Anthony Albanese:]
+  --{.interview-guest}
   I didn't even know this function had occurred.
   I'd never met the particular official and didn't know he existed,
   who was involved in this, and of course, Kaila Murnain has paid a price.
   She's been suspended from her position.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   What we need to do is to make sure that
   the [ICAC] can undertake its work.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   There's clearly a need for structural reform
   in the New South Wales branch,
   to make sure this can never happen again.
-  ----
-  @@[ICAC]
-    https://en.wikipedia.org/wiki/\
-      Independent_Commission_Against_Corruption_(New_South_Wales)
-  @@
-  
-  \leigh-sales
-  ----{.h}
+  --
+  [ICAC]:
+    https://en.wikipedia.org/wiki/Independent_Commission_Against_Corruption_(New_South_Wales)
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Shouldn't there be an audit,
   not just of the New South Wales branch, but of all branches,
   because if it's going on here, how do we know it's not going on elsewhere?
-  ----
-  
-  \anthony-albanese
-  ----{.gt}
-  {Yes of course there should, but I'm not allowed to say that.
-  So cue swipe at the Coalition:}
-  ----
-  ----{.g}
+  --
+
+  ![Anthony Albanese:]
+  --{.interview-guest .thought}
+  _Yes of course there should, but I'm not allowed to say that.
+  So cue swipe at the Coalition:_
+  --
+  --{.interview-guest}
   Well, the Labor Party federally, of course, we,
   unlike the Government: we declare all donations above $1,000
   which is what we think the threshold should be.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   That's been resisted by the current Coalition Government, and of course
   in New South Wales, 10, no less than 10 Liberal Party MPs
   either had to resign from Parliament or go to the crossbench [...].
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   But we're talking about Labor.
   Why not be transparent and have an audit? Bring out independent auditors.
-  ----
-  
-  \anthony-albanese
-  ----{.g}
+  --
+
+  ![Anthony Albanese:]
+  --{.interview-guest}
   We have processes
-  ----
-  ----{.gt}
-  {which we would like to keep in house for obvious reasons}.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _which we would like to keep in house for obvious reasons_.
+  --
+  --{.interview-guest}
   We have an audit process.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Well, it clearly hasn't worked.
-  ----
-  
-  \anthony-albanese
-  ----{.g}
+  --
+
+  ![Anthony Albanese:]
+  --{.interview-guest}
   We have an audit process of our federal funding as a national party.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   What we need to do, is, in New South Wales
-  ----
-  ----{.gt}
-  {and definitely not in Federal Labor},
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _and definitely not in Federal Labor_,
+  --
+  --{.interview-guest}
   there clearly needs to be a proper examination of how this occurred,
   and a restructuring to make sure that it can't occur again.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Why just New South Wales though?
-  ----
-  
-  \anthony-albanese
-  ----{.g}
+  --
+
+  ![Anthony Albanese:]
+  --{.interview-guest}
   But we need to ... because it is in New South Wales that this has occurred
-  ----
-  ----{.gt}
-  {and assuredly, I say to you, not in Federal Labor}.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _and assuredly, I say to you, not in Federal Labor_.
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   But how do you know?
-  ----
-  
-  \anthony-albanese
-  ----{.g}
+  --
+
+  ![Anthony Albanese:]
+  --{.interview-guest}
   In terms of, federally, ...
   Because federally we have an absolutely strong audit process
-  ----
-  ----{.gt}
-  {although if you asked me to explain exactly what makes it stronger than
-  the New South Wales branch's audit process, I wouldn't be able to}.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _although if you asked me to explain exactly what makes it stronger than
+  the New South Wales branch's audit process, I wouldn't be able to_.
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Is the party culture to do whatever it takes
   to get as much money as possible through the doors?
-  ----
-  
-  \anthony-albanese
-  ----{.gt}
-  {Oh yes, and it's one of the few things for which
-  there's bipartisan consensus too.}
-  ----
-  ----{.g}
+  --
+
+  ![Anthony Albanese:]
+  --{.interview-guest .thought}
+  _Oh yes, and it's one of the few things for which
+  there's bipartisan consensus too._
+  --
+  --{.interview-guest}
   Well, look, it's up to the New South Wales branch officials to comment
   and they've been doing that, of course,
   before the ICAC and that will continue to take place over coming weeks
-  ----
-  ----{.gt}
-  {and until it's over I shall be using the nifty
-  "I can't comment on matters relating to an ongoing investigation"}.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _and until it's over I shall be using the nifty
+  "I can't comment on matters relating to an ongoing investigation"_.
+  --
+  --{.interview-guest}
   We need to not pre-empt that, Leigh.
   It's important; one of the issues is re political interference
   in proper processes.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   There is a process, the ICAC, and what we shouldn't do is
   try and politically interfere with that while that process is ongoing.
-  ----
-  ----{.gt}
-  {However, if we are talking about political interference
+  --
+  --{.interview-guest .thought}
+  _However, if we are talking about political interference
   in the form of donations from those linked to the Chinese Communist Party,
-  I think we agree with the Liberals: How good's money?}
-  ----
-  ----{.h}
+  I think we agree with the Liberals: How good's money?_
+  --
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Anthony Albanese, thank you.
-  ----
-  
-  \anthony-albanese
-  ----{.g}
+  --
+
+  ![Anthony Albanese:]
+  --{.interview-guest}
   Thanks.
-  ----
-  
-  \END
-  
+  --
+
+  ||{.marker .end}
+    END
+  ||
+
 ||||
 
-%footer-element
+%%footer
