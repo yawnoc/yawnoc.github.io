e63a6f2e6248f50492271c8227b5f839b8248aa8
Conway <yawnoc@pm.me>
2022-06-04 16:02:20 +0800

    Convert sales-taylor-snowy to CMDv4

--- cynics-730/2019/sales-taylor-snowy.cmd
+++ cynics-730/2019/sales-taylor-snowy.cmd
@@ -1,354 +1,341 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/cynicism.cmdr
 
-{+ /resources/syntax/cynics-730.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title A cynic's 7.30: \short-title
-  %date-created 2019-10-15
-  %date-modified 2020-01-19
-  \resources
-  %footer-copyright-remark
-    except the [original interview][original],
-    whose copyright belongs to the ABC
-  %footer-remark
-    [:{Curly-bracketed words}:] inserted by me.
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %title --> A cynic's 7.30: Angus Taylor on Snowy Hydro~2.0 (14~Oct 2019)
+* %date-created --> 2019-10-15
+* %date-modified --> 2022-xx-xx
+- concluding_replacements: #.yawnoc.typography
 
-{: \short-title : Angus Taylor on Snowy Hydro~2.0 (14 Oct 2019) :}
-
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:cynicism
-====]]
+%%%
 
 
-# %title #
+^^^^
+- !home
+- !top
+- !cynicism
+^^^^
 
 
-[||||
-[Original 7.30 interview][original] ([archived version])
-||||]
+# %title
 
-@@[original]
-  https://www.abc.net.au/7.30/\
-    energy-minister-angus-taylor-discusses-snowy-hydro/\
-    11601830
-@@
+--
+([Original 7.30 interview], [archived version])
+--
 
-@@[archived version]
-  https://web.archive.org/web/20191015125355/\
-    https://www.abc.net.au/7.30/\
-      energy-minister-angus-taylor-discusses-snowy-hydro/\
-      11601830
-@@
+[Original 7.30 interview]:
+  https://www.abc.net.au/7.30/energy-minister-angus-taylor-discusses-snowy-hydro/11601830
+[archived version]:
+  https://web.archive.org/web/20191015125355/https://www.abc.net.au/7.30/energy-minister-angus-taylor-discusses-snowy-hydro/11601830
 
 
-## Energy Minister Angus Taylor discusses Snowy Hydro 2.0 ##
+## Energy Minister Angus Taylor discusses Snowy Hydro 2.0
 
 
-{: \leigh-sales : ![LEIGH SALES:] :}
-@@[leigh sales:]{.h}
-  leigh-sales-2019-10-14.jpg
-@@
+[Leigh Sales:]{.interview-host}: leigh-sales-2019-10-14.jpg
+[Angus Taylor:]{.interview-guest}: angus-taylor-2019-10-14.jpg
 
-{: \angus-taylor : ![ANGUS TAYLOR:] :}
-@@[angus taylor:]{.g}
-  angus-taylor-2019-10-14.jpg
-@@
 
 ||||{.interview}
-  
-  \leigh-sales
-  ----{.h}
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Minister, thanks for your time.
-  ----
-  
-  \angus-taylor
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest}
   Thanks for having me, Leigh.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Why wasn't a cost benefit analysis done of Snowy 2.0
   before it was commissioned?
-  ----
-  
-  \angus-taylor
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest}
   Well... it has been done
-  ----
-  ----{.gt}
-  {which, I know, doesn't answer your question,
-  but I've got a bunch of talking points to get through first}.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _which, I know, doesn't answer your question,
+  but I've got a bunch of talking points to get through first_.
+  --
+  --{.interview-guest}
   I mean, we know that the benefits of Snowy 2.0
-  will be from [\d]4.4~to 6.8~billion
+  will be from [$]4.4~to 6.8~billion
   and [...] Snowy will reduce volatility of wholesale electricity prices.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   It will put downward pressure on prices [...].
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   And what makes this possible [...]
   is the record levels of investment [...] in solar and wind,
   and Snowy works as a big battery.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   But Minister, this analysis wasn't done
   before the Snowy scheme was announced.
   How is it responsible use of [...] money [...]
   to announce a massive infrastructure spend
   without doing a feasibility study first?
-  ----
-  
-  \angus-taylor
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest}
   Leigh, we made our investment decision
-  ----
-  ----{.gt}
-  {to stick with Malcolm Turnbull's announcement,
-  which he made in March 2017 before the feasibility study was conducted,}
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _to stick with Malcolm Turnbull's announcement,
+  which he made in March 2017 before the feasibility study was conducted,_
+  --
+  --{.interview-guest}
   after we'd done a cost benefit, and after we'd done the feasibility work.
   Now the feasibility work was done in December 2017 [...].
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...] By storing energy, [Snowy] can bring down the prices
   and [...] reduce the volatility of those prices.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   On that point, if Snowy 2.0 comes online in 2025,
   how much in percentage terms
   will the average household power bill go down by?
-  ----
-  
-  \angus-taylor
-  ----{.gt}
-  {I have no idea. Dodge the first:}
-  ----
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest .thought}
+  _I have no idea. Dodge the first:_
+  --
+  --{.interview-guest}
   Well, it depends on the time of day and [...] year
   but at peak times of year it can come down by
   thousands of dollars a megawatt hour.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   And to put this again into perspective, we know that at peak times of...
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   But that--- sorry to interrupt you [...],
   I don't know what a megawatt hour is when I look at my power bill.
   I just want to know:
   by what percentage is my power bill going to come down by?
-  ----
-  
-  \angus-taylor
-  ----{.gt}
-  {I have no idea. Dodge the second:}
-  ----
-  ----{.g}
-  Well yeah the benefits across all Australians is [\d]4.4~[billion],
-  as I say \d4.4~to \d6.8~billion, and that's a very significant...
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest .thought}
+  _I have no idea. Dodge the second:_
+  --
+  --{.interview-guest}
+  Well yeah the benefits across all Australians is [$]4.4~[billion],
+  as I say $4.4~to $6.8~billion, and that's a very significant...
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   No but that doesn't mean anything to me,
   because my bill's in hundreds of dollars,
   so what does my bill go down by?
-  ----
-  
-  \angus-taylor
-  ----{.gt}
-  {I have no idea. Dodge the third:}
-  ----
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest .thought}
+  _I have no idea. Dodge the third:_
+  --
+  --{.interview-guest}
   It depends on how much you're spending now
   but we're talking, uh you know, very... significant amounts
   for businesses, for industry and for households.
-  ----
-  ----{.gt}
-  {And no, I am unable to quantify "significant"
-  because I am using it as a rhetorical device.}
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _And no, I am unable to quantify "significant"
+  because I am using it as a rhetorical device._
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Why has Snowy 2.0 doubled in cost
   and blown out significantly in terms of time?
-  ----
-  
-  \angus-taylor
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest}
   Well, it hasn't
-  ----
-  ----{.gt}
-  {but only if you ignore Malcolm Turnbull's initial quote of \d2~billion
-  and 4~years, which I sense is the whole point of your question}.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _but only if you ignore Malcolm Turnbull's initial quote of $2~billion
+  and 4~years, which I sense is the whole point of your question_.
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Well, it has. Malcolm Turnbull said it was going to be 4~years originally?
-  ----
-  
-  \angus-taylor
-  ----{.gt}
-    {Bugger.}
-  ----
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest .thought}
+  _Bugger._
+  --
+  --{.interview-guest}
   The original feasibility...
   the feasibility study work was done in December 2017.
-  The numbers were [\d]3.8~to [\d]4.5~billion and we're in line with those.
-  ----
-  ----{.gt}
-  {Of course, "in line" has a different meaning
-  if we are referring to spending by Labor.}
-  ----
-  ----{.g}
+  The numbers were [$]3.8~to [$]4.5~billion and we're in line with those.
+  --
+  --{.interview-guest .thought}
+  _Of course, "in line" has a different meaning
+  if we are referring to spending by Labor._
+  --
+  --{.interview-guest}
   The commissioning will be done in 2024 to 2025, and they were numbers,
   they were times that were established during the feasibility work.
-  ----
-  ----{.gt}
-  {Gee, it's so convenient just ignoring Malcolm Turnbull's numbers eh?}
-  ----
-  
-  \leigh-sales
-  ----{.h}
-  Australians were originally told it would cost \d2~billion,
+  --
+  --{.interview-guest .thought}
+  _Gee, it's so convenient just ignoring Malcolm Turnbull's numbers eh?_
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
+  Australians were originally told it would cost $2~billion,
   then less than a year later that doubled.
   Earlier this year a contract for part of it was signed
-  for more than \d5~billion.
-  ----
-  ----{.h}
+  for more than $5~billion.
+  --
+  --{.interview-host}
   Is there a limit to what the Government will chip in?
   Is it not a blank cheque because [...]
   [it is] key policy and therefore it has to happen?
-  ----
-  
-  \angus-taylor
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest}
   No, Leigh, the decision was made based on
   the feasibility study in December 2017.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   They were the numbers that count,
   and the numbers are still in line with that feasibility study.
-  ----
-  ----{.gt}
-  {Since we are talking about the Coalition,
+  --
+  --{.interview-guest .thought}
+  _Since we are talking about the Coalition,
   "in line" means if you squint hard enough, they look the same.
-  Never mind that \d5~billion already exceeds the \d4.5~billion
-  quoted by the feasibility study.}
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  Never mind that $5~billion already exceeds the $4.5~billion
+  quoted by the feasibility study._
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   And is there an upper limit to what the Government will chip in?
-  ----
-  
-  \angus-taylor
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest}
   Well, we intend to achieve the outcomes of the feasibility study
-  ----
-  ----{.gt}
-  {but after all, that's *intent*, so the answer is 'no'}.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _but after all, that's *intent*, so the answer is 'no'_.
+  --
+  --{.interview-guest}
   That's the plan and I'm assured by the Snowy chief executive and board
   that we will achieve those outcomes.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   The CEO of Snowy Hydro, Paul Broad, says that
   because of the amount of government money being spent,
   Australians are either going to have to pay for this
   through their taxes or through their bills.
   Which will it be?
-  ----
-  
-  \angus-taylor
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest}
   Well, the truth of the matter is,
-  it's going to reduce bills by \d4.4~to \d6.8~billion.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  it's going to reduce bills by $4.4~to $6.8~billion.
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   So does that mean that we'll be paying for it through our taxes?
-  ----
-  
-  \angus-taylor
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest}
   And the reason... Well the rea... Well, no.
-  ----
-  ----{.gt}
-  {In other words, I am disagreeing with Paul Broad,
+  --
+  --{.interview-guest .thought}
+  _In other words, I am disagreeing with Paul Broad,
   whose assurances were enough to satisfy me a few moments ago
   regarding achieving outcomes of the feasibility study.
-  And cue spiel about reducing volatility:}
-  ----
-  ----{.g}
+  And cue spiel about reducing volatility:_
+  --
+  --{.interview-guest}
   I mean, the reason there is an opportunity
   to deliver a better outcome for consumers is very simple.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   Right now, Leigh, we are seeing huge volatility in electricity prices
   [...].
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   But minister, we're talking about government spending
   of a massive scale [...].
   Where does the money come from? It's got to come from somewhere.
-  ----
-  
-  \angus-taylor
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest}
   Well, the money comes from...
   the investment is being made by the Government---
-  ----
-  ----{.gt}
-  {Whoops, did I say "Government"?
-  "Government" is a euphemism for "taxpayer".}
-  ----
-  ----{.g}
-  \d1.38~billion in equity investment [...].
-  ----
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _Whoops, did I say "Government"?
+  "Government" is a euphemism for "taxpayer"._
+  --
+  --{.interview-guest}
+  $1.38~billion in equity investment [...].
+  --
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Minister, I'm sure you've got Cabinet tonight.
   Thank you very much for making time to squeeze us in.
-  ----
-  
-  \angus-taylor
-  ----{.g}
+  --
+
+  ![Angus Taylor:]
+  --{.interview-guest}
   Thanks for having me, Leigh.
-  ----
-  
-  \END
-  
+  --
+
+  ||{.marker .end}
+    END
+  ||
+
 ||||
 
-%footer-element
+
+%%footer
