43f4d4b396ba71f3517b052947ad286cf49953ee
Conway <yawnoc@pm.me>
2022-06-04 22:54:11 +0800

    Convert /lit/lion-eating to CMDv4

--- lit/lion-eating.cmd
+++ lit/lion-eating.cmd
@@ -1,158 +1,151 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title "[An] History of Mr Shih's Eating Lions" 《施氏食獅史》
-  %date-created 2019-04-07
-  %date-modified 2022-01-24
-  \resources:rendering
-  %description
-    An English translation of the Mandarin "lion tongue twister" \
-    《施氏食獅史》, "shi shi shi shi shi", or \
-    "[An] History of Mr Shih's Eating Lions".
-%%
-
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:translation
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"[An] History of Mr Shih's Eating Lions"'
+* %title --> "[An] History of Mr Shih's Eating Lions" 《施氏食獅史》
+* %description -->
+    An English translation of the Mandarin `lion tongue twister`
+    《施氏食獅史》, `shi shi shi shi shi`, or \
+    `[An] History of Mr Shih's Eating Lions`.
+* %date-created --> 2019-04-07
+* %date-modified --> 2022-xx-xx
+- concluding_replacements:
+    #.yawnoc.double-quotes-in-attributes
+    #.yawnoc.typography
+    #backslash-continuations
 
 
-#
-  《施氏食獅史》 \+
-  ".[An] History of Mr~((Shih | ~~施~~))'s Eating Lions"
-#
+%%%
 
-[||||
-||||]
 
-----
+^^^^
+- !home
+- !top
+- !translation
+- !cite
+^^^^
+
+
+# .《施氏食獅史》 ".[An] History of Mr~(Shih|施)'s Eating Lions"
+
+--
 This is the passage composed by [Yuen Ren Chao],
-with every character pronounced 尸 (Pinyin:~shi) up to tone in Mandarin.
-----
+with every character pronounced ㄕ (Pinyin:~shi) up to tone in Mandarin.
+--
 
-@@[Yuen Ren Chao]
-  https://en.wikipedia.org/wiki/Yuen_Ren_Chao
-@@
+[Yuen Ren Chao]: https://en.wikipedia.org/wiki/Yuen_Ren_Chao
 
 
+##{#translation} Translation
 
-##{#translation}
-  Translation
-##
+--
+The Chinese source text is from Page~143
+of Chao's [《語言問題》]{l=zh-Hant} (.臺灣商務印書館, 1968).
+--
 
-
-
-----
-The Chinese source text is from Page~143 of Chao's
-[《語言問題》](
-  https://books.google.com.au/books\
-    ?id=AQs2KCQharAC&pg=PA143\
-    &dq=%E8%AA%9E%E8%A8%80%E5%95%8F%E9%A1%8C+\
-      %E6%96%BD%E6%B0%8F%E9%A3%9F%E7%8D%85%E5%8F%B2\
-    &hl=en\
-    &sa=X\
-    &ved=0ahUKEwj7-Jb0zLvhAhUSfH0KHctQAt4Q6AEILzAB#v=onepage\
-    &q=%E8%AA%9E%E8%A8%80%E5%95%8F%E9%A1%8C%20%E6%96%BD%\
-      E6%B0%8F%E9%A3%9F%E7%8D%85%E5%8F%B2\
-    &f=false
-)
-(臺灣商務印書館, 1968).
-----
+[《語言問題》]: https://books.google.com.au/books?id=AQs2KCQharAC&pg=PA143&dq=語言問題%20施氏食獅史&q=語言問題%20施氏食獅史
 
 <<
   《施氏食獅史》
-||
-  ".[An] History of Mr~((Shih | ~~施~~))'s Eating Lions"
+\\
+  ".[An] History of Mr~(Shih|施)'s Eating Lions"
 >>
-====
-* 施氏： Mr~((Shih | ~~施~~)); \lit surname ((Shih | ~~施~~))
-====
+==
+* .施氏： Mr~(Shih|施); \lit surname (Shih|施)
+==
 
 <<
   石室詩士施氏，嗜獅，誓食十獅。
-||
-  .[The] poet Mr~((Shih | ~~施~~)) of .[a] stone house,
+\\
+  .[The] poet Mr~(Shih|施) of .[a] stone house,
   liked lions, .[and] sware to eat ten lions.
 >>
-====
-* 石： stone; \or rock
-====
+==
+* .石： stone; \or rock
+==
 
 <<
   氏時時適市視獅。
-||
-  Mr~.[((Shih | ~~施~~))] constantly went unto market .[to] view lions.
+\\
+  Mr~.[(Shih|施)] constantly went unto market .[to] view lions.
 >>
-====
-* 氏 \+
-  Here, 施 has been dropped from 施氏.
-====
+==
+* .氏
+  --
+  Here, .施 has been dropped from .施氏.
+  --
+==
 
 <<
   十時，適十獅適市。是時，適施氏適市。
-||
+\\
   .[At the] tenth hour, ten lions .[were] just gone unto market.
-  .[At] this time, Mr~((Shih | ~~施~~)) .[was] just gone unto market.
+  .[At] this time, Mr~(Shih|施) .[was] just gone unto market.
 >>
 
 <<
   氏視是十獅，恃矢勢，使是十獅逝世。
-||
-  Mr~.[((Shih | ~~施~~))] viewed these ten lions,
+\\
+  Mr~.[(Shih|施)] viewed these ten lions,
   .[and], relying upon the arrow's momentum,
   made these ten lions pass away.
 >>
-====
-* 使： made; \or caused
-====
+==
+* .使： made; \or caused
+==
 
 <<
   氏拾是十獅屍，適石室。
-||
-  Mr~.[((Shih | ~~施~~))] picked up these ten lions' carcasses,
+\\
+  Mr~.[(Shih|施)] picked up these ten lions' carcasses,
   .[and] went unto .[the] stone house.
 >>
 
 <<
   石室濕，氏使侍拭石室。
-||
+\\
   .[The] stone house wet,
-  Mr~.[((Shih | ~~施~~))] made servants wipe .[the] stone house.
+  Mr~.[(Shih|施)] made servants wipe .[the] stone house.
 >>
 
 <<
   石室拭，氏始試食是十獅屍。
-||
+\\
   .[The] stone house wiped,
-  Mr~.[((Shih | ~~施~~))] began to try eating these ten lions' carcasses.
+  Mr~.[(Shih|施)] began to try eating these ten lions' carcasses.
 >>
 
 <<
   食時，始識是十獅屍，實十石獅屍。
-||
+\\
   .[Only] when eating,
   .[did he] first know .[that] these ten lions' carcasses,
   .[were] actually ten stone lions' carcasses.
 >>
-====
-* 始： first; \lit begin to
-====
+==
+* .始： first; \lit begin to
+==
 
 <<
   試釋是事。
-||
+\\
   Try to interpret these events.
 >>
-====
-* 是事： these events; \or this incident
-====
+==
+* .是事： these events; \or this incident
+==
 
-\cite-this-page[~~ "[An] History of Mr Shih's Eating Lions" ~~]
 
-%footer-element
+%%cite
+
+
+%%footer
