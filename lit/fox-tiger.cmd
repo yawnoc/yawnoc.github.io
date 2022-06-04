5dffbc0039b67440185a7f46f27bb5f3f24e3f69
Conway <yawnoc@pm.me>
2022-06-04 21:48:11 +0800

    Convert /lit/fox-tiger to CMDv4

--- lit/fox-tiger.cmd
+++ lit/fox-tiger.cmd
@@ -1,138 +1,149 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title "[The] Fox Feigneth [the] Tiger's Authority" 《狐假虎威》
-  %title-en ".[The] Fox Feigneth .[the] Tiger's Authority"
-  %date-created 2019-04-07
-  %date-modified 2022-02-19
-  \resources:rendering
-  %description
-    An English translation of 《狐假虎威》, \
-    "[The] Fox Feigneth [the] Tiger's Authority".
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"[The] Fox Feigneth [the] Tiger's Authority"'
+* %title --> "[The] Fox Feigneth [the] Tiger's Authority" 《狐假虎威》
+* %description -->
+    An English translation of the original text for 《狐假虎威》, \
+    &quot;[The] Fox Feigneth [the] Tiger's Authority&quot;.
+* %date-created --> 2019-04-07
+* %date-modified --> 2022-xx-xx
+- concluding_replacements:
+    #.yawnoc.typography
+    #backslash-continuations
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:translation
-* \header-link:cite
-====]]
+%%%
 
 
-#
-  《狐假虎威》 \+
-  %title-en
-#
+^^^^
+- !home
+- !top
+- !translation
+- !cite
+^^^^
 
-[||||
-||||]
 
-----
+# .《狐假虎威》 ".[The] Fox Feigneth .[the] Tiger's Authority"
+
+
+--
 This idiom originates from a fable told
-in the ["Warring States' Strategies"][戰國策],
-compiled/edited by [((Liu Hiang | ~~劉向~~))][劉向].
+in the [''Warring States\' Strategies''],
+compiled/edited by [(Liu Hiang|劉向)].
 See also:
-----
-====
-* ["XI.~The Fox and the Tiger"][fairy book] in '''The Chinese Fairy Book'''
+--
+==
+* [''XI.~The Fox and the Tiger''] in ''The Chinese Fairy Book''
   by Richard Wilhelm, translated by Frederick H.~Martens
-* ['''The Gruffalo'''](https://en.wikipedia.org/wiki/The_Gruffalo),
+* [''The Gruffalo''],
   by Julia Donaldson
-====
+==
 
-@@[戰國策]
+[''Warring States\' Strategies'']:
   https://en.wikipedia.org/wiki/Zhan_Guo_Ce
-@@
-@@[劉向]
+[(Liu Hiang|劉向)]:
   https://en.wikipedia.org/wiki/Liu_Xiang_(scholar)
-@@
-@@[fairy book]
+[''XI.~The Fox and the Tiger'']:
   https://en.wikisource.org/wiki/Chinese_Fairy_Book_(Richard_Wilhelm)/11
-@@
+[''The Gruffalo'']:
+  https://en.wikipedia.org/wiki/The_Gruffalo
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
 <<
   《狐假虎威》
-||
+\\
   ".[The] Fox Feigneth .[the] Tiger's Authority"
 >>
-====
-* 威： authority; \lit might
-====
+==
+* .威： authority; \lit might
+==
 
-----
+--
 The Chinese source text below is from Chinese Text Project,
 but with olden-style punctuation:
-[先秦兩漢 \> 史書 \> 戰國策 \> 楚策 \> 楚一 \> 荊宣王問群臣][source].
-----
+[先秦兩漢 > 史書 > 戰國策 > 楚策 > 楚一 > 荊宣王問群臣]{l=zh-Hant}[source].
+--
 
-@@[source]
-  https://ctext.org/zhan-guo-ce/jing-xuan-wang-wen-qun-chen
-@@
+[source]: https://ctext.org/zhan-guo-ce/jing-xuan-wang-wen-qun-chen
 
 <<
   荊宣王問群臣曰、吾聞北方之畏昭奚恤也、果誠何如。
-||
-  King~((S(u")an | ~~宣~~)) of~((King | ~~荊~~))
+\\
+  King~(S(u")an|宣) of~(King|荊)
   asked .[his] group of ministers, saying,
-    I hear of .[the] North's fear of ((Chao Hi-s(u") | ~~昭奚恤~~));
+    I hear of .[the] North's fear of (Chao Hi-s(u")|昭奚恤);
     if .[it be] true, what of .[it]?
 >>
-====
-* 荊： ((King | ~~荊~~)) \+
-  Another name for the state of~((Ch'u | ~~楚~~)), 楚.
-* 群： group; \or gathering
-* 昭奚恤： ((Chao Hi-s(u") | ~~昭奚恤~~)) \+
-  The name of a high military official of~((Ch'u | ~~楚~~)).
-====
+==
+* .荊： (King|荊)
+  --
+  Another name for the state of~(Ch'u|楚) (.楚).
+  --
+
+* .群： group; \or gathering
+
+* .昭奚恤： (Chao Hi-s(u")|昭奚恤)
+  --
+  The name of a high military official of~(Ch'u|楚).
+  --
+==
 
 <<
   群臣莫對。
-||
+\\
   .[And his] group of ministers could not reply.
 >>
 
 <<
   江一對曰、虎求百獸而食之、得狐、
-||
-  .[But] ((Kiang I | ~~江一~~)) replied, saying,
+\\
+  .[But] (Kiang I|江一) replied, saying,
     .[A] tiger sought .[the] hundred beasts .[to] then eat them,
     .[and] obtained .[a] fox.
 >>
-====
-* 江一： ((Kiang I | ~~江一~~)) \+
-  A minister of King~((S(u")an | ~~宣~~)) of~((King | ~~荊~~)).
-  In another manuscript he is called 江乙,
-  also ((Kiang I | ~~江乙~~)).
-====
+==
+* .江一： (Kiang I|江一)
+  --
+  A minister of King~(S(u")an|宣) of~(King|荊).
+  In another manuscript he is called .江乙,
+  also (Kiang I|江乙).
+  --
+==
 
 <<
   狐曰、子無敢食我也、天帝使我長百獸、今子食我、是逆天帝命也。
-||
+\\
   .[The] fox said,
     Thou darest not eat me.
     .[The] Emperor of Heaven .[hath] made me be leader over .[the] hundred beasts;
     shouldst thou eat me,
     this .[shall] defy .[the] will of .[the] Emperor of Heaven.
 >>
-====
-* 子： thou; \lit master
-* 長： be leader over \+
-  長、上聲、 \C ch(oe)ng2, \M zh(av)ng
-* 逆： defy; \or go against
-====
+==
+* .子： thou; \lit master
+
+* .長： be leader over
+  --
+  .長、上聲、 \C ch(oe)ng2, \M zh(av)ng
+  --
+
+* .逆： defy; \or go against
+==
 
 <<
   子以我為不信、吾為子先行、子隨我後、觀百獸之見我而敢不走乎。
-||
+\\
   .[Shouldst] thou think me untrustworthy,
   .[let] me walk first for thee,
   .[and] thee follow after me,
@@ -142,45 +153,49 @@ but with olden-style punctuation:
 
 <<
   虎以為然、故遂與之行、獸見之皆走。
-||
+\\
   .[The] tiger thought so, therefore .[he] then walked with him.
   .[The] beasts seeing him all ran.
 >>
 
 <<
   虎不知獸畏己而走也、以為畏狐也。
-||
+\\
   .[The] tiger knew not .[that the] beasts feared himself and ran,
   .[but] thought .[that they] feared .[the] fox.
 >>
 
 <<
   今王之地方五千里、帶甲百萬、而專屬之昭奚恤、
-||
+\\
   Today King's territory .[spanneth a] circumference of five thousand miles,
   .[with] armour-donning .[soldiers a] million,
-  and exclusively subordinate .[are] they unto ((Chao Hi-s(u") | ~~昭奚恤~~));
+  and exclusively subordinate .[are] they unto (Chao Hi-s(u")|昭奚恤);
 >>
-====
-* 地： territory; \lit land
+==
+* .地： territory; \lit land
 
-* 方： circumference
+* .方： circumference
 
-* 里： miles \+
+* .里： miles
+  --
   A [unit of distance](https://en.wikipedia.org/wiki/Li_\(unit\)),
   of the order half a kilometre.
+  --
 
-* 百萬： .[a] million; \lit hundred myriad
-====
+* .百萬： .[a] million; \lit hundred myriad
+==
 
 <<
   故北方之畏奚恤也、其實畏王之甲兵也、猶百獸之畏虎也。
-||
-  therefore .[the] North's fear of ((Hi-s(u") | ~~奚恤~~)),
+\\
+  therefore .[the] North's fear of (Hi-s(u")|奚恤),
   it .[is] actually fear of King's armoured troops,
   like .[the] hundred beasts' fear of .[the] tiger.
 >>
 
-\cite-this-page[~~ "[The] Fox Feigneth [the] Tiger's Authority" ~~]
 
-%footer-element
+%%cite
+
+
+%%footer
