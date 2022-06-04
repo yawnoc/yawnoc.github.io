7b209eb71ae90c7f2eff09a72b9f923d4868879c
Conway <yawnoc@pm.me>
2022-06-04 17:22:25 +0800

    Convert sales-bowen-climate to CMDv4

--- cynics-730/2021/sales-bowen-climate.cmd
+++ cynics-730/2021/sales-bowen-climate.cmd
@@ -1,242 +1,230 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/cynicism.cmdr
 
-{+ /resources/syntax/cynics-730.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title A cynic's 7.30: \short-title
-  %date-created 2021-02-03
-  %date-modified 2021-02-03
-  \resources
-  %footer-copyright-remark
-    except the [original interview][original],
-    whose copyright belongs to the ABC
-  %footer-remark
-    [:{Curly-bracketed words}:] inserted by me.
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %title --> A cynic's 7.30: Chris Bowen on climate change & jobs (2 Feb 2021)
+* %date-created --> 2021-02-03
+* %date-modified --> 2022-xx-xx
+- concluding_replacements:
+    #escape-idle-html
+    #.yawnoc.typography
 
-{: \short-title : Chris Bowen on climate change \& jobs (2 Feb 2021) :}
-
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:cynicism
-====]]
+%%%
 
 
-# %title #
+^^^^
+- !home
+- !top
+- !cynicism
+^^^^
 
 
-[||||
-[Original 7.30 interview][original] ([archived version])
-||||]
+# %title
 
-@@[original]
-  https://www.abc.net.au/7.30/\
-    chris-bowen-says-climate-change-is-a-key-economic/\
-    13114800
-@@
+--
+([Original 7.30 interview], [archived version])
+--
 
-@@[archived version]
-  https://web.archive.org/web/20210203064334/\
-    https://www.abc.net.au/7.30/\
-      chris-bowen-says-climate-change-is-a-key-economic/\
-      13114800
-@@
-
-##
-  Chris Bowen says climate change is a key economic issue
-##
+[Original 7.30 interview]:
+  https://www.abc.net.au/7.30/chris-bowen-says-climate-change-is-a-key-economic/13114800
+[archived version]:
+  https://web.archive.org/web/20210203064334/https://www.abc.net.au/7.30/chris-bowen-says-climate-change-is-a-key-economic/13114800
 
 
-{: \leigh-sales : ![LEIGH SALES:] :}
-@@[leigh sales:]{.h}
-  leigh-sales-2021-02-02.jpg
-@@
+## Chris Bowen says climate change is a key economic issue
 
-{: \chris-bowen : ![CHRIS BOWEN:] :}
-@@[chris bowen:]{.g}
-  chris-bowen-2021-02-02.jpg
-@@
+
+[Leigh Sales:]{.interview-host}: leigh-sales-2021-02-02.jpg
+[Chris Bowen:]{.interview-guest}: chris-bowen-2021-02-02.jpg
+
 
 ||||{.interview}
-  
-  \leigh-sales
-  ----{.h}
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Chris Bowen, thanks for coming on.
-  ----
-  
-  \chris-bowen
-  ----{.g}
+  --
+
+  ![Chris Bowen:]
+  --{.interview-guest}
   Pleasure Leigh, good evening.
-  ----
-  
-  ----{.h}
+  --
+
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   So, let's say people in coal mines in the Hunter Valley do lose their jobs;
   what kind of new jobs would you see them transitioning into?
-  ----
-  
-  \chris-bowen
-  ----{.g}
+  --
+
+  ![Chris Bowen:]
+  --{.interview-guest}
   Well, um, I see many thousands of jobs
-  ----
-  ----{.gt}
-  {of unspecified type}
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _of unspecified type_
+  --
+  --{.interview-guest}
   being created by good climate change policy,
   both [...] [in] renewable energy,
   and also in reinvigorating traditional manufacturing industries [...].
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   So manufacturing jobs:
   like what sort of manufacturing would you see happening
   somewhere like the Hunter?
-  ----
-  
-  \chris-bowen
-  ----{.g}
+  --
+
+  ![Chris Bowen:]
+  --{.interview-guest}
   Oh, well, um, when you get the policy settings right,
   then the private sector can invest in all sorts of opportunities
-  ----
-  ----{.gt}
-  {again of unspecified type, but this time I'll throw in some electorates:}
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _again of unspecified type, but this time I'll throw in some electorates:_
+  --
+  --{.interview-guest}
   in the Hunter, in Western Sydney, in Western Melbourne,
   right across the country.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   So I guess, [...] if I was sitting in the Hunter watching this tonight,
   what I want to know is, [...] in concrete terms,
   like what sort of a job would I have?
   [...] do you mean [...] in a factory? on an assembly line?
   Like what sort of jobs do you mean?
-  ----
-  
-  \chris-bowen
-  ----{.g}
+  --
+
+  ![Chris Bowen:]
+  --{.interview-guest}
   Well, I mean jobs that'll be created across the board, Leigh,
-  ----
-  ----{.gt}
-  {and the scope is so wide that I can't name a specific example.}
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _and the scope is so wide that I can't name a specific example._
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Before the last election,
   Labor pledged a 45 per cent cut to emissions by 2030.
   [...]
   What *has* changed, now that that target is seen as too ambitious?
-  ----
-  
-  \chris-bowen
-  ----{.g}
+  --
+
+  ![Chris Bowen:]
+  --{.interview-guest}
   Well, we haven't been in office, Leigh, [...]
-  ----
-  
-  \leigh-sales
-  ----{.ht}
-  {That's not a change,
-  but I'll hand it to you for making a frank admission...}
-  ----
-  
-  \chris-bowen
-  ----{.gt}
-  {No no no! Totally am not admitting we need to
-  make our climate policy less ambitious to get elected!}
-  ----
-  ----{.g}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host .thought}
+  _That's not a change,
+  but I'll hand it to you for making a frank admission..._
+  --
+
+  ![Chris Bowen:]
+  --{.interview-guest .thought}
+  _No no no! Totally am not admitting we need to
+  make our climate policy less ambitious to get elected!_
+  --
+  --{.interview-guest}
   [...] Now we've got a very clear commitment to net zero by 2050...
-  ----
-  
-  ----{.h}
+  --
+
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Labor has had trouble getting its own party united on a policy in this area.
   If you can't persuade your own party on one direction,
   how will you be able to persuade the electorate?
-  ----
-  
-  \chris-bowen
-  ----{.g}
+  --
+
+  ![Chris Bowen:]
+  --{.interview-guest}
   Well, I don't necessarily agree with that Leigh,
   because everybody in the Labor Party
   accepts that climate change is real and we need a policy to deal with it
-  ----
-  ----{.gt}
-  {except Joel Fitzgibbon, who thinks we need a policy to not lose elections}.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _except Joel Fitzgibbon, who thinks we need a policy to not lose elections_.
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Yeah, but there's a lot of wiggle room after that...
-  ----
-  
-  \chris-bowen
-  ----{.g}
+  --
+
+  ![Chris Bowen:]
+  --{.interview-guest}
   [...]
   In the Labor Party, sure we have a policy debate about mechanisms etc.
-  ----
-  ----{.gt}
-  {particularly for selecting the party leader,}
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _particularly for selecting the party leader,_
+  --
+  --{.interview-guest}
   but we are all 100 per cent committed to a climate change policy,
   which deals with man---
-  ----
-  ----{.gt}
-  {whoopsie-daisy}
-  ----
-  ----{.g}
-  ---uh humankind causing climate change.
-  ----
-  
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _whoopsie-daisy_
+  --
+  --{.interview-guest}
+  <` `>---uh humankind causing climate change.
+  --
+
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Chris Bowen, thank you very much.
-  ----
-  
-  \chris-bowen
-  ----{.g}
+  --
+
+  ![Chris Bowen:]
+  --{.interview-guest}
   Thank you, Leigh.
-  ----
-  
-  \END
-  
+  --
+
+  ||{.marker .end}
+    END
+  ||
+
 ||||
 
-%footer-element
+
+%%footer
