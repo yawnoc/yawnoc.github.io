bb29078665fd3c8557805e2f3e5da753a050c41a
Conway <yawnoc@pm.me>
2022-06-04 16:57:26 +0800

    Convert sales-frydenberg-surplus to CMDv4

--- cynics-730/2020/sales-frydenberg-surplus.cmd
+++ cynics-730/2020/sales-frydenberg-surplus.cmd
@@ -1,468 +1,439 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/cynicism.cmdr
 
-{+ /resources/syntax/cynics-730.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title A cynic's 7.30: \short-title
-  %date-created 2020-03-16
-  %date-modified 2020-03-16
-  \resources
-  %footer-copyright-remark
-    except the [original interview][original],
-    whose copyright belongs to the ABC
-  %footer-remark
-    [:{Curly-bracketed words}:] inserted by me.
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %title -->
+    A cynic's 7.30: Josh Frydenberg on surplus predictions (12~Mar 2020)
+* %date-created --> 2020-03-16
+* %date-modified --> 2022-xx-xx
+- concluding_replacements: #.yawnoc.typography
 
-{: \short-title : Josh Frydenberg on surplus predictions (12 Mar 2020) :}
-
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:cynicism
-====]]
+%%%
 
 
-# %title #
+^^^^
+- !home
+- !top
+- !cynicism
+^^^^
 
 
-[||||
-[Original 7.30 interview][original] ([archived version])
-||||]
+# %title
 
-@@[original]
-  https://www.abc.net.au/7.30/\
-    josh-frydenberg-says-the-governments-surplus/\
-    12052830
-@@
-
-@@[archived version]
-  https://web.archive.org/web/20200315042335/\
-    https://www.abc.net.au/7.30/\
-      josh-frydenberg-says-the-governments-surplus/\
-      12052830
-@@
+--
+([Original 7.30 interview], [archived version])
+--
 
 
-##
-  Josh Frydenberg says the Government's surplus prediction \
-  was based on 'forecasts at the time'
-##
-
-<## Excerpt images ##>
-
-{: \leigh-sales-2019 : ![LEIGH SALES (2019):] :}
-@@[leigh sales (2019):]{.h}
-  leigh-sales-2019-04-02.jpg
-@@
-
-{: \josh-frydenberg-2019-730 : ![JOSH FRYDENBERG (2019) on 7.30:] :}
-@@[josh frydenberg (2019) on 7.30:]{.g}
-  josh-frydenberg-2019-04-02-730.jpg
-@@
-
-{: \david-speers-2019 : ![DAVID SPEERS (2019):] :}
-@@[david speers (2019):]{.h}
-  david-speers-2019-04-02.jpg
-@@
-
-{: \josh-frydenberg-2019-sky : ![JOSH FRYDENBERG (2019) on Sky News:] :}
-@@[josh frydenberg (2019) on sky news:]{.g}
-  josh-frydenberg-2019-04-02-sky.jpg
-@@
-
-<## Main images ##>
-
-{: \leigh-sales : ![LEIGH SALES:] :}
-@@[leigh sales:]{.h}
-  leigh-sales-2020-03-12.jpg
-@@
-
-{: \josh-frydenberg : ![JOSH FRYDENBERG:] :}
-@@[josh frydenberg:]{.g}
-  josh-frydenberg-2020-03-12.jpg
-@@
+[Original 7.30 interview]:
+  https://www.abc.net.au/7.30/josh-frydenberg-says-the-governments-surplus/12052830
+[archived version]:
+  https://web.archive.org/web/20200315042335/https://www.abc.net.au/7.30/josh-frydenberg-says-the-governments-surplus/12052830
 
 
+## Josh Frydenberg says the Government's surplus prediction was based on 'forecasts at the time'
 
-||||||{.interview}
-  
-  \leigh-sales
-  ----{.h}
+
+[Leigh Sales:]{.interview-host}: leigh-sales-2020-03-12.jpg
+[Josh Frydenberg:]{.interview-guest}: josh-frydenberg-2020-03-12.jpg
+
+[Leigh Sales 2019]{.interview-host}: leigh-sales-2019-04-02.jpg
+[Josh Frydenberg 2019 7.30]{.interview-guest}: josh-frydenberg-2019-04-02-730.jpg
+
+[David Speers:]{.interview-host}: david-speers-2019-04-02.jpg
+[Josh Frydenberg 2019 Sky News]{.interview-guest}: josh-frydenberg-2019-04-02-sky.jpg
+
+
+||||{.interview}
+
+  ![Leigh Sales:]
+  --{.interview-host}
   The Treasurer, Josh Frydenberg, is with me now from Canberra.
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   Treasurer, let me start with a question in your capacity,
   as a member of the National Security Committee of Cabinet:
   why is the current decision not to close Australia's borders
   or to cancel mass events?
-  ----
-  
-  \josh-frydenberg
-  ----{.gt}
-  {Because the advice we've received says so.}
-  ----
-  ----{.g}
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest .thought}
+  _Because the advice we've received says so._
+  --
+  --{.interview-guest}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   The best possible medical advice to us is that
   [the existing] travel restrictions should stay in place
   but they shouldn't be extended to other countries,
   including to broader Europe.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   So we'll continue to take the best possible advice
-  ----
-  ----{.gt}
-  {and blame the advisors if necessary}.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _and blame the advisors if necessary_.
+  --
+  --{.interview-guest}
   When it comes to mass gatherings, again,
   we take the advice of the chief medical officer [...].
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   Of course, states can take their own decisions
-  ----
-  ----{.gt}
-  {which I welcome, since *they* would then take the blame
+  --
+  --{.interview-guest .thought}
+  _which I welcome, since *they* would then take the blame
   for any public-health or economy-related consequences
   associated with those decisions,
-  instead of us,}
-  ----
-  ----{.g}
+  instead of us,_
+  --
+  --{.interview-guest}
   but right now the advice is not to avoid those mass gatherings.
-  ----
-  
-  ----{.h}
+  --
+
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Given how quickly this situation is evolving,
   is it possible that the stimulus package you've announced today
   could be the first of a number of them for the Australian economy?
-  ----
-  
-  \josh-frydenberg
-  ----{.gt}
-  {I hope not,
-  I would really like to see a surplus before the next election...}
-  ----
-  ----{.g}
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest .thought}
+  _I hope not,
+  I would really like to see a surplus before the next election..._
+  --
+  --{.interview-guest}
   Well this is a very substantial package [...]
   in response to a pretty significant economic impact.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   But of course, the budget is in eight weeks' time
   and the coronavirus is not going away,
   so we'll continue to do what is necessary
   to support the Australian economy.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   You mentioned the budget,
   you've made these measures temporary so they're not baked into the budget,
   but isn't this a time to *use* the budget
   to also make some structural changes,
   for example bringing forward your planned income tax cuts
   across all brackets?
-  ----
-  
-  \josh-frydenberg
-  ----{.gt}
-  {I'll take that as a compliment.}
-  ----
-  ----{.g}
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest .thought}
+  _I'll take that as a compliment._
+  --
+  --{.interview-guest}
   Well again, we've shown our mettle when it comes to reducing taxes.
-  We've passed through the Parliament over \d300~billion of tax cuts...
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  We've passed through the Parliament over $300~billion of tax cuts...
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   I didn't ask what you've done, I asked about what you might do.
-  ----
-  
-  \josh-frydenberg
-  ----{.gt}
-  {I know, but free airtime for the Coalition \& tax cuts...}
-  ----
-  ----{.g}
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest .thought}
+  _I know, but free airtime for the Coalition & tax cuts..._
+  --
+  --{.interview-guest}
   Yeah, but in terms of what we have announced today,
-  there is [\sic] substantial incentives through the tax system
+  there is [sic] substantial incentives through the tax system
   for businesses to invest,
   and that continues to be a priority for us.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...] the actual initiatives [for our stimulus package] are
   targeted, [...] temporary and [...] proportionate [...].
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   But that doesn't address my question which is,
   shouldn't the budget be used to make some structural changes
   that can help the country's fiscal position going forward?
-  ----
-  
-  \josh-frydenberg
-  ----{.gt}
-  {The Coalition's tax cuts are good!}
-  ----
-  ----{.g}
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest .thought}
+  _The Coalition's tax cuts are good!_
+  --
+  --{.interview-guest}
   Well we continue to undertake structural reform
   and that includes our tax legislation that passed through the Parliament.
   But today's announcements had pretty substantial...
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   You're still not addressing, sorry Treasurer, what I'm asking.
-  ----
-  
-  \josh-frydenberg
-  ----{.gt}
-  {Oh I don't know. Wait till the budget.}
-  ----
-  ----{.g}
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest .thought}
+  _Oh I don't know. Wait till the budget._
+  --
+  --{.interview-guest}
   Well, in terms of what comes forward in the budget,
   you'll have to switch on in May for that outcome.
   [...]
-  ----
-  ----{.gt}
-  {The Coalition doesn't spend as much as Labor!}
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _The Coalition doesn't spend as much as Labor!_
+  --
+  --{.interview-guest}
   But Leigh, we have done the hard yards,
   over the last six and a half years
   to bring the budget back into balance,
   which has given us now the flexibility
   to use this financial firepower at a time when it's needed most.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   You'll only be able to tell in hindsight if you've made the right decision
   about the size of today's stimulus package
   and the way the money is directed.
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   Will its success be judged on whether or not Australia avoids recession?
-  ----
-  
-  \josh-frydenberg
-  ----{.g}
-  [...] what we'll see is \d11~billion go into the economy
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest}
+  [...] what we'll see is $11~billion go into the economy
   before June the 30th [...].
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   But how do you judge the success of it, Treasurer?
-  ----
-  
-  \josh-frydenberg
-  ----{.g}
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest}
   Well, how I judge the success of it
   is that it supports the Australian economy,
   keeps people in a job
   and it keeps businesses in business [...].
-  ----
-  ----{.gt}
-  {In other words, if we do avoid recession,
+  --
+  --{.interview-guest .thought}
+  _In other words, if we do avoid recession,
   then I'll be judging it on the fact that we avoided recession,
   but if we don't avoid recession,
-  then I won't be judging it on the fact that we didn't avoid recession.}
-  ----
-  ----{.g}
+  then I won't be judging it on the fact that we didn't avoid recession._
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Last year you were claiming credit
   for a surplus that you'd not yet delivered,
   saying the budget was back in black.
   You were warned at the time
   that global events could derail that prospect,
   which they now have.
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   What does your premature rush to claim credit for a phantom surplus
   say about your political maturity and judgement?
-  ----
-  ----{.ht}
-  {There. Nailed him.}
-  ----
-  
-  \josh-frydenberg
-  ----{.gt}
-  {Ouch... Er, my premature rush did help us get re-elected,
+  --
+  --{.interview-host .thought}
+  _There. Nailed him._
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest .thought}
+  _Ouch... Er, my premature rush did help us get re-elected,
   so I think it says good things
-  about my political maturity and judgement?}
-  ----
-  ----{.g}
+  about my political maturity and judgement?_
+  --
+  --{.interview-guest}
   Well, we took the best possible advice
   [...] based on the forecasts at the time [...]
   we've now taken these major spending decisions
   and the surplus won't arrive in '19/20
   as a result of some of these circumstances we've seen.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   But Treasurer, you claimed credit
   for something that you hadn't yet delivered
   and I'm just wondering why you did that
   when at the time you were warned that events could derail it
   and they have?
-  ----
-  
-  \josh-frydenberg
-  ----{.g}
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest}
   Well, [...] we made our presentation
   based on the forecasts at the time,
   and clearly when it comes to
   the fires, [...] flood, [...] drought and [...] coronavirus,
   the economy has been hit by a number of external economic shocks
   that have been beyond our control.
-  ----
-  ----{.gt}
-  {My premature rush to take credit was also beyond my control.}
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _My premature rush to take credit was also beyond my control._
+  --
+  --{.interview-guest}
   When it comes to our response...
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   But, Treasurer, can I...
-  ----
-  
-  \josh-frydenberg
-  ----{.g}
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest}
   ... we can control that.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Sorry to interrupt you there.
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   You say, you know, these external shocks,
   you couldn't have seen them coming.
   Treasury budget papers did warn about this kind of thing.
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   I want to play our audience something
   from around the time of the budget last year.
-  ----
-  
-  \josh-frydenberg
-  ----{.gt}
-  {You're going to make me look like a proper dickhead now, right?}
-  ----
-  
-  \leigh-sales
-  ----{.ht}
-  {Yep.}
-  ----
-  
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest .thought}
+  _You're going to make me look like a proper dickhead now, right?_
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host .thought}
+  _Yep._
+  --
+
   """"
-    
-    ||||{.marker .start}
+
+    ||{.marker .start}
       (Excerpt from 7.30 interview on 2 Apr 2019)
-    ||||
-    
-    \leigh-sales-2019
-    ----{.h}
+    ||
+
+    ![Leigh Sales:][Leigh Sales 2019]
+    --{.interview-host}
     Like any projection,
-    your ability to deliver that \d7~billion surplus
+    your ability to deliver that $7~billion surplus
     is reliant on factors that are outside your control;
     it might not happen?
-    ----
-    
-    \josh-frydenberg-2019-730
-    ----{.g}
+    --
+
+    ![Josh Frydenberg:][Josh Frydenberg 2019 7.30]
+    --{.interview-guest}
     No, it *will* happen
     and it's the product of bringing the spending under control.
-    ----
-    
-    \END-excerpt
-    
+    --
+
+    ||{.marker .end}
+      END of Excerpt
+    ||
+
   """"
-  
+
   """"
-    
-    ||||{.marker .start}
+
+    ||{.marker .start}
       (Excerpt from Sky News interview on 2 Apr 2019)
-    ||||
-    
-    \david-speers-2019
-    ----{.h}
+    ||
+
+    ![David Speers:]
+    --{.interview-host}
     There might be a collapse in China;
     there might be another terrible drought;
     then you might have to write down the NBN.
     Things can happen, that could prevent you actually achieving this.
-    ----
-    
-    \josh-frydenberg-2019-sky
-    ----{.g}
+    --
+
+    ![Josh Frydenberg:][Josh Frydenberg 2019 Sky News]
+    --{.interview-guest}
     There's not a wafer thin surplus.
     This is a very significant surplus.
-    ----
-    
-    \END-excerpt
-    
+    --
+
+    ||{.marker .end}
+      END of Excerpt
+    ||
+
   """"
-  
-  \leigh-sales
-  ----{.h}
+
+  ![Leigh Sales:]
+  --{.interview-host}
   You did know things could happen.
-  ----
-  
-  \josh-frydenberg
-  ----{.g}
+  --
+
+  ![Josh Frydenberg:]
+  --{.interview-guest}
   Well, we maintain that those were the forecasts at the time.
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   But what we could not foresee [...]
   was this one-in-a-century event, namely the spread of the coronavirus,
   which is putting the shutters up on the global economy
-  ----
-  ----{.gt}
-  {and upon my precious surplus}.
-  ----
-  
-  \leigh-sales
-  ----{.ht}
-  {Oh cheer up now Josh, out of everyone I have on this show,
-  you talk over me the least.}
-  ----
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _and upon my precious surplus_.
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host .thought}
+  _Oh cheer up now Josh, out of everyone I have on this show,
+  you talk over me the least._
+  --
+  --{.interview-host}
   Treasurer, you had a very busy day today,
   you've got some busy ones ahead.
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   Thanks for making time to speak to our audience.
-  ----
-  
-  \josh-frydenberg
-  ----{.g}
-  Always good to be with you.
-  ----
-  
-  \END
-  
-||||||
+  --
 
-%footer-element
+  ![Josh Frydenberg:]
+  --{.interview-guest}
+  Always good to be with you.
+  --
+
+  ||{.marker .end}
+    END
+  ||
+
+||||
+
+
+%%footer
