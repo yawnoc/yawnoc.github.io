935b07b6a1f6846ec83bb4a99f8bd3ef48f1d3a0
Conway <yawnoc@pm.me>
2022-06-04 11:35:29 +0800

    Eliminate /code/unicode-math repetition with CMDv4

--- code/unicode-math.cmd
+++ code/unicode-math.cmd
@@ -12,7 +12,15 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 * %date-created --> 2019-03-30
 * %date-modified --> 2022-xx-xx
 
+RegexDictionaryReplacement: #.katex-and-katex-source-cells
+- queue_position: BEFORE #tables
+* [,]{2} [\s]+ (?P<katex_source> [^\n]+ )
+    -->
+  , $ \g<katex_source> $
+  , ` \g<katex_source> `
+
 %%%
+
 
 ^^^^
 - !home
@@ -62,48 +70,39 @@ Convenient for use in emails.
 |:
   //
     , °
-    , $  \degree  $
-    , `  \degree  `
+    ,, \degree
     , degree
   //
     , %
-    , $  \%  $
-    , `  \%  `
+    ,, \%
     , percent
   //
     , ∞
-    , $  \infty  $
-    , `  \infty  `
+    ,, \infty
     , infinity
   //
     , ½
-    , $  \frac{1}{2}  $
-    , `  \frac{1}{2}  `
+    ,, \frac{1}{2}
     , half
   //
     , ⅓ ⅔
-    , $  \frac{1}{3} \frac{2}{3}  $
-    , `  \frac{1}{3} \frac{2}{3}  `
+    ,, \frac{1}{3} \frac{2}{3}
     , thirds
   //
     , ¼ ¾
-    , $  \frac{1}{4} \frac{3}{4}  $
-    , `  \frac{1}{4} \frac{3}{4}  `
+    ,, \frac{1}{4} \frac{3}{4}
     , quarters
   //
     , ⅕ ⅖ ⅗ ⅘
-    , $  \frac{1}{5} \frac{2}{5} \frac{3}{5} \frac{4}{5}  $
-    , `  \frac{1}{5} \frac{2}{5} \frac{3}{5} \frac{4}{5}  `
+    ,, \frac{1}{5} \frac{2}{5} \frac{3}{5} \frac{4}{5}
     , fifths
   //
     , ⅙ ⅚
-    , $  \frac{1}{6} \frac{5}{6}  $
-    , `  \frac{1}{6} \frac{5}{6}  `
+    ,, \frac{1}{6} \frac{5}{6}
     , sixths
   //
     , ⅛ ⅜ ⅝ ⅞
-    , $  \frac{1}{8} \frac{3}{8} \frac{5}{8} \frac{7}{8}  $
-    , `  \frac{1}{8} \frac{3}{8} \frac{5}{8} \frac{7}{8}  `
+    ,, \frac{1}{8} \frac{3}{8} \frac{5}{8} \frac{7}{8}
     , eighths
 ''
 ||
@@ -122,13 +121,11 @@ Convenient for use in emails.
 |:
   //
     , ₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉
-    , $  {_0} {_1} {_2} {_3} {_4} {_5} {_6} {_7} {_8} {_9}  $
-    , `  {_0} {_1} {_2} {_3} {_4} {_5} {_6} {_7} {_8} {_9}  `
+    ,, {_0} {_1} {_2} {_3} {_4} {_5} {_6} {_7} {_8} {_9}
     , digits
   //
     , ₍ ₎ ₊ ₋ ₌
-    , $  {_(} {_)} {_+} {_-} {_=}  $
-    , `  {_(} {_)} {_+} {_-} {_=}  `
+    ,, {_(} {_)} {_+} {_-} {_=}
     , operators etc.
 ''
 ||
@@ -147,13 +144,11 @@ Convenient for use in emails.
 |:
   //
     , ⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹
-    , $  {^0} {^1} {^2} {^3} {^4} {^5} {^6} {^7} {^8} {^9}  $
-    , `  {^0} {^1} {^2} {^3} {^4} {^5} {^6} {^7} {^8} {^9}  `
+    ,, {^0} {^1} {^2} {^3} {^4} {^5} {^6} {^7} {^8} {^9}
     , digits
   //
     , ⁽ ⁾ ⁺ ⁻ ⁼
-    , $  {^(} {^)} {^+} {^-} {^=}  $
-    , `  {^(} {^)} {^+} {^-} {^=}  `
+    ,, {^(} {^)} {^+} {^-} {^=}
     , operators etc.
 ''
 ||
@@ -172,38 +167,31 @@ Convenient for use in emails.
 |:
   //
     , +
-    , $  +  $
-    , `  +  `
+    ,, +
     , plus
   //
     , −
-    , $  -  $
-    , `  -  `
+    ,, -
     , minus
   //
     , ±
-    , $  \pm  $
-    , `  \pm  `
+    ,, \pm
     , plus or minus
   //
     , ∓
-    , $  \mp  $
-    , `  \mp  `
+    ,, \mp
     , minus or plus
   //
     , ×
-    , $  \times  $
-    , `  \times  `
+    ,, \times
     , times
   //
     , ÷
-    , $  \div  $
-    , `  \div  `
+    ,, \div
     , divide
   //
     , ·
-    , $  \cdot  $
-    , `  \cdot  `
+    ,, \cdot
     , dot
 ''
 ||
@@ -222,63 +210,53 @@ Convenient for use in emails.
 |:
   //
     , =
-    , $  =  $
-    , `  =  `
+    ,, =
     , equals
   //
     , ≐
-    , $  \doteq  $
-    , `  \doteq  `
+    ,, \doteq
     , approximately equals
   //
     , ≈
-    , $  \approx  $
-    , `  \approx  `
+    ,, \approx
     , approximately
   //
     , ≃
-    , $  \simeq  $
-    , `  \simeq  `
+    ,, \simeq
     , asymptotically equals
   //
     , <`` ~ ``>
-    , $  \sim  $
-    , `  \sim  `
+    ,, \sim
     , asymptotically
   //
     , ≠
-    , $  \ne  $
-    , `  \ne  `
+    ,, \ne
     , does not equal
   //
     , \<
-    , $  <  $
-    , `  &lt;  `
+    , $<$
+    , `&lt;`
     , less than
   //
     , \>
-    , $  >  $
-    , `  &gt;  `
+    , $>$
+    , `&gt;`
     , greater than
   //
     , ≤
-    , $  \le  $
-    , `  \le  `
+    ,, \le
     , less than or equal to
   //
     , ≥
-    , $  \ge  $
-    , `  \ge  `
+    ,, \ge
     , greater than or equal to
   //
     , ≪
-    , $  \ll  $
-    , `  \ll  `
+    ,, \ll
     , much less than
   //
     , ≫
-    , $  \gg  $
-    , `  \gg  `
+    ,, \gg
     , much greater than
 ''
 ||
@@ -297,18 +275,15 @@ Convenient for use in emails.
 |:
   //
     , ∂
-    , $  \pd  $
-    , `  \pd  `
+    ,, \pd
     , partial differential
   //
     , ∇
-    , $  \del  $
-    , `  \del  `
+    ,, \del
     , del
   //
     , ∫
-    , $  \int  $
-    , `  \int  `
+    ,, \int
     , integral
 ''
 ||
@@ -334,123 +309,99 @@ see `macros` in [`rendering.js`].
 |:
   //
     , Α α
-    , $  \Alpha \alpha  $
-    , `  \Alpha \alpha  `
+    ,, \Alpha \alpha
     , alpha
   //
     , Β β
-    , $  \Beta \beta  $
-    , `  \Beta \beta  `
+    ,, \Beta \beta
     , beta
   //
     , Γ γ
-    , $  \Gamma \gamma  $
-    , `  \Gamma \gamma  `
+    ,, \Gamma \gamma
     , gamma
   //
     , Δ δ
-    , $  \Delta \delta  $
-    , `  \Delta \delta  `
+    ,, \Delta \delta
     , delta
   //
     , Ε ϵ ε
-    , $  \Epsilon \epsilon \varepsilon  $
-    , `  \Epsilon \epsilon \varepsilon  `
+    ,, \Epsilon \epsilon \varepsilon
     , epsilon
   //
     , Ζ ζ
-    , $  \Zeta \zeta  $
-    , `  \Zeta \zeta  `
+    ,, \Zeta \zeta
     , zeta
   //
     , Η η
-    , $  \Eta \eta  $
-    , `  \Eta \eta  `
+    ,, \Eta \eta
     , eta
   //
     , Θ θ
-    , $  \Theta \theta  $
-    , `  \Theta \theta  `
+    ,, \Theta \theta
     , theta
   //
     , Ι ι
-    , $  \Iota \iota  $
-    , `  \Iota \iota  `
+    ,, \Iota \iota
     , iota
   //
     , Κ κ
-    , $  \Kappa \kappa  $
-    , `  \Kappa \kappa  `
+    ,, \Kappa \kappa
     , kappa
   //
     , Λ λ
-    , $  \Lambda \lambda  $
-    , `  \Lambda \lambda  `
+    ,, \Lambda \lambda
     , lambda
   //
     , Μ μ
-    , $  \Mu \mu  $
-    , `  \Mu \mu  `
+    ,, \Mu \mu
     , mu
   //
     , Ν ν
-    , $  \Nu \nu  $
-    , `  \Nu \nu  `
+    ,, \Nu \nu
     , nu
   //
     , Ξ ξ
-    , $  \Xi \xi  $
-    , `  \Xi \xi  `
+    ,, \Xi \xi
     , xi
   //
     , Ο ο
-    , $  \Omicron \omicron  $
-    , `  \Omicron \omicron  `
+    ,, \Omicron \omicron
     , omicron
   //
     , Π π
-    , $  \Pi \pi  $
-    , `  \Pi \pi  `
+    ,, \Pi \pi
     , pi
   //
     , Ρ ρ
-    , $  \Rho \rho  $
-    , `  \Rho \rho  `
+    ,, \Rho \rho
     , rho
   //
     , Σ σ ς
-    , $  \Sigma \sigma \varsigma  $
-    , `  \Sigma \sigma \varsigma  `
+    ,, \Sigma \sigma \varsigma
     , sigma
   //
     , Τ τ
-    , $  \Tau \tau  $
-    , `  \Tau \tau  `
+    ,, \Tau \tau
     , tau
   //
     , Υ υ
-    , $  \Upsilon \upsilon  $
-    , `  \Upsilon \upsilon  `
+    ,, \Upsilon \upsilon
     , upsilon
   //
     , Φ ϕ φ
-    , $  \Phi \phi \varphi  $
-    , `  \Phi \phi \varphi  `
+    ,, \Phi \phi \varphi
     , phi
   //
     , Χ χ
-    , $  \Xi \xi  $
-    , `  \Xi \xi  `
+    ,, \Xi \xi
     , xi
   //
     , Ψ ψ
-    , $  \Psi \psi  $
-    , `  \Psi \psi  `
+    ,, \Psi \psi
     , psi
   //
     , Ω ω
-    , $  \Omega \omega  $
-    , `  \Omega \omega  `
+    ,, \Omega \omega
     , omega
 ''
 ||
