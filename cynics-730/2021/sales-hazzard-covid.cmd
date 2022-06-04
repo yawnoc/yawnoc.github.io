981a5db9d76fd97e4ac3ac377baba6491d75aff6
Conway <yawnoc@pm.me>
2022-06-04 20:40:19 +0800

    Convert sales-hazzard-covid to CMDv4

--- cynics-730/2021/sales-hazzard-covid.cmd
+++ cynics-730/2021/sales-hazzard-covid.cmd
@@ -1,217 +1,195 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/cynicism.cmdr
 
-{+ /resources/syntax/cynics-730.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title A cynic's 7.30: \short-title
-  %date-created 2021-06-18
-  %date-modified 2021-06-18
-  \resources
-  %footer-copyright-remark
-    except the [original interview][original],
-    whose copyright belongs to the ABC
-  %footer-remark
-    [:{Curly-bracketed words}:] inserted by me.
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %title -->
+    A cynic's 7.30: Brad Hazzard on the NSW COVID outbreak (17~Jun 2021)
+* %date-created --> 2021-06-18
+* %date-modified --> 2022-xx-xx
+- concluding_replacements: #.yawnoc.typography
 
-{: \short-title : Brad Hazzard on the NSW COVID outbreak (17 Jun 2021) :}
-
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:cynicism
-====]]
+%%%
 
 
-# %title #
+^^^^
+- !home
+- !top
+- !cynicism
+^^^^
 
 
-[||||
-[Original 7.30 interview][original] ([archived version])
-||||]
+# %title
 
-@@[original]
-  https://www.abc.net.au/7.30/\
-    nsw-covid-outbreak-started-by-an-unvaccinated/\
-    13396682
-@@
+--
+([Original 7.30 interview], [archived version])
+--
 
-@@[archived version]
-  https://web.archive.org/web/20210618053329/\
-    https://www.abc.net.au/7.30/\
-      nsw-covid-outbreak-started-by-an-unvaccinated/\
-      13396682
-@@
-
-##
-  NSW COVID outbreak started by an unvaccinated driver
-##
+[Original 7.30 interview]:
+  https://www.abc.net.au/7.30/nsw-covid-outbreak-started-by-an-unvaccinated/13396682
+[archived version]:
+  https://web.archive.org/web/20210618053329/https://www.abc.net.au/7.30/nsw-covid-outbreak-started-by-an-unvaccinated/13396682
 
 
-{: \leigh-sales : ![LEIGH SALES:] :}
-@@[leigh sales:]{.h}
-  leigh-sales-2021-06-17.jpg
-@@
+## NSW COVID outbreak started by an unvaccinated driver
 
-{: \brad-hazzard : ![BRAD HAZZARD:] :}
-@@[brad hazzard:]{.g}
-  brad-hazzard-2021-06-17.jpg
-@@
+
+[Leigh Sales:]{.interview-host}: leigh-sales-2021-06-17.jpg
+[Brad Hazzard:]{.interview-guest}: brad-hazzard-2021-06-17.jpg
+
 
 ||||{.interview}
-  
-  \leigh-sales
-  ----{.h}
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Minister, why was an unvaccinated driver allowed
   anywhere near an international flight crew?
-  ----
-  
-  \brad-hazzard
-  ----{.gt}
-  {But first, let me preface with a non-answer about saliva:}
-  ----
-  ----{.g}
+  --
+
+  ![Brad Hazzard:]
+  --{.interview-guest .thought}
+  _But first, let me preface with a non-answer about saliva:_
+  --
+  --{.interview-guest}
   The current arrangements [...] are that there should be,
   every day, a saliva test undertaken [etc. etc.].
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   It would appear that uh, he *may* have *had* the saliva test
   [blah blah etc. etc.].
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [Having vaccines] hasn't been mandatory to date, but at any rate,
   we've announced today that we'll be reviewing that situation
   because of the current circumstances.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   How come every time [...] the minister responsible,
   regardless of the state, says,
   "Oh yep, okay, we'll have to review it to make sure it doesn't happen again."
   Cause [vaccinated drivers and daily testing] seem really obvious [...].
   It's not rocket science.
-  ----
-  
-  \brad-hazzard
-  ----{.g}
+  --
+
+  ![Brad Hazzard:]
+  --{.interview-guest}
   Yeah uhh... thank you for that, Leigh,
   but the reality is that we are in a 1-in-100-year pandemic...
   and it's the "unknown unknowns".
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   There has been a reluctance through National Cabinet
   to have mandatory testing
-  ----
-  ----{.gt}
-  {so blame them}.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _so blame them_.
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Do you reckon that the audience would buy that an unvaccinated driver
   picking up an infection from an international flight crew
   is an "unknown unknown"?
-  ----
-  
-  \brad-hazzard
-  ----{.g}
+  --
+
+  ![Brad Hazzard:]
+  --{.interview-guest}
   Well, I just said it was...
-  ----
-  
-  \leigh-sales
-  ----{.ht}
-  {Yeah nah.}
-  ----
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host .thought}
+  _Yeah nah._
+  --
+  --{.interview-host}
   But how can that be?
-  ----
-  
-  \brad-hazzard
-  ----{.g}
+  --
+
+  ![Brad Hazzard:]
+  --{.interview-guest}
   Well... it *was*.
-  ----
-  ----{.gt}
-  {And it helps run down the clock in this interview.}
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _And it helps run down the clock in this interview._
+  --
+  --{.interview-guest}
   [...] There are other factors.
   [...] the companies that actually engage these drivers,
   often as employees or subcontractors
-  ----
-  ----{.gt}
-  {who are cheaper},
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _who are cheaper_,
+  --
+  --{.interview-guest}
   have expressed concerns [...]
   that they may not be able to get drivers to actually do the job
   if we actually force them to have the vaccine
-  ----
-  ----{.gt}
-  {unless we pay them more, which we won't}.
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _unless we pay them more, which we won't_.
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   New South Wales has a higher appetite for risk than other states [...].
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   What assurance can you give [...] that it isn't just luck
   that this is the first incident of its kind?
-  ----
-  
-  \brad-hazzard
-  ----{.g}
+  --
+
+  ![Brad Hazzard:]
+  --{.interview-guest}
   [...] We take the majority of people coming back from overseas
-  ----
-  ----{.gt}
-  {but don't you dare talk to me about NSW's larger population}.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _but don't you dare talk to me about NSW's larger population_.
+  --
+  --{.interview-guest}
   [...] we've taken over 190,000 people.
   That's about 10~times what WA's taken
-  ----
-  ----{.gt}
-  {which is just begging for a fact-check, because
-  [Dr~Tarun Weeramanthri's Interim Advice #1 from 4~Feb 2021][wa #1]
-  ([archived version][wa #1 archived])
-  says that """Over 37,000 people have been through the HQ system in WA"""}.
-  ----
-  @@[wa #1]
-    https://www.wa.gov.au/sites/default/files/2021-04/\
-      Review-of-Hotel-Quarantine-Arrangements-in-Western-Australia-\
-      Interim-Advice-1.pdf
-  @@
-  @@[wa #1 archived]
-    https://web.archive.org/web/20210618080052/\
-      https://www.wa.gov.au/sites/default/files/2021-04/\
-        Review-of-Hotel-Quarantine-Arrangements-in-Western-Australia-\
-        Interim-Advice-1.pdf
-  @@
-  
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _which is just begging for a fact-check, because
+  [Dr~Tarun Weeramanthri's Interim Advice~#1 from 4~Feb 2021]
+  says that ""Over 37,000 people have been through the HQ system in WA""_.
+  --
+
+  [Dr~Tarun Weeramanthri's Interim Advice~#1 from 4~Feb 2021]:
+    https://web.archive.org/web/20210618080052/https://www.wa.gov.au/sites/default/files/2021-04/Review-of-Hotel-Quarantine-Arrangements-in-Western-Australia-Interim-Advice-1.pdf
+
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \leigh-sales
-  ----{.h}
+  --
+
+  ![Leigh Sales:]
+  --{.interview-host}
   Brad Hazzard, thanks for your time.
-  ----
-  
-  \brad-hazzard
-  ----{.g}
+  --
+
+  ![Brad Hazzard:]
+  --{.interview-guest}
   Pleasure.
-  ----
-  
-  \END
-  
+  --
+
+  ||{.marker .end}
+    END
+  ||
+
 ||||
 
-%footer-element
+
+%%footer
