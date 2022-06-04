378d7713c913a995a9697756a904387b3c22ebdc
Conway <yawnoc@pm.me>
2022-06-04 15:32:29 +0800

    Convert bolt-liu-association to CMDv4

--- cynics-730/2019/bolt-liu-association.cmd
+++ cynics-730/2019/bolt-liu-association.cmd
@@ -1,224 +1,200 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/cynicism.cmdr
+< /resources/rules/language.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/cynics-730.cmd +}
-
-%%
-  %title A cynic's Bolt Report: \short-title
-  %date-created 2019-09-13
-  %date-modified 2020-02-16
-  \resources
-  %footer-copyright-remark
-    except the [original interview][original],
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: BEFORE #.cynicism.footer-properties-override
+* %copyright-prior-years --> get_year@%date-created--
+* %copyright-exception -->
+    , except the [original Bolt Report interview],
     whose copyright belongs to Sky News
-  %footer-remark
-    [:{Curly-bracketed words}:] inserted by me.
-%%
 
-{: \short-title : Gladys Liu on Chinese Associations (10 Sep 2019) :}
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %title -->
+    A cynic's Bolt Report: Gladys Liu on Chinese Associations (10~Sep 2019)
+* %date-created --> 2019-09-13
+* %date-modified --> 2022-xx-xx
+- concluding_replacements: #.yawnoc.typography
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:cynicism
-====]]
-
-
-# %title #
+%%%
 
 
-[||||
-[Original Bolt Report interview][original] ([youtube])
-||||]
-
-@@[original]
-  https://www.heraldsun.com.au/blogs/andrew-bolt/\
-    gladys-liu-car-crash-interview-on-her-links-to-chinas-dictators/\
-    news-story/2d00bf2a43cc0ca6047cc6873736d067
-@@
-
-@@[youtube]
-  https://youtu.be/qoy8mKRmALA
-@@
-
-||||{.important .statement}
-"I can't remember."
-||||
+^^^^
+- !home
+- !top
+^^^^
 
 
-## Gladys Liu discusses membership of various Chinese Associations. ##
+# %title
+
+--
+([Original Bolt Report interview], [youtube])
+--
+
+[Original Bolt Report interview]:
+  https://www.heraldsun.com.au/blogs/andrew-bolt/gladys-liu-car-crash-interview-on-her-links-to-chinas-dictators/news-story/2d00bf2a43cc0ca6047cc6873736d067
+[youtube]:
+  https://www.youtube.com/watch?v=qoy8mKRmALA
 
 
-{: \andrew-bolt : ![ANDREW BOLT:] :}
-@@[andrew bolt:]{.h}
-  andrew-bolt-2019-09-10.jpg
-@@
+## Gladys Liu discusses membership of various Chinese Associations.
 
-{: \gladys-liu : ![GLADYS LIU:] :}
-@@[gladys liu:]{.g}
-  gladys-liu-2019-09-10.jpg
-@@
+
+[Andrew Bolt:]{.interview-host}: andrew-bolt-2019-09-10.jpg
+[Gladys Liu:]{.interview-guest}: gladys-liu-2019-09-10.jpg
 
 
 ||||{.interview}
-  
-  \andrew-bolt
-  ----{.h}
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   Gladys, thank you so much for your time.
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   Can I just start: were you on the committee of
   two chapters of the China Overseas Exchange Association:
   one in Guangdong, from between 2003 and 2015, and the other in Shandong?
-  ----
-  
-  \gladys-liu
-  ----{.g}
+  --
+
+  ![Gladys Liu:]
+  --{.interview-guest}
   Well, good to be with you Andrew.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   I can tell you that, I cannot recall, as is reported,
   that from 2003 to 2015, twelve years long, that if I can't recall,
   I can't be an active member of that council, can I.
-  ----
-  ----{.gt}
-  {However, my memory shall be magically restored within the next 24~hours,
+  --
+  --{.interview-guest .thought}
+  _However, my memory shall be magically restored within the next 24~hours,
   and I shall release a [statement] tomorrow, Wednesday the 11th,
   confirming that I was a member of the
-  Guangdong Overseas Exchange Association in~2011.}
-  ----
-  @@[statement]
-    https://www.abc.net.au/news/2019-09-11/\
-      gladys-liu-admits-to-association-with-guangdong-association/\
-      11501088
-  @@
-  
-  \andrew-bolt
-  ----{.h}
+  Guangdong Overseas Exchange Association in~2011._
+  --
+  [statement]:
+    https://www.abc.net.au/news/2019-09-11/gladys-liu-admits-to-association-with-guangdong-association/11501088
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   How can you not recall a membership of twelve years? [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \andrew-bolt
-  ----{.h}
+  --
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   You've been Honorary President also,
   of the Australia Jiangmen General Commerce Association.
   It has backed China's theft of the South China Sea.
   Was your Association wrong to support that theft?
-  ----
-  
-  \gladys-liu
-  ----{.g}
+  --
+
+  ![Gladys Liu:]
+  --{.interview-guest}
   Well, as the Honorary President of the organisation,
   that they didn't tell me everything that they do
   [...] they want to use your fame,
   they want you to attend their functions,
   [...] to lift the profile of their organisation and their event.
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   Definitely, there was no information to me, what they do day to day,
   and I have no knowledge [...].
-  ----
-  ----{.gt}
-  {So there I was in an honorary leadersip role,
+  --
+  --{.interview-guest .thought}
+  _So there I was in an honorary leadersip role,
   happy to attend their functions and lift their profile,
   and content with them exploiting my name, in return for having
   absolutely no knowledge of the actual operations of the Association;
-  what the hell was I even doing there, and how naive/stupid can I be eh?}
-  ----
-  ----{.h}
+  what the hell was I even doing there, and how naive/stupid can I be eh?_
+  --
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \andrew-bolt
-  ----{.h}
+  --
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   [...] Can I just hear, you were also Honorary President of the
   United Chinese Commerce Association.
   Now there is evidence that this too is connected to the
-  [United Front (Work Department)](
+  [United Front (Work Department)]: true or not?
+  --
+  [United Front (Work Department)]:
     https://en.wikipedia.org/wiki/United_Front_Work_Department
-  ):
-  true or not?
-  ----
-  
-  \gladys-liu
-  ----{.g}
+
+  ![Gladys Liu:]
+  --{.interview-guest}
   What is that organisation again? I don't think I am. [...]
-  ----
-  \andrew-bolt
-  ----{.h}
+  --
+  ![Andrew Bolt:]
+  --{.interview-host}
   You have been Honorary President of the United Chinese Commerce Association
   and you've forgotten that as well.
-  ----
-  ----
+  --
+  --
   <small>
     __Conway's note.__
-    At the time of writing, Gladys Liu (廖嬋娥) is still listed
-    as Honorary Chairman (榮譽主席) on
-    [UCCA's website][ucca] ([archived version][ucca archived])
+    At the time of writing, Gladys Liu (.廖嬋娥) is still listed
+    as Honorary Chairman (.榮譽主席) on
+    [UCCA's website] ([archived version])
     along with her former role as
-    "Victorian Premier's Chinese Affairs Director" (維州州長華人事務主任).
+    "Victorian Premier's Chinese Affairs Director" (.維州州長華人事務主任).
   </small>
-  ----
-  
-  @@[ucca]
-    http://uccaai.org/
-  @@
-  
-  @@[ucca archived]
-    https://web.archive.org/web/20190912155936/http://uccaai.org/
-  @@
-  
-  ----{.g}
+  --
+
+  [UCCA's website]: http://uccaai.org/
+  [archived version]: https://web.archive.org/web/20190912155936/http://uccaai.org/
+
+  --{.interview-guest}
   [...]
-  ----
-  ----{.h}
+  --
+  --{.interview-host}
   [...]
-  ----
-  
-  \andrew-bolt
-  ----{.h}
+  --
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   Can you now tell us, in what way should Australia
   be more friendly to China? Can you give some examples?
-  ----
-  
-  \gladys-liu
-  ----{.g}
+  --
+
+  ![Gladys Liu:]
+  --{.interview-guest}
   ...
-  ----
-  ----{.gt}
-  {Handball:}
-  ----
-  ----{.g}
+  --
+  --{.interview-guest .thought}
+  _Handball:_
+  --
+  --{.interview-guest}
   Well... that is what our Foreign Minister has been doing very well,
   and she has been looking at the relationship between all countries,
   and she has been talking for the interests of Australia with other
   Foreign Ministers.
-  ----
-  ----{.gt}
-  {And by "the interests of Australia" I of course mean
-  "the interests of Australia's government".}
-  ----
-  ----{.h}
+  --
+  --{.interview-guest .thought}
+  _And by "the interests of Australia" I of course mean
+  "the interests of Australia's government"._
+  --
+  --{.interview-host}
   [...]
-  ----
-  ----{.g}
+  --
+  --{.interview-guest}
   [...]
-  ----
-  
-  \andrew-bolt
-  ----{.h}
+  --
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   Can I just end by just asking you a couple of things:
   Xi Jinping has made himself dictator for life;
   he is cracking down on dissidents, human rights activists;
@@ -226,107 +202,108 @@
   he has imprisoned Uyghurs in re-education camps;
   he's threatening Taiwan;
   do your consider Xi Jinping a dictator?
-  ----
-  
-  \gladys-liu
-  ----{.gt}
-  {Eep!}
-  ----
-  ----{.g}
+  --
+
+  ![Gladys Liu:]
+  --{.interview-guest .thought}
+  _Eep!_
+  --
+  --{.interview-guest}
   Umm... well honestly I have been focusing on serving the seat of Chisholm
   [...] I don't really know and I don't really...
-  ----
-  
-  \andrew-bolt
-  ----{.h}
+  --
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   Gladys, excuse me. You were born in Hong Kong;
   at the moment you have seen in your home place,
   people in their millions at times, 1.7 million, in their millions,
   protesting against what China's doing there,
   and you don't have any opinion on Xi Jinping?
-  ----
-  
-  \gladys-liu
-  ----{.g}
+  --
+
+  ![Gladys Liu:]
+  --{.interview-guest}
   OK. [...] In terms of Hong Kong,
   it is very very sad for anyone to see what has happened
-  ----
-  ----{.gt}
-  {which, depending on who you're talking to, translates to either
+  --
+  --{.interview-guest .thought}
+  _which, depending on who you're talking to, translates to either
   (a)~"it's sad, there is no hope for HK people anymore...
   2047 is really coming..."
   or
   (b)~"it's sad, this violence and smashing stuff up...
   but [those comrades who blew children up in 1967][1967]:
   they were heroes of the Glorious Motherland!
-  Do you hear me? Heroes!"}
-  ----
-  
-  @@[1967]
-    https://en.wikipedia.org/wiki/Hong_Kong_1967_leftist_riots
-  @@
-  
-  \andrew-bolt
-  ----{.h}
+  Do you hear me? Heroes!"_
+  --
+
+  [1967]: https://en.wikipedia.org/wiki/Hong_Kong_1967_leftist_riots
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   But Gladys, simple question: is Xi Jinping a dictator?
-  ----
-  
-  \gladys-liu
-  ----{.gt}
-  {Eep!!}
-  ----
-  ----{.g}
+  --
+
+  ![Gladys Liu:]
+  --{.interview-guest .thought}
+  _Eep!!_
+  --
+  --{.interview-guest}
   That is not a question for me to answer.
   But I can tell you about Hong Kong, it is very very sad to see...
-  ----
-  
-  \andrew-bolt
-  ----{.h}
+  --
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   I don't want to know about Hong Kong; I'm asking you about Xi Jinping,
   the dictator of Communist China, and you don't have an opinion about him?
-  ----
-  
-  \gladys-liu
-  ----{.g}
+  --
+
+  ![Gladys Liu:]
+  --{.interview-guest}
   I do have an opinion, but I'm not going to use the word "dictator"
-  ----
-  ----{.gt}
-  {because, having joined the Australian government,
+  --
+  --{.interview-guest .thought}
+  _because, having joined the Australian government,
   I am now obliged to cower in submission
   and not offend the Communist Party,
-  because money}.
-  ----
-  ----{.g}
+  because money_.
+  --
+  --{.interview-guest}
   He is, in their system, an elected Chairman, or President,
   they call it, for China.
-  ----
-  
-  \andrew-bolt
-  ----{.h}
+  --
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   Gladys Liu, are you, in effect, a spokesman
   for the Chinese Communist regime in Australia?
-  ----
-  
-  \gladys-liu
-  ----{.gt}
-  {Aren't we all, except Andrew Hastie and a few others?}
-  ----
-  ----{.g}
+  --
+
+  ![Gladys Liu:]
+  --{.interview-guest .thought}
+  _Aren't we all, except Andrew Hastie and a few others?_
+  --
+  --{.interview-guest}
   The simple answer is no. As I said...
-  ----
-  
-  \andrew-bolt
-  ----{.h}
+  --
+
+  ![Andrew Bolt:]
+  --{.interview-host}
   Gladys Liu, thank you so much indeed for your time.
-  ----
-  
-  \gladys-liu
-  ----{.g}
+  --
+
+  ![Gladys Liu:]
+  --{.interview-guest}
   Thank you Andrew.
-  ----
-  
-  \END
-  
+  --
+
+  ||{.marker .end}
+    END
+  ||
+
 ||||
 
-%footer-element
+
+%%footer
