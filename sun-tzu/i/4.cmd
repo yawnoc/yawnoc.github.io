cda9b15df454eb93e842cec61daa2b9b4fe8e961
Conway <yawnoc@pm.me>
2022-06-11 12:24:08 +0800

    Convert /sun-tzu/i/4 to CMDv4

--- sun-tzu/i/4.cmd
+++ sun-tzu/i/4.cmd
@@ -1,105 +1,104 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
+< /resources/rules/indices.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/superscripts.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~I" 《孫子算經\.卷上》 \P4
-  %date-created 2019-06-09
-  %date-modified 2022-02-20
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~I \P4"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~I" 《孫子算經卷上》 \P4
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-11
+- concluding_replacements:
+    #.yawnoc.double-quotes-in-attributes
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [I] ["i" Volume~I]
-*> [\P4](\- You are viewing \P4)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~I](./)
+* %%surrounds-previous --> [<-- \P3](3)
+* %%surrounds-current --> \P4
+* %%surrounds-next --> [\P5 -->](5)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
 
 
-#
-  《孫子算經\.卷上》 \+
-  "\sun-tzu's Computational Classic: Volume~I" \+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !i
+-{.breadcrumbed} !!4
+- !cite
+^^^^
+
+# .《孫子算經卷上》"(Sun Tz(uu)|孫子)'s Computational Classic: Volume~I" <br>
   \P4. Names of large numbers
-#
 
-{{{{|
-  \nav-up: Volume~I : ["i"]
-  {{|
-    \nav-prev: \P3 : ["i 3"]
-    \nav-curr: \P4
-    \nav-next: \P5 : ["i 5"]
-  |}}
-|}}}}
+%%noscript-equations
 
 
-\noscript:maths
+%%surrounds
+
+--
+This paragraph introduces a 'long scale' for named powers of ten.
+Note that the [previous paragraph (\P3)](3) uses a 'short scale'.
+--
 
 
-[||||
-||||]
+##{#translation} Translation
 
-
-----
-This paragraph introduces a "long scale" for __named powers of ten__.
-Note that the [previous paragraph (\P3)] ["i 3"] uses a "short scale".
-----
-
-
-##{#translation}
-  Translation
-##
-
-----
-Chinese source text: \a[6], \b[147], \c[52060], \d[13]. \+
-\d-default
-----
+--
+Chinese source text: \a[6], \b[147], \c[52060], \d[13]. <br>
+%%version-d-default
+--
 
 <<
-  凡大數之法、 \+
-  萬萬曰億、 \+
-  萬萬億曰兆、 \+
-  萬萬兆曰京、 \+
-  萬萬京曰陔、 \+
-  萬萬陔曰秭、 \+
-  萬萬秭曰壤、 \+
-  萬萬壤曰溝、 \+
-  萬萬溝曰澗、 \+
-  萬萬澗曰正、 \+
+  凡大數之法、 <br>
+  萬萬曰億、 <br>
+  萬萬億曰兆、 <br>
+  萬萬兆曰京、 <br>
+  萬萬京曰陔、 <br>
+  萬萬陔曰秭、 <br>
+  萬萬秭曰壤、 <br>
+  萬萬壤曰溝、 <br>
+  萬萬溝曰澗、 <br>
+  萬萬澗曰正、 <br>
   萬萬正曰載。
-||
-  .[In the] method of all great numbers: \+
-  .[a] myriad myriad .[is] called .[a] square-myriad; \+
-  .[a] myriad myriad square-myriad .[is] called .[a] multitude; \+
-  .[a] myriad myriad multitude .[is] called .[a] capital; \+
-  .[a] myriad myriad capital .[is] called .[a] terrace; \+
-  .[a] myriad myriad terrace .[is] called .[a] haystack; \+
-  .[a] myriad myriad haystack .[is] called .[a] soil; \+
-  .[a] myriad myriad soil .[is] called .[a] ditch; \+
-  .[a] myriad myriad ditch .[is] called .[a] stream; \+
-  .[a] myriad myriad stream .[is] called .[a] right; \+
+\\
+  .[In the] method of all great numbers: <br>
+  .[a] myriad myriad .[is] called .[a] square-myriad; <br>
+  .[a] myriad myriad square-myriad .[is] called .[a] multitude; <br>
+  .[a] myriad myriad multitude .[is] called .[a] capital; <br>
+  .[a] myriad myriad capital .[is] called .[a] terrace; <br>
+  .[a] myriad myriad terrace .[is] called .[a] haystack; <br>
+  .[a] myriad myriad haystack .[is] called .[a] soil; <br>
+  .[a] myriad myriad soil .[is] called .[a] ditch; <br>
+  .[a] myriad myriad ditch .[is] called .[a] stream; <br>
+  .[a] myriad myriad stream .[is] called .[a] right; <br>
   .[a] myriad myriad right .[is] called .[a] carry.
 >>
-====
-* \c[52060] erroneously has 萬萬澗日正 for 萬萬澗曰正.
-====
+=={.translation-annotations}
+* \c[52060] erroneously has .萬萬澗日正 for .萬萬澗曰正.
+==
 
 
-##{#commentary}
-  Extended commentary
-##
+##{#commentary} Extended commentary
 
-----
-Here we have a "long scale" where the named quantities go up
-by a factor of 10 ** 8:
-----
+--
+Here we have a 'long scale' where the named quantities go up
+by a factor of 10^8:
+--
 $$
 \begin{aligned}
   \unit{square-myriad~(億)} &= 10^8 \\
@@ -114,111 +113,103 @@ $$
   \unit{carry~(載)} &= 10^{80}.
 \end{aligned}
 $$
-----
-Recall that in the [previous paragraph (\P3)] ["i 3"],
-we had a "short scale" where the named quantities
+--
+Recall that in the [previous paragraph (\P3)](3),
+we had a 'short scale' where the named quantities
 only went up by a factor of 10.
-----
+--
 
-----
-In modern usage, a "medium scale" is used where
-the named quantities go up by a factor of 10 ** 4.
+--
+In modern usage, a 'medium scale' is used where
+the named quantities go up by a factor of 10^4.
 In summary:
-----
-
-||||||{.centred-block}
-||||{.overflowing}
+--
+||||{.centred}
+||{.wide}
 ''''
 |^
-  ==
+  //
     ; Quantity
     ; Rendering
-    ; [\P3] ["i 3"]
-    ; [\P4] ["i 4"]
+    ; [\P3](3)
+    ; [\P4](4)
     ; Modern
 |:
-  ==
-    , 億
+  //
+    ,{l=zh-Hant} 億
     , square-myriad
-    , 10 ** 8
-    , 10 ** 8
-    , 10 ** 8
-  ==
-    , 兆
+    , 10^8
+    , 10^8
+    , 10^8
+  //
+    ,{l=zh-Hant} 兆
     , multitude
-    , 10 ** 9
-    , 10 ** 16
-    , 10 ** 12
-  ==
-    , 京
+    , 10^9
+    , 10^16
+    , 10^12
+  //
+    ,{l=zh-Hant} 京
     , capital
-    , 10 ** 10
-    , 10 ** 24
-    , 10 ** 16
-  ==
-    , 陔
+    , 10^10
+    , 10^24
+    , 10^16
+  //
+    ,{l=zh-Hant} 陔
     , terrace
-    , 10 ** 11
-    , 10 ** 32
-    , 10 ** 20
-  ==
-    , 秭
+    , 10^11
+    , 10^32
+    , 10^20
+  //
+    ,{l=zh-Hant} 秭
     , haystack
-    , 10 ** 12
-    , 10 ** 40
-    , 10 ** 24
-  ==
-    , 壤
+    , 10^12
+    , 10^40
+    , 10^24
+  //
+    ,{l=zh-Hant} 壤
     , soil
-    , 10 ** 13
-    , 10 ** 48
-    , 10 ** 28
-  ==
-    , 溝
+    , 10^13
+    , 10^48
+    , 10^28
+  //
+    ,{l=zh-Hant} 溝
     , ditch
-    , 10 ** 14
-    , 10 ** 56
-    , 10 ** 32
-  ==
-    , 澗
+    , 10^14
+    , 10^56
+    , 10^32
+  //
+    ,{l=zh-Hant} 澗
     , stream
-    , 10 ** 15
-    , 10 ** 64
-    , 10 ** 36
-  ==
-    , 正
+    , 10^15
+    , 10^64
+    , 10^36
+  //
+    ,{l=zh-Hant} 正
     , right
-    , 10 ** 16
-    , 10 ** 72
-    , 10 ** 40
-  ==
-    , 載
+    , 10^16
+    , 10^72
+    , 10^40
+  //
+    ,{l=zh-Hant} 載
     , carry
-    , 10 ** 17
-    , 10 ** 80
-    , 10 ** 44
+    , 10^17
+    , 10^80
+    , 10^44
 ''''
+||
 ||||
-||||||
 
-----
-Note that 兆, "multitude", is also used for the SI Prefix mega~(10 ** 6).
+
+--
+Note that .兆, "multitude", is also used for the SI Prefix mega~(10^6).
 Confusing eh?
-----
+--
 
 
-{{{{|
-  \nav-up: Volume~I : ["i"]
-  {{|
-    \nav-prev: \P3 : ["i 3"]
-    \nav-curr: \P4
-    \nav-next: \P5 : ["i 5"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~I \P4"
-]
+%%cite
 
-%footer-element
+
+%%footer
