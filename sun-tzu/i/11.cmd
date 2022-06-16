9c48bd2e80b695a0f88f51a878e27c4e4e3d3b19
Conway <yawnoc@pm.me>
2022-06-16 20:37:38 +0800

    Convert /sun-tzu/i/11 to CMDv4

--- sun-tzu/i/11.cmd
+++ sun-tzu/i/11.cmd
@@ -1,102 +1,103 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~I" 《孫子算經\.卷上》 \P11
-  %date-created 2019-06-09
-  %date-modified 2020-05-13
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~I \P11"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~I" 《孫子算經卷上》 \P11
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-16
+- concluding_replacements:
+    #.yawnoc.double-quotes-in-attributes
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [I] ["i" Volume~I]
-*> [\P11](\- You are viewing \P11)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~I](./)
+* %%surrounds-previous --> [<-- \P10](10)
+* %%surrounds-current --> \P11
+* %%surrounds-next --> [\P12 -->](12)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
 
 
-#
-  《孫子算經\.卷上》 \+
-  "\sun-tzu's Computational Classic: Volume~I" \+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !i
+-{.breadcrumbed} !!11
+- !cite
+^^^^
+
+# .《孫子算經卷上》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~I" <br>
   \P11. Some equivalent fractions~(1)
-#
 
-{{{{|
-  \nav-up: Volume~I : ["i"]
-  {{|
-    \nav-prev: \P10 : ["i 10"]
-    \nav-curr: \P11
-    \nav-next: \P12 : ["i 12"]
-  |}}
-|}}}}
+%%noscript-equations
 
 
-\noscript:maths
+%%surrounds
 
-
-[||||
-||||]
-
-
-----
+--
 This paragraph gives some equivalent fractions.
-----
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \a[8], \b[148], \c[52067], \d[16]. \+
-\d-default
-----
-
-
+--
+Chinese source text: \a[8], \b[148], \c[52067], \d[16]. <br>
+%%version-d-default
+--
 
 <<
-  十分減一者，以二乘，二十除。 \+
-  減二者，以四乘，二十除。 \+
-  減三者，以六乘，二十除。 \+
-  減四者，以八乘，二十除。 \+
-  減五者，以十乘，二十除。 \+
-  減六者，以十二乘，二十除。 \+
-  減七者，以十四乘，二十除。 \+
-  減八者，以十六乘，二十除。 \+
+  十分減一者，以二乘，二十除。 <br>
+  減二者，以四乘，二十除。 <br>
+  減三者，以六乘，二十除。 <br>
+  減四者，以八乘，二十除。 <br>
+  減五者，以十乘，二十除。 <br>
+  減六者，以十二乘，二十除。 <br>
+  減七者，以十四乘，二十除。 <br>
+  減八者，以十六乘，二十除。 <br>
   減九者，以十八乘，二十除。
-||
+\\
   Subtracting one of ten shares:
-    multiply by two, .[and] divide .[by] twenty; \+
+    multiply by two, .[and] divide .[by] twenty; <br>
   subtracting two:
-    multiply by four, .[and] divide .[by] twenty; \+
+    multiply by four, .[and] divide .[by] twenty; <br>
   subtracting three:
-    multiply by six, .[and] divide .[by] twenty; \+
+    multiply by six, .[and] divide .[by] twenty; <br>
   subtracting four:
-    multiply by eight, .[and] divide .[by] twenty; \+
+    multiply by eight, .[and] divide .[by] twenty; <br>
   subtracting five:
-    multiply by ten, .[and] divide .[by] twenty; \+
+    multiply by ten, .[and] divide .[by] twenty; <br>
   subtracting six:
-    multiply by twelve, .[and] divide .[by] twenty; \+
+    multiply by twelve, .[and] divide .[by] twenty; <br>
   subtracting seven:
-    multiply by fourteen, .[and] divide .[by] twenty; \+
+    multiply by fourteen, .[and] divide .[by] twenty; <br>
   subtracting eight:
-    multiply by sixteen, .[and] divide .[by] twenty; \+
+    multiply by sixteen, .[and] divide .[by] twenty; <br>
   subtracting nine:
     multiply by eighteen, .[and] divide .[by] twenty.
 >>
-====
-* 分： shares; \or portions \+
+=={.translation-annotations}
+* .分： shares; \or portions
+  --
   In Chinese, the fraction $\dfrac{a}{b}$ is referred to
-  as <span lang="zh-Hant">$b$ ~~分之~~ $a$</span>,
+  as ::{l=zh-Hant} $b$ 分之 $a$::,
   literally "$a$ of $b$ shares".
+  --
 
 * I think this paragraph is saying
   $$
@@ -109,22 +110,14 @@ Chinese source text: \a[8], \b[148], \c[52067], \d[16]. \+
   $$
   but I have no clue as to why this might be useful,
   and why it is phrased in terms of subtraction.
-  Ditto for the [next paragraph (\P12)] ["i 12"].
-====
+  Ditto for the [next paragraph (\P12)](12).
+==
 
 
-{{{{|
-  \nav-up: Volume~I : ["i"]
-  {{|
-    \nav-prev: \P10 : ["i 10"]
-    \nav-curr: \P11
-    \nav-next: \P12 : ["i 12"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~I \P11"
-]
+%%cite
 
-%footer-element
+
+%%footer
