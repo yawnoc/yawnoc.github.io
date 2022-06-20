df5e4255d53d4cd4dea7a37b61e81070e5c5c571
Conway <yawnoc@pm.me>
2022-06-20 22:49:56 +0800

    Convert /sun-tzu/i/21 to CMDv4

--- sun-tzu/i/21.cmd
+++ sun-tzu/i/21.cmd
@@ -1,116 +1,97 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~I" 《孫子算經\.卷上》 \P21
-  %date-created 2019-06-09
-  %date-modified 2020-05-13
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~I \P21"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~I" 《孫子算經卷上》 \P21
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-20
+- concluding_replacements:
+    #.yawnoc.double-quotes-in-attributes
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [I] ["i" Volume~I]
-*> [\P21](\- You are viewing \P21)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~I](./)
+* %%surrounds-previous --> [<-- \P20](20)
+* %%surrounds-current --> \P21
+* %%surrounds-next --> [\P22 -->](22)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
 
-
-#
-  《孫子算經\.卷上》 \+
-  "\sun-tzu's Computational Classic: Volume~I" \+
-  \P21. Results of multiplication \& division: multiples of~3
-#
-
-{{{{|
-  \nav-up: Volume~I : ["i"]
-  {{|
-    \nav-prev: \P20 : ["i 20"]
-    \nav-curr: \P21
-    \nav-next: \P22 : ["i 22"]
-  |}}
-|}}}}
+%%%
 
 
-\noscript:maths
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !i
+-{.breadcrumbed} !!21
+- !cite
+^^^^
+
+# .《孫子算經卷上》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~I" <br>
+  \P21. Results of multiplication & division: multiples of~3
+
+%%noscript-equations
 
 
-[||||
-||||]
+%%surrounds
 
 
-----
-\P15 through \P22 give the results of multiplications and divisions
-of the form
-----
-$$
-  \begin{aligned}
-    (m n) \times (m n) &= m^2 n^2 \\
-    (m^2 n^2) \div m &= m n^2
-  \end{aligned}
-$$
-----
-along with the result of the division
-----
-$$
-  \Bigl[ n \times n + (n - 1) \times n + \dots + 1 \times n \Bigr] ^ 2
-    \div
-  n.
-$$
-----
-Since this gets rather repetitive and boring,
-you may wish to [skip to \P23] ["i 23"].
-----
-----
+%%volume-i-multiplications-and-divisions
+--
 This paragraph corresponds to $n = 3$.
-----
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \a[18], \b[151], \c[52112], \d[30]. \+
-\d-default
-----
+--
+Chinese source text: \a[18], \b[151], \c[52112], \d[30]. <br>
+%%version-d-default
+--
 
 <<
   三三如九，自相乘，得八十一。三人分之，人得二十七。
-||
+\\
   Three threes .[are] as nine,
   .[which], multiplied with itself,
   resulteth in eighty-one.
   .[With] three men sharing it,
   .[each] man receiveth twenty-seven.
 >>
-====
-* \a[18] erroneously has 得 for 人得.
-====
+=={.translation-annotations}
+* \a[18] erroneously has .得 for .人得.
+==
 
 <<
   二三如六，自相乘，得三十六。二人分之，人得一十八。
-||
+\\
   Two threes .[are] as six,
   .[which], multiplied with itself,
   resulteth in thirty-six.
   .[With] two men sharing it,
   .[each] man receiveth eighteen.
 >>
-====
-* \a[18] erroneously has 得 for 人得.
-====
+=={.translation-annotations}
+* \a[18] erroneously has .得 for .人得.
+==
 
 <<
   一三如三，自相乘，得九。一人得九。
-||
+\\
   One three .[is] as three,
   .[which], multiplied with itself,
   resulteth in nine.
@@ -119,7 +100,7 @@ Chinese source text: \a[18], \b[151], \c[52112], \d[30]. \+
 
 <<
   右三三一條，得一十八，自相乘，得三百二十四。三人分之，人得一百八。
-||
+\\
   .[The] three threes above .[in] one strand,
   result in eighteen,
   .[which], multiplied with itself,
@@ -127,7 +108,7 @@ Chinese source text: \a[18], \b[151], \c[52112], \d[30]. \+
   .[With] three men sharing it,
   .[each] man receiveth one hundred .[and] eight.
 >>
-====
+=={.translation-annotations}
 * In modern notation:
   $$
     \begin{gathered}
@@ -136,20 +117,13 @@ Chinese source text: \a[18], \b[151], \c[52112], \d[30]. \+
       \quad 324 \div 3 = 108.
     \end{gathered}
   $$
-====
-
-{{{{|
-  \nav-up: Volume~I : ["i"]
-  {{|
-    \nav-prev: \P20 : ["i 20"]
-    \nav-curr: \P21
-    \nav-next: \P22 : ["i 22"]
-  |}}
-|}}}}
+==
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~I \P21"
-]
+%%surrounds
 
-%footer-element
+
+%%cite
+
+
+%%footer
