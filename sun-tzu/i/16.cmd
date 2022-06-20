fdddf58453e575b1a75b0d7cc1955a1907f917cd
Conway <yawnoc@pm.me>
2022-06-20 21:18:00 +0800

    Convert /sun-tzu/i/16 to CMDv4

--- sun-tzu/i/16.cmd
+++ sun-tzu/i/16.cmd
@@ -1,113 +1,84 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~I" 《孫子算經\.卷上》 \P16
-  %date-created 2019-06-09
-  %date-modified 2020-05-13
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~I \P16"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~I" 《孫子算經卷上》 \P16
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-20
+- concluding_replacements:
+    #.yawnoc.double-quotes-in-attributes
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [I] ["i" Volume~I]
-*> [\P16](\- You are viewing \P16)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~I](./)
+* %%surrounds-previous --> [<-- \P15](15)
+* %%surrounds-current --> \P16
+* %%surrounds-next --> [\P17 -->](17)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
 
-
-#
-  《孫子算經\.卷上》 \+
-  "\sun-tzu's Computational Classic: Volume~I" \+
-  \P16. Results of multiplication \& division: multiples of~8
-#
-
-{{{{|
-  \nav-up: Volume~I : ["i"]
-  {{|
-    \nav-prev: \P15 : ["i 15"]
-    \nav-curr: \P16
-    \nav-next: \P17 : ["i 17"]
-  |}}
-|}}}}
+%%%
 
 
-\noscript:maths
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !i
+-{.breadcrumbed} !!16
+- !cite
+^^^^
+
+# .《孫子算經卷上》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~I" <br>
+  \P16. Results of multiplication & division: multiples of~8
+
+%%noscript-equations
 
 
-[||||
-||||]
+%%surrounds
 
 
-----
-\P15 through \P22 give the results of multiplications and divisions
-of the form
-----
-$$
-  \begin{aligned}
-    (m n) \times (m n) &= m^2 n^2 \\
-    (m^2 n^2) \div m &= m n^2
-  \end{aligned}
-$$
-----
-along with the result of the division
-----
-$$
-  \Bigl[ n \times n + (n - 1) \times n + \dots + 1 \times n \Bigr] ^ 2
-    \div
-  n.
-$$
-----
-Since this gets rather repetitive and boring,
-you may wish to [skip to \P23] ["i 23"].
-----
-----
+%%volume-i-multiplications-and-divisions
+--
 This paragraph corresponds to $n = 8$.
-----
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \b[149], \c[52082], \d[22]. \+
-\d-default \+
-This entire paragraph is missing from Version~A;
-it should be between [page/n12] and [page/n14].
-----
-
-@@[page/n12]
-  https://archive.org/details/02094034.cn/page/n12
-@@
-@@[page/n14]
-  https://archive.org/details/02094034.cn/page/n14
-@@
-
+--
+Chinese source text: \b[149], \c[52082], \d[22]. (Missing in Version~A.) <br>
+%%version-d-default
+--
 
 <<
   八八六十四，自相乘，得四千九十六。八人分之，人得五百十二。
-||
+\\
   Eight eights .[are] sixty-four,
   .[which], multiplied with itself,
   resulteth in four thousand .[and] ninety-six.
   .[With] eight men sharing it,
   .[each] man receiveth five hundred .[and] twelve.
 >>
-====
-* \b[149] has 五百一十二 for 五百十二.
-====
+=={.translation-annotations}
+* \b[149] has .五百一十二 for .五百十二.
+==
 
 <<
   七八五十六，自相乘，得三千一百三十六。七人分之，人得四百四十八。
-||
+\\
   Seven eights .[are] fifty-six,
   .[which], multiplied with itself,
   resulteth in three thousand, one hundred .[and] thirty-six.
@@ -117,7 +88,7 @@ it should be between [page/n12] and [page/n14].
 
 <<
   六八四十八，自相乘，得二千三百四。六人分之，人得三百八十四。
-||
+\\
   Six eights .[are] forty-eight,
   .[which], multiplied with itself,
   resulteth in two thousand, three hundred .[and] four.
@@ -127,7 +98,7 @@ it should be between [page/n12] and [page/n14].
 
 <<
   五八四十，自相乘，得一千六百。五人分之，人得三百二十。
-||
+\\
   Five eights .[are] forty,
   .[which], multiplied with itself,
   resulteth in one thousand six hundred.
@@ -137,7 +108,7 @@ it should be between [page/n12] and [page/n14].
 
 <<
   四八三十二，自相乘，得一千二十四。四人分之，人得二百五十六。
-||
+\\
   Four eights .[are] thirty-two,
   .[which], multiplied with itself,
   resulteth in one thousand .[and] twenty-four.
@@ -147,7 +118,7 @@ it should be between [page/n12] and [page/n14].
 
 <<
   三八二十四，自相乘，得五百七十六。三人分之，人得一百九十二。
-||
+\\
   Three eights .[are] twenty-four,
   .[which], multiplied with itself,
   resulteth in five hundred .[and] seventy-six.
@@ -157,7 +128,7 @@ it should be between [page/n12] and [page/n14].
 
 <<
   二八十六，自相乘，得二百五十六。二人分之，人得一百二十八。
-||
+\\
   Two eights .[are] sixteen,
   .[which], multiplied with itself,
   resulteth in two hundred .[and] fifty-six.
@@ -167,7 +138,7 @@ it should be between [page/n12] and [page/n14].
 
 <<
   一八如八，自相乘，得六十四。一人得六十四。
-||
+\\
   One eight .[is] as eight,
   .[which], multiplied with itself,
   resulteth in sixty-four.
@@ -177,7 +148,7 @@ it should be between [page/n12] and [page/n14].
 <<
   右八八一條，得二百八十八，自相乘，得八萬二千九百四十四。\
   八人分之，人得一萬三百六十八。
-||
+\\
   .[The] eight eights above .[in] one strand,
   result in two hundred .[and] eighty-eight,
   .[which], multiplied with itself,
@@ -185,7 +156,7 @@ it should be between [page/n12] and [page/n14].
   .[With] eight men sharing it,
   .[each] man receiveth one myriad, three hundred .[and] sixty-eight.
 >>
-====
+=={.translation-annotations}
 * In modern notation:
   $$
     \begin{gathered}
@@ -194,21 +165,13 @@ it should be between [page/n12] and [page/n14].
       \quad 82944 \div 8 = 10368.
     \end{gathered}
   $$
-====
+==
 
 
-{{{{|
-  \nav-up: Volume~I : ["i"]
-  {{|
-    \nav-prev: \P15 : ["i 15"]
-    \nav-curr: \P16
-    \nav-next: \P17 : ["i 17"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~I \P16"
-]
+%%cite
 
-%footer-element
+
+%%footer
