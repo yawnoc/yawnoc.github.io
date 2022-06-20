df78a4fdf3f011e6a4cc3052f67283ea9bfd8a6d
Conway <yawnoc@pm.me>
2022-06-20 23:10:02 +0800

    Convert /sun-tzu/i/23 to CMDv4

--- sun-tzu/i/23.cmd
+++ sun-tzu/i/23.cmd
@@ -1,101 +1,105 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~I" 《孫子算經\.卷上》 \P23
-  %date-created 2019-06-09
-  %date-modified 2020-05-13
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~I \P23"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~I" 《孫子算經卷上》 \P23
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-20
+- concluding_replacements:
+    #.yawnoc.double-quotes-in-attributes
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [I] ["i" Volume~I]
-*> [\P23](\- You are viewing \P23)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~I](./)
+* %%surrounds-previous --> [<-- \P22](22)
+* %%surrounds-current --> \P23
+* %%surrounds-next --> [\P24 -->](24)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
 
-
-#
-  《孫子算經\.卷上》 \+
-  "\sun-tzu's Computational Classic: Volume~I" \+
-  \P23. Results of multiplication \& division: multiples of~1,
-        and all multiples of~9 through~1 combined
-#
-
-{{{{|
-  \nav-up: Volume~I : ["i"]
-  {{|
-    \nav-prev: \P22 : ["i 22"]
-    \nav-curr: \P23
-    \nav-next: \P24 : ["i 24"]
-  |}}
-|}}}}
+%%%
 
 
-\noscript:maths
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !i
+-{.breadcrumbed} !!23
+- !cite
+^^^^
+
+# .《孫子算經卷上》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~I" <br>
+  \P23. Results of multiplication & division: multiples of~1,
+    and all multiples of~9 through~1 combined
+
+%%noscript-equations
 
 
-[||||
-||||]
+%%surrounds
 
 
-----
+--
 \P15 through \P22 gave the results of multiplications and divisions
 of the form
-----
+--
 $$
   \begin{aligned}
     (m n) \times (m n) &= m^2 n^2 \\
-    (m^2 n^2) \div m &= m n^2
+    (m^2 n^2) \div m &= m n^2,
   \end{aligned}
 $$
-----
+--
 along with the result of the division
-----
+--
 $$
-  \Bigl[ n \times n + (n - 1) \times n + \dots + 1 \times n \Bigr] ^ 2
+  \Bigl[ n \times n + (n - 1) \times n + \dots + 1 \times n \Bigr]^2
     \div
   n.
 $$
-----
+--
 This paragraph gives the multiplication corresponding to $m = n = 1$,
 before giving the sum of all single-digit products of the form $m \times n$,
 the result of which is used in its own example of multiplication and division.
-----
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \a[20], \b[151], \c[52117], \d[32]. \+
-\d-default
-----
+--
+Chinese source text: \a[20], \b[151], \c[52117], \d[32]. <br>
+%%version-d-default
+--
 
 <<
   一一如一，自相乘，得一。一乘不長。
-||
+\\
   One one .[is] as one, .[which], multiplied with itself, resulteth in one.
   Multiplying .[by] one augmenteth not.
 >>
-====
-* 長： augmenteth \+
-  長，上聲， \C ch(oe)ng2, \M zh(av)ng
-====
+=={.translation-annotations}
+* .長： augmenteth
+  --
+  .長，上聲， \C ch(oe)ng2, \M zh(av)ng
+  --
+==
 
 <<
   右從九九至一一，總成一千一百五十五，自相乘，得一百三十三萬四千二十五。\
   九人分之，人得一十四萬八千二百二十五。
-||
+\\
   From nine nines unto one one above:
   .[the] total becometh one thousand, one hundred .[and] fifty-five,
   .[which], multiplied with itself,
@@ -105,8 +109,8 @@ Chinese source text: \a[20], \b[151], \c[52117], \d[32]. \+
   .[each] man receiveth
     fourteen myriad, eight thousand, two hundred .[and] twenty-five.
 >>
-====
-* \c[52117] erroneously has 一百十三萬 for 一百三十三萬.
+=={.translation-annotations}
+* \c[52117] erroneously has .一百十三萬 for .一百三十三萬.
 
 * In modern notation:
   $$
@@ -124,21 +128,13 @@ Chinese source text: \a[20], \b[151], \c[52117], \d[32]. \+
       \end{aligned}
     \end{gathered}
   $$
-====
+==
 
 
-{{{{|
-  \nav-up: Volume~I : ["i"]
-  {{|
-    \nav-prev: \P22 : ["i 22"]
-    \nav-curr: \P23
-    \nav-next: \P24 : ["i 24"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~I \P23"
-]
+%%cite
 
-%footer-element
+
+%%footer
