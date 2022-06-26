4e751d7d66a4a2507a514ad0d9837d04ec1b4f8a
Conway <yawnoc@pm.me>
2022-06-26 18:18:53 +0800

    Convert /sun-tzu/ii/11 to CMDv4

--- sun-tzu/ii/11.cmd
+++ sun-tzu/ii/11.cmd
@@ -1,58 +1,60 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~II" 《孫子算經\.卷中》 \P11
-  %date-created 2019-06-09
-  %date-modified 2020-07-19
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~II \P11"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~II" 《孫子算經卷中》 \P11
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-26
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [II] ["ii" Volume~II]
-*> [\P11](\- You are viewing \P11)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~II](./)
+* %%surrounds-previous --> [<-- \P10](10)
+* %%surrounds-current --> \P11
+* %%surrounds-next --> [\P12 -->](12)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
 
-
-#
-  《孫子算經\.卷中》 \+
-  "\sun-tzu's Computational Classic: Volume~II" \+
-  \P11. Volume of a rectangular prism~(1)
-#
-
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P10 : ["ii 10"]
-    \nav-curr: \P11
-    \nav-next: \P12 : ["ii 12"]
-  |}}
-|}}}}
+%%%
 
 
-\noscript:maths
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !ii
+-{.breadcrumbed} !!11
+- !cite
+^^^^
+
+# .《孫子算經卷中》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~II" <br>
+  Volume of a rectangular prism~(1)
+
+%%noscript-equations
 
 
-[||||
-||||]
+%%surrounds
 
-
-----
+--
 This paragraph gives a worked example of computing
-the __volume of a rectangular prism__.
-----
-----
+the volume of a rectangular prism.
+--
+--
 The relevant unit conversions for length are
-----
+--
 $$
   \begin{aligned}
     1 \unit{rod~(丈)} &= 10 \unit{rules~(尺)} \\
@@ -60,44 +62,38 @@ $$
     1 \unit{inch~(寸)} &= 10 \unit{tenths~(分)},
   \end{aligned}
 $$
-----
+--
 and, for capacity,
-----
+--
 $$
   \begin{aligned}
     1 \unit{barrel~(斛)} &= 10 \unit{pecks~(斗)} \\
     1 \unit{peck~(斗)} &= 10 \unit{quarts~(升)}.
   \end{aligned}
 $$
-----
+--
 The conversion between capacity units and volume units is given by
-----
+--
 $$
   1 \unit{barrel~(斛)} = 1.62 \unit{rules~(尺)}^3.
 $$
-----
-See [Vol.~I \P1 (Units of length)] ["i 1"],
-[Vol.~I \P3 (Units of capacity)] ["i 3"],
-and [Vol.~II \P10 Extended commentary (capacity--volume conversion)] [斛法].
-----
-
-@@[斛法]
-  /sun-tzu/ii/10#commentary
-@@
+--
+See [Vol.~I \P1 (Units of length)](/sun-tzu/i/1),
+[Vol.~I \P3 (Units of capacity)](/sun-tzu/i/3),
+and [Vol.~II \P10 Extended commentary](/sun-tzu/ii/10#commentary).
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \a[34], \b[154], \c[52160], \d[46]. \+
-\d-default
-----
+--
+Chinese source text: \a[34], \b[154], \c[52160], \d[46]. <br>
+%%version-d-default
+--
 
 <<
   今有方窖，廣四丈六尺，長五丈四尺，深三丈五尺。問受粟幾何？
-||
+\\
   Now there be .[a] rectangular cellar,
   of width four rods .[and] six rules,
   length five rods .[and] four rules,
@@ -107,18 +103,18 @@ Chinese source text: \a[34], \b[154], \c[52160], \d[46]. \+
 
 <<
   答曰：五萬三千六百六十六斛六斗六升三分升之二。
-||
+\\
   Answer saith:
     five myriad, three thousand, six hundred .[and] sixty-six barrels,
     six pecks, six quarts, .[and] two thirds of .[a] quart.
 >>
-====
-* \a[34] and \d[46] erroneously have 三分升之一 for 三分升之二.
-====
+=={.translation-annotations}
+* \a[34] and \d[46] erroneously have .三分升之一 for .三分升之二.
+==
 
 <<
   術曰：置廣四丈六尺、長五丈四尺，相乘，得二千四百八十四尺。
-||
+\\
   Method saith:
   put .[down the] width, four rods .[and] six rules,
   .[and the] length, five rods .[and] four rules,
@@ -128,7 +124,7 @@ Chinese source text: \a[34], \b[154], \c[52160], \d[46]. \+
 
 <<
   以深三丈五尺乘之，得八萬六千九百四十尺。
-||
+\\
   Multiplying it by .[the] depth, three rods .[and] five rules,
   resulteth in
     eight myriad, six thousand, nine hundred .[and] forty rules.
@@ -136,17 +132,20 @@ Chinese source text: \a[34], \b[154], \c[52160], \d[46]. \+
 
 <<
   以斛法一尺六寸二分除之，即得。
-||
+\\
   Dividing it by .[the] barrel divisor,
     one rule, six inches, .[and] two tenths,
   .[we] are done.
 >>
-====
-* 斛法： .[the] barrel divisor \+
+=={.translation-annotations}
+* .斛法： .[the] barrel divisor
+  --
   This is the conversion between the capacity unit "barrel",
-  ((hu | ~~斛~~))~(斛),
-  and the volume unit "cubic rule", ((ch'ih | ~~尺~~))~(尺);
-  see [\P10 Extended commentary] [斛法].
+    (hu|斛)~(.斛),
+  and the volume unit "cubic rule",
+    (ch'ih|尺)~(.尺);
+  see [\P10 Extended commentary](10#commentary).
+  --
 
 * In modern notation, the volume (or capacity) of a rectangular prism of
   width $W = 46 \unit{rules}$,
@@ -165,21 +164,13 @@ Chinese source text: \a[34], \b[154], \c[52160], \d[46]. \+
       &= 53666 \unit{barrels} + 6 \unit{pecks} + 6 \tfrac{2}{3} \unit{quarts}.
     \end{aligned}
   $$
-====
+==
 
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P10 : ["ii 10"]
-    \nav-curr: \P11
-    \nav-next: \P12 : ["ii 12"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~II \P11"
-]
+%%cite
 
-%footer-element
+
+%%footer
