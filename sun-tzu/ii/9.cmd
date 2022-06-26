77f2c5d02e84aa1571c314e5a496cf7e73081427
Conway <yawnoc@pm.me>
2022-06-26 16:53:23 +0800

    Convert /sun-tzu/ii/9 to CMDv4

--- sun-tzu/ii/9.cmd
+++ sun-tzu/ii/9.cmd
@@ -1,111 +1,115 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~II" 《孫子算經\.卷中》 \P9
-  %date-created 2019-06-09
-  %date-modified 2020-07-19
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~II \P9"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~II" 《孫子算經卷中》 \P9
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-26
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [II] ["ii" Volume~II]
-*> [\P9](\- You are viewing \P9)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~II](./)
+* %%surrounds-previous --> [<-- \P8](8)
+* %%surrounds-current --> \P9
+* %%surrounds-next --> [\P10 -->](10)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
 
 
-#
-  《孫子算經\.卷中》 \+
-  "\sun-tzu's Computational Classic: Volume~II" \+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !ii
+-{.breadcrumbed} !!9
+- !cite
+^^^^
+
+# .《孫子算經卷中》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~II" <br>
   \P9. Area of a rectangle
-#
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P8 : ["ii 8"]
-    \nav-curr: \P9
-    \nav-next: \P10 : ["ii 10"]
-  |}}
-|}}}}
+%%noscript-equations
 
 
-\noscript:maths
+%%surrounds
 
-
-[||||
-||||]
-
-
-----
-This paragraph gives a worked example of computing the __area of a rectangle__.
-----
-----
+--
+This paragraph gives a worked example of computing the area of a rectangle.
+--
+--
 The relevant unit conversion for length is
-----
+--
 $$
   1 \unit{rod~(丈)} = 10 \unit{rules~(尺)}.
 $$
-----
-See [Vol.~I \P1 (Units of length)] ["i 1"].
-----
+--
+See [Vol.~I \P1 (Units of length)](/sun-tzu/i/1).
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \a[32], \b[154], \c[52154], \d[44]. \+
-\d-default
-----
+--
+Chinese source text: \a[32], \b[154], \c[52154], \d[44]. <br>
+%%version-d-default
+--
 
 <<
   今有屋基，南北三丈，東西六丈，欲以甎砌之。凡積二尺，用甎五枚。問計幾何？
-||
+\\
   Now there be .[an] house foundation,
   three rods north-.[to]-south, .[and] six rods east-.[to]-west,
   .[and we] wish to build it with bricks.
   Every two rules of area, useth five bricks.
   .[We] ask, how many amounteth .[this] to?
 >>
-====
-* 南北： north-.[to]-south; \lit south-.[to]-north
+=={.translation-annotations}
+* .南北： north-.[to]-south; \lit south-.[to]-north
 
-* \c[52154] erroneously has ●~`U+25CF` for both instances of 甎.
+* \c[52154] erroneously has ●~`U+25CF` for both instances of .甎.
 
-* 枚 \+
+* .枚
+  --
   Classifier for bricks, omitted in the English.
-====
+  --
+==
 
 <<
   答曰：四千五百枚。
-||
+\\
   Answer saith: four thousand five hundred .[bricks].
 >>
 
 <<
   術曰：置東西六丈，以南北三丈乘之，得一千八百尺。\
   以五乘之，得九千尺。以二除之，即得。
-||
+\\
   Method saith: put .[down the] six rods east-.[to]-west.
   Multiplying it by .[the] three rods north-.[to]-south,
   resulteth in one thousand eight hundred rules.
   Multiplying it by five, resulteth in nine thousand rules.
   Dividing it by two, .[we] are done.
 >>
-====
-* 九千尺： nine thousand rules \+
+=={.translation-annotations}
+* .九千尺： nine thousand rules
+  --
   This is area, but square units were simply referred to as units.
+  --
 
 * In modern notation, the number of bricks required to fill
   a rectangle of length $L = 3 \unit{rods}$
@@ -123,21 +127,13 @@ Chinese source text: \a[32], \b[154], \c[52154], \d[44]. \+
       &= 4500.
     \end{aligned}
   $$
-====
+==
 
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P8 : ["ii 8"]
-    \nav-curr: \P9
-    \nav-next: \P10 : ["ii 10"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~II \P9"
-]
+%%cite
 
-%footer-element
+
+%%footer
