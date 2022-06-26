5dd158228d63e1a78fd11582e8527b1e785e1a85
Conway <yawnoc@pm.me>
2022-06-26 16:32:07 +0800

    Convert /sun-tzu/ii/8 to CMDv4

--- sun-tzu/ii/8.cmd
+++ sun-tzu/ii/8.cmd
@@ -1,58 +1,60 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~II" 《孫子算經\.卷中》 \P8
-  %date-created 2019-06-09
-  %date-modified 2020-07-19
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~II \P8"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~II" 《孫子算經卷中》 \P8
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-26
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [II] ["ii" Volume~II]
-*> [\P8](\- You are viewing \P8)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~II](./)
+* %%surrounds-previous --> [<-- \P7](7)
+* %%surrounds-current --> \P8
+* %%surrounds-next --> [\P9 -->](9)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
 
 
-#
-  《孫子算經\.卷中》 \+
-  "\sun-tzu's Computational Classic: Volume~II" \+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !ii
+-{.breadcrumbed} !!8
+- !cite
+^^^^
+
+# .《孫子算經卷中》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~II" <br>
   \P8. Volume conversions for grain~(4)
-#
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P7 : ["ii 7"]
-    \nav-curr: \P8
-    \nav-next: \P9 : ["ii 9"]
-  |}}
-|}}}}
+%%noscript-equations
 
 
-\noscript:maths
+%%surrounds
 
-
-[||||
-||||]
-
-
-----
+--
 This paragraph gives a worked example of a volume conversion for trading grain
-using __cross-multiplication__.
-----
-----
+using cross-multiplication.
+--
+--
 The relevant unit conversions for capacity are
-----
+--
 $$
   \begin{aligned}
     1 \unit{peck~(斗)} &= 10 \unit{quarts~(升)} \\
@@ -60,45 +62,43 @@ $$
     1 \unit{gill~(合)} &= 10 \unit{ladles~(勺)}.
   \end{aligned}
 $$
-----
-See [Vol.~I \P3 (Units of capacity)] ["i 3"].
-----
+--
+See [Vol.~I \P3 (Units of capacity)](/sun-tzu/i/3).
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \a[30], \b[154], \c[52151], \d[44]. \+
-\d-default
-----
+--
+Chinese source text: \a[30], \b[154], \c[52151], \d[44]. <br>
+%%version-d-default
+--
 
 <<
-  今有粟七斗九升。問為御米幾何？ \+
+  今有粟七斗九升。問為御米幾何？ <br>
   答曰：三斗三升一合八勺。
-||
+\\
   Now there be grain, seven pecks .[and] nine quarts.
-  .[We] ask, how much be .[this in] poppy seed? \+
+  .[We] ask, how much be .[this in] poppy seed? <br>
   Answer saith: three pecks, three quarts, one gill, .[and] eight ladles.
 >>
-====
-* \c[52148] erroneously has 栗 for 粟.
+=={.translation-annotations}
+* \c[52148] erroneously has .栗 for .粟.
 
-* 御米： poppy seed; \lit imperial grain; \or royal grain
-====
+* .御米： poppy seed; \lit imperial grain; \or royal grain
+==
 
 <<
   術曰：置七斗九升。\
   以御米率二十一乘之，得一千六百五十九升為實。以粟率五十除之，即得。
-||
+\\
   Method saith: put .[down the] seven pecks .[and] nine quarts.
   Multiplying it by .[the] poppy seed rate, twenty-one,
   resulteth in one thousand, six hundred .[and] fifty-nine quarts
   as .[the] dividend.
   Dividing it by .[the] grain rate, fifty, .[we] are done.
 >>
-====
+=={.translation-annotations}
 * In modern notation:
   $$
     \begin{aligned}
@@ -115,23 +115,15 @@ Chinese source text: \a[30], \b[154], \c[52151], \d[44]. \+
     \end{aligned}
   $$
   The rates 21~for poppy seed and 50~for (regular) grain appear to come from
-  《九章算術\.粟米》, "Nine Chapters .[on] Computational Methods: Grain".
-  See [\P5 commentary](/sun-tzu/ii/5#commentary).
-====
+  .《九章算術粟米》, "Nine Chapters .[on] Computational Methods: Grain".
+  See [\P5 commentary](5#commentary).
+==
 
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P7 : ["ii 7"]
-    \nav-curr: \P8
-    \nav-next: \P9 : ["ii 9"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~II \P8"
-]
+%%cite
 
-%footer-element
+
+%%footer
