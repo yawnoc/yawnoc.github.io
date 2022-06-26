6e4f3ab6ef1df2801f22d561f2eb28c05c10b496
Conway <yawnoc@pm.me>
2022-06-26 11:30:50 +0800

    Convert /sun-tzu/ii/3 to CMDv4

--- sun-tzu/ii/3.cmd
+++ sun-tzu/ii/3.cmd
@@ -1,86 +1,88 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~II" 《孫子算經\.卷中》 \P3
-  %date-created 2019-06-09
-  %date-modified 2020-07-19
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~II \P3"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~II" 《孫子算經卷中》 \P3
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-23
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [II] ["ii" Volume~II]
-*> [\P3](\- You are viewing \P3)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~II](./)
+* %%surrounds-previous --> [<-- \P2](2)
+* %%surrounds-current --> \P3
+* %%surrounds-next --> [\P4 -->](4)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
 
 
-#
-  《孫子算經\.卷中》 \+
-  "\sun-tzu's Computational Classic: Volume~II" \+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !ii
+-{.breadcrumbed} !!3
+- !cite
+^^^^
+
+# .《孫子算經卷中》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~II" <br>
   \P3. Subtracting fractions
-#
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P2 : ["ii 2"]
-    \nav-curr: \P3
-    \nav-next: \P4 : ["ii 4"]
-  |}}
-|}}}}
+%%noscript-equations
 
 
-\noscript:maths
+%%surrounds
 
-
-[||||
-||||]
-
-
-----
+--
 This paragraph gives a worked example of subtracting fractions.
-----
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \a[28], \b[153], \c[52136], \d[40]. \+
-\d-default
-----
+--
+Chinese source text: \a[28], \b[153], \c[52136], \d[40]. <br>
+%%version-d-default
+--
 
 <<
-  今有九分之八，減其五分之一。問餘幾何？ \+
+  今有九分之八，減其五分之一。問餘幾何？ <br>
   答曰：四十五分之三十一。
-||
+\\
   Now there be eight of nine shares,
   .[and we] diminish of it one of five shares.
-  .[We] ask, how much remaineth? \+
+  .[We] ask, how much remaineth? <br>
   Answer saith: thirty-one of forty-five shares.
 >>
-====
-* <span lang="zh-Hant">$b$ ~~分之~~ $a$</span>:
-    \lit $a$ of $b$ shares \+
+=={.translation-annotations}
+* ::{l=zh-Hant} $b$ 分之 $a$:::
+    \lit $a$ of $b$ shares
+  --
   I have rendered fractions literally, since there are references to
-  the numerator <span lang="zh-Hant">~~「之~~ $a$~~」~~</span>, "$a$ of", and
-  the denominator <span lang="zh-Hant">~~「~~$b$ ~~分」~~</span>, "$b$ shares",
+  the numerator "::{l=zh-Hant} 之 $a$::" ("$a$ of"), and
+  the denominator "::{l=zh-Hant} $b$ 分::" ("$b$ shares"),
   in the lines to follow.
-====
+  --
+==
 
 <<
   術曰：置九分、五分在右方，之八、之一在左方。
-||
+\\
   Method saith: put .[the] nine shares .[and the] five shares on .[the] right;
   .[the] eight of .[and the] one of, on .[the] left.
 >>
@@ -88,7 +90,7 @@ Chinese source text: \a[28], \b[153], \c[52136], \d[40]. \+
 <<
   母互乘子，五分之一得九，九分之八得四十，以少減多，餘三十一為實。\
   母相乘得四十五為法。
-||
+\\
   .[The] denominators .[are] mutually multiplied with .[the] numerators:
   .[for the] one of five shares .[there] resulteth nine,
   .[and for the] eight of nine shares .[there] resulteth forty;
@@ -97,8 +99,8 @@ Chinese source text: \a[28], \b[153], \c[52136], \d[40]. \+
   .[The] denominators multiplied with each other,
   result in forty-five as .[the] divisor.
 >>
-====
-* \c[52136] erroneously has 王十一 for 三十一.
+=={.translation-annotations}
+* \c[52136] erroneously has .王十一 for .三十一.
 
 * In modern notation:
   $$
@@ -106,28 +108,20 @@ Chinese source text: \a[28], \b[153], \c[52136], \d[40]. \+
     = \frac{8 \times 5 - 1 \times 9}{9 \times 5}
     = \frac{31}{45}.
   $$
-====
+==
 
 <<
   不滿法，以法命之，即得。
-||
+\\
   .[The dividend] reacheth not .[the] divisor;
   naming it .[for a fraction] using .[the] divisor, .[we] are done.
 >>
 
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P2 : ["ii 2"]
-    \nav-curr: \P3
-    \nav-next: \P4 : ["ii 4"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~II \P3"
-]
+%%cite
 
-%footer-element
+
+%%footer
