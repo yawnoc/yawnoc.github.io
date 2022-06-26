651e6bc3f37ceb27cc290c47f33d29afe9ea44db
Conway <yawnoc@pm.me>
2022-06-26 12:01:36 +0800

    Convert /sun-tzu/ii/4 to CMDv4

--- sun-tzu/ii/4.cmd
+++ sun-tzu/ii/4.cmd
@@ -1,97 +1,96 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/coloured-spans.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~II" 《孫子算經\.卷中》 \P4
-  %date-created 2019-06-09
-  %date-modified 2020-08-31
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~II \P4"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~II" 《孫子算經卷中》 \P4
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-26
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [II] ["ii" Volume~II]
-*> [\P4](\- You are viewing \P4)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~II](./)
+* %%surrounds-previous --> [<-- \P3](3)
+* %%surrounds-current --> \P4
+* %%surrounds-next --> [\P5 -->](5)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
 
 
-#
-  《孫子算經\.卷中》 \+
-  "\sun-tzu's Computational Classic: Volume~II" \+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !ii
+-{.breadcrumbed} !!4
+- !cite
+^^^^
+
+# .《孫子算經卷中》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~II" <br>
   \P4. Redistributing fractions for equality
-#
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P3 : ["ii 3"]
-    \nav-curr: \P4
-    \nav-next: \P5 : ["ii 5"]
-  |}}
-|}}}}
+%%noscript-equations
 
 
-\noscript:maths
+%%surrounds
 
-
-[||||
-||||]
-
-
-----
+--
 This paragraph gives a worked example of
-__redistributing__ three __fractions for equality__.
-----
+redistributing three fractions for equality.
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \a[28], \b[153], \c[52139], \d[41]. \+
-\d-default
-----
+--
+Chinese source text: \a[28], \b[153], \c[52139], \d[41]. <br>
+%%version-d-default
+--
 
 <<
   今有三分之一、三分之二、四分之三。問減多益少，幾何而平？
-||
+\\
   Now there be one of three shares,
   two of three shares, .[and] three of four shares.
   .[We] ask, .[in] diminishing .[the] greater .[to] benefit .[the] lesser,
   how much that .[they be] level?
 >>
-====
-* <span lang="zh-Hant">$b$ ~~分之~~ $a$</span>:
-    \lit $a$ of $b$ shares \+
+=={.translation-annotations}
+* ::{l=zh-Hant} $b$ 分之 $a$:::
+    \lit $a$ of $b$ shares
+  --
   I have rendered fractions literally, since there are references to
-  the numerator <span lang="zh-Hant">~~「之~~ $a$~~」~~</span>, "$a$ of", and
-  the denominator <span lang="zh-Hant">~~「~~$b$ ~~分」~~</span>, "$b$ shares",
+  the numerator "::{l=zh-Hant} 之 $a$::" ("$a$ of"), and
+  the denominator "::{l=zh-Hant} $b$ 分::" ("$b$ shares"),
   in the lines to follow.
-
-* 而平： that .[they be] level; \or that .[it be] fair
-====
+  --
+==
 
 <<
-  答曰：減四分之三者[r/二/]，三分之二者[v/一/]，\
-  [g/并/]以益三分之一，而各[b/平於一十二分之七/]。
-||
+  答曰：減四分之三者::{.colour-r}二::，三分之二者::{.colour-v}一::，\
+  ::{.colour-g}并::以益三分之一，而各::{.colour-b}平於一十二分之七::。
+\\
   Answer saith:
-  that diminished of .[the] three of four shares, .[is] [r/ two /];
-  that of .[the] two of three shares, [v/ one /];
-  [g/ combined /] to benefit .[the] one of three shares,
-  that each .[be] [b/ level at seven of twelve shares /].
+  that diminished of .[the] three of four shares, .[is] ::{.colour-r} two::;
+  that of .[the] two of three shares, ::{.colour-v} one::;
+  ::{.colour-g} combined:: to benefit .[the] one of three shares,
+  that each .[be] ::{.colour-b} level at seven of twelve shares::.
 >>
-====
+=={.translation-annotations}
 * In modern notation:
   $$
     \frac{3}{4} - \colr{\frac{2}{12}}
@@ -100,7 +99,7 @@ Chinese source text: \a[28], \b[153], \c[52139], \d[41]. \+
     = \colb{\frac{7}{12}}.
   $$
 
-* To see why \sun-tzu's algorithm works,
+* To see why (Sun Tz(uu)|孫子)'s algorithm works,
   it is helpful to first work through the problem once ourselves.
   More generally, consider redistributing the fractions
   $a/A$, $b/B$ and $c/C$ for equality.
@@ -118,13 +117,13 @@ Chinese source text: \a[28], \b[153], \c[52139], \d[41]. \+
     = \frac{L - (3 aBC, 3 bCA, 3 cAB)}{3 ABC}
   $$
   respectively.
-  Of course \sun-tzu did not have the luxury of modern mathematical notation:
-====
+  Of course (Sun Tz(uu)|孫子) did not have the luxury of modern mathematical notation:
+==
 
 <<
   術曰：置三分、三分、四分在右方，之一、之二、之三在左方。\
   母互乘子，副并得六十三，置右為平實。
-||
+\\
   Method saith: put
   .[the] three shares, .[the] three shares .[and the] four shares
     on .[the] right;
@@ -133,11 +132,13 @@ Chinese source text: \a[28], \b[153], \c[52139], \d[41]. \+
   combined subsidiarily, result in sixty-three,
   put to .[the] right as .[the] level dividend.
 >>
-====
+=={.translation-annotations}
 * \c[52139] erroneously has 母互乘于 for 母互乘子.
 
-* 平實： level dividend \+
+* 平實： level dividend
+  --
   $L = aBC + bCA + cAB = 63$.
+  --
 
 * Altogether:
   $$
@@ -148,32 +149,34 @@ Chinese source text: \a[28], \b[153], \c[52139], \d[41]. \+
         &           &   &           &   L &= 63.
     \end{alignedat}
   $$
-====
+==
 
 <<
   母相乘，得三十六為法。
-||
+\\
   .[The] denominators multiplied with each other,
   result in thirty-six as .[the] divisor.
 >>
-====
+=={.translation-annotations}
 * In modern notation:
   $$
     ABC = 3 \times 3 \times 4 = 36.
   $$
-====
+==
 
 <<
   以列數三乘未并者及法。
-||
+\\
   Multiply those not yet combined, and .[the] divisor,
   by .[the] number of rows, three.
 >>
-====
-* 未并者： those not yet combined \+
+=={.translation-annotations}
+* 未并者： those not yet combined
+  --
   These are the individual terms $aBC$, $bCA$ and $cAB$,
   before they were combined subsidiarily to make the level dividend $L$.
   We then obtain
+  --
   $$
     \begin{aligned}
       3 aBC &= 36 \\
@@ -182,20 +185,24 @@ Chinese source text: \a[28], \b[153], \c[52139], \d[41]. \+
       3 ABC &= 108.
     \end{aligned}
   $$
-====
+==
 
 <<
   等數得九，約訖。
-||
+\\
   .[For their] equal quantity .[there] resulteth nine;
   finish reducing .[them].
 >>
-====
-* 等數： equal quantity \+
+=={.translation-annotations}
+* 等數： equal quantity
+  --
   Greatest common divisor (GCD) is called an "equal quantity"
   because it is determined by repeated subtraction
   until an equal quantity is obtained (see [\P1] ["ii 1"]).
+  --
+  --
   In the current example, we have
+  --
   $$
     \gcd (L, 3 aBC, 3 bCA, 3 cAB, 3 ABC) = 9,
   $$
@@ -208,11 +215,11 @@ Chinese source text: \a[28], \b[153], \c[52139], \d[41]. \+
       &= \frac{(+3, -1, -2)}{12}.
     \end{aligned}
   $$
-====
+==
 
 <<
   減四分之三者二，減三分之二者一，并以益三分之一，各平於一十二分之七。
-||
+\\
   That diminished of .[the] three of four shares,
   .[is] two;
   that diminished of .[the] two of three shares, one;
@@ -221,18 +228,10 @@ Chinese source text: \a[28], \b[153], \c[52139], \d[41]. \+
 >>
 
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P3 : ["ii 3"]
-    \nav-curr: \P4
-    \nav-next: \P5 : ["ii 5"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~II \P4"
-]
+%%cite
 
-%footer-element
+
+%%footer
