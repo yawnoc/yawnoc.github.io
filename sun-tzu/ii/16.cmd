e7ebb92a862a1a9bdea24e1db46933eefc8ea2e6
Conway <yawnoc@pm.me>
2022-06-27 22:15:59 +0800

    Convert /sun-tzu/ii/16 to CMDv4

--- sun-tzu/ii/16.cmd
+++ sun-tzu/ii/16.cmd
@@ -1,90 +1,90 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~II" 《孫子算經\.卷中》 \P16
-  %date-created 2019-06-09
-  %date-modified 2020-07-19
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~II \P16"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~II" 《孫子算經卷中》 \P16
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-27
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [II] ["ii" Volume~II]
-*> [\P16](\- You are viewing \P16)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~II](./)
+* %%surrounds-previous --> [<-- \P15](15)
+* %%surrounds-current --> \P16
+* %%surrounds-next --> [\P17 -->](17)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
 
 
-#
-  《孫子算經\.卷中》 \+
-  "\sun-tzu's Computational Classic: Volume~II" \+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !ii
+-{.breadcrumbed} !!16
+- !cite
+^^^^
+
+# .《孫子算經卷中》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~II" <br>
   \P16. Side length of a square of given perimeter
-#
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P15 : ["ii 15"]
-    \nav-curr: \P16
-    \nav-next: \P17 : ["ii 17"]
-  |}}
-|}}}}
+%%noscript-equations
 
 
-\noscript:maths
+%%surrounds
 
-
-[||||
-||||]
-
-
-----
+--
 This paragraph gives a worked example of computing
-the __side length of a square given its perimeter__.
-----
-----
+the side length of a square given its perimeter.
+--
+--
 The relevant unit conversion for length is
-----
+--
 $$
   1 \unit{pace~(步)} = 6 \unit{rules~(尺)}.
 $$
-----
-See [Vol.~I \P1 (Units of length)] ["i 1"].
-----
+--
+See [Vol.~I \P1 (Units of length)](/sun-tzu/i/1).
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \a[38], \b[155], \c[52177], \d[50]. \+
-\d-default
-----
+--
+Chinese source text: \a[38], \b[155], \c[52177], \d[50]. <br>
+%%version-d-default
+--
 
 <<
-  今有索長五千七百九十四步，欲使作方。問幾何？ \+
+  今有索長五千七百九十四步，欲使作方。問幾何？ <br>
   答曰：一千四百四十八步三尺。
-||
+\\
   Now there be rope,
   of length five thousand, seven hundred .[and] ninety-four paces,
   .[and we] wish to make .[it] be .[a] square.
-  .[We] ask, how much .[be its edge]? \+
+  .[We] ask, how much .[be its edge]? <br>
   Answer saith:
     one thousand, four hundred .[and] forty-eight paces .[and] three rules.
 >>
 
 <<
   術曰：置索長五千七百九十四步。以四除之，得一千四百四十八步，餘二步。
-||
+\\
   Method saith:
   put .[down the] rope's length,
     five thousand, seven hundred .[and] ninety-four paces.
@@ -95,22 +95,24 @@ Chinese source text: \a[38], \b[155], \c[52177], \d[50]. \+
 
 <<
   以六因之，得一丈二尺。以四除之，得三尺。
-||
+\\
   Multiplying it by six, resulteth in one rod .[and] two rules.
   Dividing it by four, resulteth in three rules.
 >>
-====
-* 因： multiplying \+
-  As in times tables, 九因歌, ".[the] Song of Nine Multiples".
-====
+=={.translation-annotations}
+* .因： multiplying
+  --
+  As in times tables, .九因歌, ".[the] Song of Nine Multiples".
+  --
+==
 
 <<
   通計，即得。
-||
+\\
   Computing .[the] total, .[we] are done.
 >>
-====
-* 通：總也； total
+=={.translation-annotations}
+* .通：總也； total
 
 * In modern notation, the side length of the square is
   $$
@@ -125,21 +127,13 @@ Chinese source text: \a[38], \b[155], \c[52177], \d[50]. \+
       &= 1448 \unit{paces} + 3 \unit{rules}.
     \end{aligned}
   $$
-====
+==
 
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P15 : ["ii 15"]
-    \nav-curr: \P16
-    \nav-next: \P17 : ["ii 17"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~II \P16"
-]
+%%cite
 
-%footer-element
+
+%%footer
