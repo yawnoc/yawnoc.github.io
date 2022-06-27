9411a900aaff29b78681369fd074934e6098744e
Conway <yawnoc@pm.me>
2022-06-27 23:02:21 +0800

    Convert /sun-tzu/ii/18 to CMDv4

--- sun-tzu/ii/18.cmd
+++ sun-tzu/ii/18.cmd
@@ -1,78 +1,78 @@
-{+ /resources/syntax/general.cmd +}
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
 
-{+ /resources/syntax/chinese-lang.cmd +}
-{+ /resources/syntax/romanisations.cmd +}
-{+ /resources/syntax/sun-tzu.cmd +}
-{+ /resources/syntax/translations.cmd +}
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
-%%
-  %title
-    "\sun-tzu's Computational Classic: Volume~II" 《孫子算經\.卷中》 \P18
-  %date-created 2019-06-09
-  %date-modified 2020-07-19
-  \resources:maths
-%%
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~II \P18"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~II" 《孫子算經卷中》 \P18
+* %date-created --> 2019-06-09
+* %date-modified --> 2022-06-27
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
 
-[[====
-* \header-link:home
-* \header-link:top
-* \header-link:sun-tzu
-*> [II] ["ii" Volume~II]
-*> [\P18](\- You are viewing \P18)
-* \header-link:cite
-====]]
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~II](./)
+* %%surrounds-previous --> [<-- \P17](17)
+* %%surrounds-current --> \P18
+* %%surrounds-next --> [\P19 -->](19)
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
 
 
-#
-  《孫子算經\.卷中》 \+
-  "\sun-tzu's Computational Classic: Volume~II" \+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !ii
+-{.breadcrumbed} !!18
+- !cite
+^^^^
+
+# .《孫子算經卷中》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~II" <br>
   \P18. Volume of a rectangular prism~(2)
-#
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P17 : ["ii 17"]
-    \nav-curr: \P18
-    \nav-next: \P19 : ["ii 19"]
-  |}}
-|}}}}
+%%noscript-equations
 
 
-\noscript:maths
+%%surrounds
 
-
-[||||
-||||]
-
-
-----
+--
 This paragraph gives a worked example of computing
-the __volume of a rectangular prism__.
-----
-----
+the volume of a rectangular prism.
+--
+--
 The relevant unit conversion for length is
-----
+--
 $$
   1 \unit{rod~(丈)} = 10 \unit{rules~(尺)}.
 $$
-----
-See [Vol.~I \P1 (Units of length)] ["i 1"].
-----
+--
+See [Vol.~I \P1 (Units of length)](/sun-tzu/i/1).
+--
 
 
-##{#translation}
-  Translation
-##
+##{#translation} Translation
 
-----
-Chinese source text: \a[38], \b[155], \c[52183], \d[51]. \+
-\d-default
-----
+--
+Chinese source text: \a[38], \b[155], \c[52183], \d[51]. <br>
+%%version-d-default
+--
 
 <<
   今有溝，廣十丈，深五丈，長二十丈，欲以千尺作一方。問得幾何？
-||
+\\
   Now there be .[a] ditch, of
     width ten rods,
     depth five rods, .[and]
@@ -83,13 +83,13 @@ Chinese source text: \a[38], \b[155], \c[52183], \d[51]. \+
 
 <<
   答曰：一千方。
-||
+\\
   Answer saith: one thousand blocks.
 >>
 
 <<
   術曰：置廣一十丈。以深五丈乘之，得五千尺。又以長二十丈乘之，得一百萬尺。
-||
+\\
   Method saith: put .[down the] width, ten rods.
   Multiplying it by .[the] depth, five rods,
   resulteth in five thousand rules.
@@ -99,10 +99,10 @@ Chinese source text: \a[38], \b[155], \c[52183], \d[51]. \+
 
 <<
   以一千除之，即得。
-||
+\\
   Dividing it by one thousand, .[we] are done.
 >>
-====
+=={.translation-annotations}
 * In modern notation, the volume of a rectangular prism of
     width $W = 10 \unit{rods}$,
     depth $H = 5 \unit{rods}$, and
@@ -120,21 +120,13 @@ Chinese source text: \a[38], \b[155], \c[52183], \d[51]. \+
       &= 1000 \unit{blocks}.
     \end{aligned}
   $$
-====
+==
 
 
-{{{{|
-  \nav-up: Volume~II : ["ii"]
-  {{|
-    \nav-prev: \P17 : ["ii 17"]
-    \nav-curr: \P18
-    \nav-next: \P19 : ["ii 19"]
-  |}}
-|}}}}
+%%surrounds
 
 
-\cite-this-page[
-  "\sun-tzu's Computational Classic: Volume~II \P18"
-]
+%%cite
 
-%footer-element
+
+%%footer
