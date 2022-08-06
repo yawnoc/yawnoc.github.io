03f8ef6dc3652a983a135148098739c65eea98e4
Conway <yawnoc@pm.me>
2022-08-06 22:48:50 +0800

    Translate /sun-tzu/iii/1 unto end

--- sun-tzu/iii/1.cmd
+++ sun-tzu/iii/1.cmd
@@ -90,7 +90,7 @@ Chinese source text: \a[54], \b[159], \c[52217], \d[67]. <br>
 \\
   .[In] total .[the] nine families together
   contribute land-tax one thousand barrels,
-  .[and] renting of transport .[is the] worth reduced
+  .[and] renting transport .[is the] worth reduced
   two hundred barrels externally.
   .[We] ask, how much of each family?
 >>
@@ -122,46 +122,73 @@ Chinese source text: \a[54], \b[159], \c[52217], \d[67]. <br>
 <<
   術曰、置甲出三十五斛、以四乘之、得一百四十斛、以五除之、得二十八斛。
 \\
+  Method saith: put .[down] A supplying thirty-five barrels;
+  multiplying it by four, resulteth in one hundred .[and] forty barrels;
+  dividing it by five, resulteth in twenty-eight barrels.
 >>
 
 <<
   乙出四十六斛、以四乘之、得一百八十四斛、以五除之、得三十六斛八斗。
 \\
+  B supplieth forty-six barrels;
+  multiplying it by four, resulteth in one hundred .[and] eighty-four barrels;
+  dividing it by five, resulteth in thirty-six barrels .[and] eight pecks.
 >>
 
 <<
   丙出五十七斛、以四乘之、得二百二十八斛、以五除之、得四十五斛六斗。
 \\
+  C supplieth fifty-seven barrels;
+  multiplying it by four, resulteth in two hundred .[and] twenty-eight barrels;
+  dividing it by five, resulteth in forty-five barrels .[and] six pecks.
 >>
 
 <<
   丁出六十八斛、以四乘之、得二百七十二斛、以五除之、得五十四斛四斗。
 \\
+  D supplieth sixty-eight barrels;
+  multiplying it by four, resulteth in two hundred .[and] seventy-two barrels;
+  dividing it by five, resulteth in fifty-four barrels .[and] four pecks.
 >>
 
 <<
   戊出七十九斛、以四乘之、得三百一十六斛、以五除之、得六十三斛二斗。
 \\
+  E supplieth seventy-nine barrels;
+  multiplying it by four, resulteth in three hundred .[and] sixteen barrels;
+  dividing it by five, resulteth in sixty-three barrels .[and] two pecks.
 >>
 
 <<
   己出八十斛、以四乘之、得三百二十斛、以五除之、得六十四斛。
 \\
+  F supplieth eighty barrels;
+  multiplying it by four, resulteth in three hundred .[and] twenty barrels;
+  dividing it by five, resulteth in sixty-four barrels.
 >>
 
 <<
   庚出一百斛、以四乘之、得四百斛、以五除之、得八十斛。
 \\
+  G supplieth one hundred barrels;
+  multiplying it by four, resulteth in four hundred barrels;
+  dividing it by five, resulteth in eighty barrels.
 >>
 
 <<
   辛出二百一十斛、以四乘之，得八百四十斛、以五除之、得一百六十八斛。
 \\
+  H supplieth two hundred .[and] ten barrels;
+  multiplying it by four, resulteth in eight hundred .[and] forty barrels;
+  dividing it by five, resulteth in one hundred .[and] sixty-eight barrels.
 >>
 
 <<
   壬出三百二十五斛、以四乘之、得一千三百斛、以五除之、得二百六十斛。
 \\
+  I supplieth three hundred .[and] twenty-five barrels;
+  multiplying it by four, resulteth in one thousand three hundred barrels;
+  dividing it by five, resulteth in two hundred .[and] sixty barrels.
 >>
 
 
