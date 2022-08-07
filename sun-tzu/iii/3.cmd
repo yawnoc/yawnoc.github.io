0c8f969b689776894957498b68c8f8f9d9580779
Conway <yawnoc@pm.me>
2022-08-07 16:25:21 +0800

    Translate /sun-tzu/iii/3

--- sun-tzu/iii/3.cmd
+++ sun-tzu/iii/3.cmd
@@ -63,31 +63,45 @@ Chinese source text: \a[58], \b[159], \c[52223], \d[70]. <br>
 <<
   今有平地聚粟、下周三丈六尺、高四尺五寸。問粟幾何。
 \\
+  Suppose there be gathered grain .[on] level ground,
+  of lower circumference three rods .[and] six rules,
+  .[and] height four rules .[and] five inches.
+  .[We] ask, how much grain .[be there]?
 >>
 
 <<
   答曰、一百斛。
 \\
+  Answer saith: one hundred barrels.
 >>
 
 <<
   術曰、置周三丈六尺、自相乘、得一千二百九十六尺。
 \\
+  Method saith: put .[down the] circumference three rods .[and] six rules,
+  .[which], multiplied with itself,
+  resulteth in one thousand, two hundred .[and] ninety-six rules.
 >>
 
 <<
   以高四尺五寸乘之、得五千八百三十二尺。
 \\
+  Multiplying it by .[the] height four rules .[and] five inches,
+  resulteth in five thousand, eight hundred .[and] thirty-two rules.
 >>
 
 <<
   以三十六除之、得一百六十二尺。
 \\
+  Dividing it by thirty-six, resulteth in one hundred .[and] sixty-two rules.
 >>
 
 <<
   以斛法一尺六寸二分除之、即得。
 \\
+  Dividing it by .[the] barrel divisor,
+  one rule, six inches, .[and] two tenths,
+  .[we] are done.
 >>
 
 
