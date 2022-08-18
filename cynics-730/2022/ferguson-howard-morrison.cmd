6fdc3a4cab84e1fe95b1743d5b02486ddd0c631a
Conway <yawnoc@pm.me>
2022-08-18 23:19:08 +0800

    Create Cynic's 7.30: Howard on Morrison

---
+++ cynics-730/2022/ferguson-howard-morrison.cmd
@@ -0,0 +1,344 @@
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/cynicism.cmdr
+
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %title --> A cynic's 7.30: Howard on Morrison (16 Aug 2022)
+* %date-created --> 2022-08-18
+* %date-modified --> 2022-08-18
+- concluding_replacements:
+    #escape-idle-html
+    #.yawnoc.typography
+
+%%%
+
+
+^^^^
+- !home
+- !top
+- !cynicism
+^^^^
+
+
+# %title
+
+--
+([Original 7.30 interview], [archived version])
+--
+
+[Original 7.30 interview]:
+  https://www.abc.net.au/7.30/former-prime-minister-john-howard-speaks-to-7.30/14021948
+[archived version]:
+  https://web.archive.org/web/20220818130813/https://www.abc.net.au/7.30/former-prime-minister-john-howard-speaks-to-7.30/14021948
+
+
+## Former prime minister John Howard speaks to 7.30
+
+
+[Sarah Ferguson:]{.interview-host}: sarah-ferguson-2022-08-16.jpg
+[John Howard:]{.interview-guest}: john-howard-2022-08-16.jpg
+
+
+||||{.interview}
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  John Howard, welcome to 7.30.
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  Thank you.
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  In all your years as prime minister,
+  did you ever secretly appoint yourself to other ministries?
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  No, I didn't.
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  We know Scott Morrison did it five times;
+  what possible justification could there be?
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  Well that's a matter for him
+  --
+  --{.interview-guest .thought}
+  _which is an awfully diplomatic way of saying None, he's insane_.
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  Well, you're the former prime minister and a grandee of the party...
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  That doesn't give me some special right
+  to pontificate on what some of my successors have done.
+  --
+  --{.interview-guest}
+  Look, Scott Morrison has talked about this, he's contextualised it
+  --
+  --{.interview-guest .thought}
+  _with his I'm-Scomo-the-Messiah unlogic_,
+  --
+  --{.interview-guest}
+  and I think everybody, particularly the current Prime Minister,
+  should get on with the present and the future.
+  I say to Mr~Albanese, stop behaving like an opposition leader.
+  You won the election; let's hear a bit of government from you.
+  --
+  --{.interview-guest .thought}
+  _Shut up, Labor._
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  Erm, perhaps your instincts as a senior Liberal are to protect the party,
+  [...] but as both a conservative
+  and someone who believes in parliamentary conventions,
+  how big a breach was this?
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  Look, I'm not going to... pontificate
+  --
+  --{.interview-guest .thought}
+  _even though I look like one_.
+  --
+  --{.interview-guest}
+  There are
+  --
+  --{.interview-guest .thought}
+  _stupid_
+  --
+  --{.interview-guest}
+  reasons why he did it,
+  and part of the conservative tradition is to always understand
+  the context in which things occur
+  --
+  --{.interview-guest .thought}
+  _particularly when those things are poor decisions made by conservatives_.
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  I don't think that belongs to the conservatives...
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  No, but it's not denied to the conservatives.
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  Indeed. But, as someone who has held that position,
+  can you conceive of such a thing,
+  of secretly appointing yourself to ministerial positions
+  without telling your colleagues?
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  Sarah, good try,
+  --
+  --{.interview-guest .thought}
+  _i.e.~screw you,_
+  --
+  --{.interview-guest}
+  but look, I've indicated that it's a matter
+  for Scott to explain and contextualise.
+  --
+  --{.interview-guest}
+  I think most people, and allow me the expression of this opinion,
+  most people
+  --
+  --{.interview-guest .thought}
+  _in the Liberal Party_
+  --
+  --{.interview-guest}
+  are going to say,
+  "Well, that's interesting but let's get on with the present and the future."
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  I think there's a number of people in your own party
+  who are calling on Scott Morrison to resign from parliament.
+  Should he at least do that?
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  No, I don't think he should do that.
+  Apart from anything else, it's not in the interests of the Liberal Party
+  --
+  --{.interview-guest .thought}
+  _even though it may well be in the interests of this country_
+  --
+  --{.interview-guest}
+  to have a by-election at the moment [...],
+  so, if anybody cares about my party, the Liberal Party,
+  then the last thing they'll do is be requesting unwanted by-elections.
+  --
+
+  --{.interview-host}
+  [...]
+  --
+  --{.interview-guest}
+  [...]
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  Do you understand why the governor-general waved it through?
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  I don't think any criticism can be offered of the governor-general because,
+  there is on the face of it [...] nothing illegal.
+  So in those circumstances, the true conservative principle
+  is that he should act on the advice of his ministers
+  --
+  --{.interview-guest .thought}
+  _(Morrison, Morrison, Morrison, Morrison, and Morrison)._
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  It's a legacy for your party however,
+  which is now in the process of tearing itself apart, to some degree.
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  Now, I don't want to be combative...
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  No; nor do I, sir.
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  and I know you don't want to be combative,
+  but I thought we were going to talk about...
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  Mmm. There are *plenty* of things to talk about.
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  Yes I know, I know; well I don't want to be,
+  suggest[ing] for a *moment*,
+  that I have been got on this program under false pretences
+  --
+  --{.interview-guest .thought}
+  _except right now~😈._
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  No, we certainly asked you on this program to talk about [...] your book,
+  but you would not deny me the opportunity to ask you an important...
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  No, I don't deny because it's a very free country...
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  Indeed.
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  ... but I can point out the sequence of events,
+  and I can give context to the questions that you're asking
+  --
+  --{.interview-guest .thought}
+  _which are starting to irritate me ever so slightly._
+  --
+
+  --{.interview-host}
+  [...]
+  --
+  --{.interview-guest}
+  [...]
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  One thing you're very clear about in your book
+  is your disapproval for Donald Trump. [...]
+  --
+
+  --{.interview-guest}
+  [...]
+  --
+  --{.interview-host}
+  [...]
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  [...]
+  My attitude to Donald Trump has been set in stone
+  by his behaviour after he lost.
+  --
+  --{.interview-guest}
+  Part of democracy is to accept results [...].
+  I didn't like losing in 2007, but I didn't carry on that I'd been cheated,
+  and no doubt Paul Keating didn't like losing to me in '96
+  but *he* didn't say he'd been cheated.
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  And it didn't turn out that you'd appointed yourself
+  to numerous ministries at the same time.
+  --
+
+  ![John Howard:]
+  --{.interview-guest .thought}
+  _Dammit, I was going to make the closing remark._
+  --
+
+  ![Sarah Ferguson:]
+  --{.interview-host}
+  Mr~Howard, that's all we have time for this evening
+  but thank you very much indeed for joining us.
+  --
+
+  ![John Howard:]
+  --{.interview-guest}
+  ::{style="font-size: small"} Thank you.::
+  --
+
+  ||{.marker .end}
+    END
+  ||
+
+||||
+
+
+%%footer
