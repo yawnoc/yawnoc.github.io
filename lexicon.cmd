6970736b0f4e95cc5b470dbe9363d76189c24309
Conway <yawnoc@pm.me>
2022-09-18 11:58:58 +0800

    Translate /sun-tzu/iii/9

--- lexicon.cmd
+++ lexicon.cmd
@@ -16,7 +16,7 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 * %description -->
     Conway's Lexicon for translating Literary Chinese into English.
 * %date-created --> 2020-08-05
-* %date-modified --> 2022-08-13
+* %date-modified --> 2022-09-18
 
 RegexDictionaryReplacement: #.radical-stroke-count-links
 - queue_position: BEFORE #specified-links
@@ -4977,7 +4977,7 @@ See also: [御米]
 
 8 [綿] U+7DBF
 ==
-* cotton
+* silk-floss
 * unbroken
 ==
 
