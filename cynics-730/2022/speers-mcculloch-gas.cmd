27aead0bdd024a68ccc821af05660be31948c336
Conway <yawnoc@pm.me>
2022-11-03 23:46:51 +0800

    Create Cynic's 7.30: McCulloch on gas

---
+++ cynics-730/2022/speers-mcculloch-gas.cmd
@@ -0,0 +1,485 @@
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/cynicism.cmdr
+
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %title --> A cynic's 7.30: Samantha McCulloch on gas prices (3~Nov 2022)
+* %date-created --> 2022-11-03
+* %date-modified --> 2022-11-03
+- concluding_replacements:
+    #.yawnoc.typography
+
+%%%
+
+
+^^^^
+- !home
+- !top
+- !cynicism
+^^^^
+
+
+# %title
+
+--
+([Original 7.30 interview], [archived version])
+--
+
+[Original 7.30 interview]:
+  https://www.abc.net.au/news/2022-11-03/albanese-government-considering-an-intervention-in/101614570
+[archived version]:
+  https://web.archive.org/web/20221103131322/https://www.abc.net.au/news/2022-11-03/albanese-government-considering-an-intervention-in/101614570
+
+
+## David Speers interviews Samantha McCulloch
+
+
+[David Speers:]{.interview-host}: david-speers-2022-11-03.jpg
+[Samantha McCulloch:]{.interview-guest}: samantha-mcculloch-2022-11-03.jpg
+
+
+||||{.interview}
+
+  ![David Speers:]
+  --{.interview-host}
+  Samantha McCulloch is the chief executive of the
+  Australian Petroleum Production & Exploration Association,
+  which represents gas producers.
+  --
+  --{.interview-host}
+  Samantha McCulloch, welcome to the program.
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  Thanks for having me David.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  So the industry minister says the problem with gas prices right now
+  is "a glut of greed" by the gas companies. Is he right?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest .thought}
+  _Oh, what a direct question! But first, talking points:_
+  --
+  --{.interview-guest}
+  David what we're seeing is high energy prices across the globe.
+  This is the result of a global energy crisis,
+  triggered by Russia's invasion of Ukraine.
+  --
+  --{.interview-guest}
+  It's affecting all energy sources, and it's affecting all countries.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  But can you explain to me,
+  why that should push up the price of *Australian* gas, in Australia?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  We're seeing
+  --
+  --{.interview-guest .thought}
+  _specifically ogling, and salivating at_
+  --
+  --{.interview-guest}
+  very strong demand for gas domestically. [...]
+  It's increasing demand for gas that's driving up these prices,
+  as well as of course international pressures.
+  And the key to driving down these prices [...] is increasing supply.
+  --
+  --{.interview-guest}
+  That's what the industry is seeking to do,
+  --
+  --{.interview-guest .thought}
+  _within the constraints of maximising profit,_
+  --
+  --{.interview-guest}
+  but we need a clear runway, for more investment,
+  to boost supply, and put downward pressure on prices.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Well, OK but you did say a few days ago yourself,
+  "The domestic market is well supplied."
+  And the agreement that was struck only just over a month ago
+  [...] was about boosting supply [...].
+  Supply doesn't appear to be the problem does it?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  The gas industry is committed, to supplying
+  --
+  --{.interview-guest .thought}
+  _at exorbitant prices_
+  --
+  --{.interview-guest}
+  the domestic market, and the domestic market has been well supplied.
+  The heads of agreement that were struck [...]
+  committed to ensuring adequate supply for the domestic market.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  So what's the problem with supply? Is *price* the problem, in reality?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest .thought}
+  _Oh look, some states:_
+  --
+  --{.interview-guest}
+  The problem that we're seeing, is the increasing demand for gas
+  is predominantly in the southern states [...].
+  Yet, we're not seeing the investment in supply, in those states.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  But you just said, we're well supplied.
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  The cheapest gas is the gas that's close to the use
+  --
+  --{.interview-guest .thought}
+  _which is why we love sending it overseas_.
+  --
+  --{.interview-guest}
+  So we
+  --
+  --{.interview-guest .thought}
+  _and this one is an inclusive we, which includes you the taxpayer_
+  --
+  --{.interview-guest}
+  need to be building more supply, where it's needed.
+  [...]
+  --
+
+  --{.interview-host}
+  [...]
+  --
+  --{.interview-guest}
+  [...]
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  [...] Alright. Well if more supply is the answer here,
+  one proposal being considered by the government,
+  is to let the three big LNG exporters
+  keep their international contracts in place,
+  no breaching of contracts,
+  but *excess* gas that's currently sold on the international spot market
+  would instead be made available in Australia first,
+  at a price of no more than $10 a gigajoule.
+  --
+  --{.interview-host}
+  More than they were getting last year, so they still get a profit.
+  What's wrong with that idea?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  David, that uncontracted gas is already being offered
+  to the domestic market first.
+  That was the basis of the heads of agreement [...].
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  But at *very* high prices. But not at $10 a gigajoule,
+  it's being offered for more than that.
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  Well let's just be clear on what these gas price caps would do.
+  --
+  --{.interview-guest}
+  I know it sounds like a[n] easy and quick solution...
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Yep.
+  --
+  --{.interview-host .thought}
+  _And that yep isn't even one of these inserted thought bubbles!_
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  ... but it's not that simple.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Why not?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  Gas prices, gas price caps, would have... a chilling effect on
+  --
+  --{.interview-guest .thought}
+  _profit, and to a lesser extent_
+  --
+  --{.interview-guest}
+  new investment.
+  It will undermine investment confidence in new supply,
+  it'll actually increase demand,
+  while creating structural shortages in supply,
+  which just creates further difficulties and problems long-term.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Well, you talk about a chilling effect,
+  manufacturers are facing more than a chilling effect [...].
+  That's spooking a lot of investors,
+  having to, you know, face double the gas price they were a year ago.
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  Let's... when we're talking about gas prices,
+  let's just be clear, this is a complex market,
+  and gas pricing is complex
+  --
+  --{.interview-guest .thought}
+  _meaning profitable_.
+  --
+  --{.interview-guest}
+  There's the retail market, and then there's the wholesale market.
+  Now, I'm going to, I'm focused on the wholesale market [...].
+  And the realised average price, for gas sold in the wholesale market,
+  90% of which is sold under long-term contracts,
+  is priced between [$]6 and $12 a gigajoule.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Well that was probably on contracts
+  written before this price spike though right?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  That's the average price being realised, in third quarter this year.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  [...] That's between July and September
+  and presumably contracts that pre-date what's happened with the gas price.
+  What's being offered now?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  So David, this is where we need far more transparency in the market.
+  --
+  --{.interview-guest .thought}
+  _Whoops. Did I just say that instead of
+  "we need *to be* far more transparent?"_
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Can't you tell us?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  Because we're seeing... well we're seeing, uh claims of...
+  of very high prices being offered in the market...
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Well don't you know?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  ... but there's no details around that.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Why don't you know?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  No, this is reported to the ACCC.
+  We'll get more details of this but there will be a delay.
+  --
+  --{.interview-guest .thought}
+  _Suck shit, David._
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Can you tell me how much, uh, have the profits of the LNG exporters grown,
+  since the war in Ukraine?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest .thought}
+  _🤑🤑🤑🤑🤑🤑🤑🤑🤑🤑🤑🤑🤑_
+  --
+  --{.interview-guest}
+  David, what I can tell you, is that the strong demand for our LNG exports,
+  is delivering returns for
+  --
+  --{.interview-guest .thought}
+  _us the gas industry, at the expensive of_
+  --
+  --{.interview-guest}
+  Australian budgets and the Australian people.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Well I'm asking about the profits for the companies though.
+  There are estimates it's around $26~billion in profit *growth*.
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  I don't have an estimate for individual companies,
+  they report this on a quarterly basis.
+  But what I can say, is that we're seeing more
+  --
+  --{.interview-guest .thought}
+  _which is a euphemism for fuck-all_
+  --
+  --{.interview-guest}
+  returns to the Australian people in the budget [...].
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  To be clear on that, the petroleum resource rent tax,
+  this financial year, is returning very little...
+  $2.6~billion, and that's barely moved from before the gas price spike.
+  So is that really fair given the sort of profits that are estimated
+  to be $26~billion in profit growth?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  The petroleum resource rent tax is projected to provide $11~billion,
+  to the federal government budget, in the forward estimates...
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Over *four years*.
+  --
+  --{.interview-host .thought}
+  _Nice try. I'm not innumerate._
+  --
+  --{.interview-host}
+  [...] What are profits over four years, if you want to use that figure?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  This is already a profits-based tax
+  --
+  --{.interview-guest .thought}
+  _which you all ought to be grateful for_
+  --
+  --{.interview-guest}
+  of at least 40%
+  --
+  --{.interview-guest .thought}
+  _of the excess of assessable receipts
+  over the deductible expenditure and transferred exploration expenditure_
+  --
+  --{.interview-guest}
+  and it's just one aspect of the
+  --
+  --{.interview-guest .thought}
+  _measly_
+  --
+  --{.interview-guest}
+  financial contribution of the industry to government revenues.
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Why can't you tell us the profits they're raking in?
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  ...
+  --
+  --{.interview-guest .thought}
+  _How dare you leave me speechless for a few seconds!_
+  --
+  --{.interview-guest}
+  ... Look, I'm... I'm representing the industry, the... these are
+  --
+  --{.interview-guest .thought}
+  _allegedly tax-avoiding_
+  --
+  --{.interview-guest}
+  international companies too of course, David,
+  but what I can tell you is the direct contribution that we're making,
+  to the Australian governments, and to Australian people,
+  --
+  --{.interview-guest .thought}
+  _whom we do shaft with glee,_
+  --
+  --{.interview-guest}
+  will *triple* this year according to our estimates. [...]
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  OK.
+  --
+  --{.interview-host .thought}
+  _But triple an infinitesimal amount is still infinitesimal._
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  That includes royalties, it includes taxes, [...].
+  --
+  --{.interview-guest}
+  [...]
+  --
+
+  ![David Speers:]
+  --{.interview-host}
+  Alright. Samantha McCulloch, we'll have to leave it there.
+  Thanks for joining us.
+  --
+
+  ![Samantha McCulloch:]
+  --{.interview-guest}
+  Thank you David.
+  --
+
+  ||{.marker .end}
+    END
+  ||
+
+||||
+
+
+%%footer
