b1ef3c700f6a81ec0a0bf6149c748707b58f3786
Conway <yawnoc@pm.me>
2022-11-10 20:59:32 +0800

    Update /sun-tzu/iii/11 surrounds for 12

--- sun-tzu/iii/11.cmd
+++ sun-tzu/iii/11.cmd
@@ -14,7 +14,7 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 * %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~III \S11"'
 * %title --> "Sun Tz(uu)'s Computational Classic: Volume~III" 《孫子算經卷下》 \S11
 * %date-created --> 2022-11-05
-* %date-modified --> 2022-11-05
+* %date-modified --> 2022-11-10
 - concluding_replacements:
     #.yawnoc.typography
     #.romanisation.special-characters
@@ -24,7 +24,7 @@ OrdinaryDictionaryReplacement: #.surrounds-navigation
 * %%surrounds-up --> [^ Volume~III](./)
 * %%surrounds-previous --> [<-- \S10](10)
 * %%surrounds-current --> \S11
-* %%surrounds-next -->
+* %%surrounds-next --> [\S12 -->](12)
 - concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
 
 %%%
