e96f412265b9b425dda3cb871b09a6f4d6fe7dde
Conway <yawnoc@pm.me>
2022-11-12 22:04:05 +0800

    Rewrite generate-sitemap.py nicer

--- generate-sitemap.py
+++ generate-sitemap.py
@@ -19,60 +19,26 @@ import os
 import re
 
 
-NEWLINE = '\n'
+
+def to_canonical_url(html_name):
+  url = html_name
+
+  url = re.sub(r'\\', '/', url)
+  url = re.sub('/index[.]html$', '/', url)
+  url = re.sub('[.]html$', '', url)
+  url = re.sub('^[.]', 'https://yawnoc.github.io', url)
+
+  return url
 
 
-def list_to_string(list_):
-  """
-  Convert list to newline-separated string
-  """
-  
-  return NEWLINE.join(list_) + NEWLINE
-
-
-def string_to_list(string):
-  """
-  Convert newline-separated string to list
-  """
-  
-  return string.strip().split(NEWLINE)
-
-
-# Get list of all HTML pages
-EXCLUDED_PAGES = [
-  f'{file_name}.html'
-    for file_name in [
-      '404',
-    ]
-]
-sitemap = [
-  os.path.join(path, name)
+if __name__ == '__main__':
+  urls = [
+    to_canonical_url(os.path.join(path, name))
     for path, _, files in os.walk('.')
-      for name in files
-        if name.endswith('.html') and name not in EXCLUDED_PAGES
-]
+    for name in files
+    if name.endswith('.html') and name not in ['404.html']
+  ]
 
-# Convert sitemap from list to newline-separated string
-sitemap = list_to_string(sitemap)
-
-# Convert Windows backslashes to forward slashes
-sitemap = re.sub(r'\\', '/', sitemap)
-
-# Canonicalise /index.html as /
-sitemap = re.sub('/index[.]html$', '/', sitemap, flags=re.MULTILINE)
-
-# Remove .html extension
-sitemap = re.sub('[.]html$', '', sitemap, flags=re.MULTILINE)
-
-# Replace . with actual root
-ROOT = 'https://yawnoc.github.io'
-sitemap = re.sub('^[.]', ROOT, sitemap, flags=re.MULTILINE)
-
-# Sort sitemap
-sitemap = string_to_list(sitemap)
-sitemap.sort()
-sitemap = list_to_string(sitemap)
-
-# Write to sitemap.txt
-with open('sitemap.txt', 'w', encoding='utf-8') as sitemap_file:
-  sitemap_file.write(sitemap)
+  with open('sitemap.txt', 'w', encoding='utf-8') as sitemap_file:
+    for url in sorted(urls):
+      sitemap_file.write(f'{url}\n')
