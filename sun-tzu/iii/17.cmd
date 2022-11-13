91781470ed3cfa0c96dfcde7aa7806d47ee934c3
Conway <yawnoc@pm.me>
2022-11-13 18:35:47 +0800

    Add /sun-tzu/iii/17 translation annotations

--- sun-tzu/iii/17.cmd
+++ sun-tzu/iii/17.cmd
@@ -79,6 +79,17 @@ Chinese source text: \a[68], \b[161], \c[52265], \d[80]. <br>
   .[in] total use cups sixty-five.
   Know .[we] not how many guests .[there be]?
 >>
+=={.translation-annotations}
+* \c[52265] erroneously has .用聣 for .用桮.
+
+* .不知客幾何： "know .[we] not how many guests .[there be]"
+  --
+  The quotation marks in \c[52265] mark this as spoken by the woman.
+  Personally I disagree, and think that .不知客幾何 is spoken by the narrator.
+  The original Chinese is ambiguous as it carries no punctuation;
+  I have preserved the ambiguity by not editing in any quotation marks.
+  --
+==
 
 <<
   答曰、六十人。
@@ -93,6 +104,16 @@ Chinese source text: \a[68], \b[161], \c[52265], \d[80]. <br>
   multiplying it by twelve, resulteth in seven hundred .[and] eighty.
   Dividing it by thirteen, .[we] are done.
 >>
+=={.translation-annotations}
+* In modern notation, supposing that the guests be $n$ people, we have
+  $$
+    65 = \frac{n}{2} + \frac{n}{3} + \frac{n}{4} = \frac{13n}{12},
+  $$
+  and hence
+  $$
+    n = \frac{65 \times 12}{13} = 60.
+  $$
+==
 
 
 %%surrounds
