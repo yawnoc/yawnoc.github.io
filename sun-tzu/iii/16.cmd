bdcf3ded9b8a0319690575ed51c2480af9f1314e
Conway <yawnoc@pm.me>
2022-11-13 11:35:52 +0800

    Create /sun-tzu/iii/16 (skeleton)

---
+++ sun-tzu/iii/16.cmd
@@ -0,0 +1,89 @@
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
+
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~III \S16"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~III" 《孫子算經卷下》 \S16
+* %date-created --> 2022-11-13
+* %date-modified --> 2022-11-13
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
+
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~III](./)
+* %%surrounds-previous --> [<-- \S15](15)
+* %%surrounds-current --> \S16
+* %%surrounds-next -->
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
+
+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !iii
+-{.breadcrumbed} !!16
+- !cite
+^^^^
+
+# .《孫子算經卷下》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~III" <br>
+  \S16. Division utilising a rate~(3)
+
+%%noscript-equations
+
+
+%%surrounds
+
+--
+This section gives a word problem
+where division by a rate is used to determine an amount.
+--
+--
+The relevant unit conversions for length are
+--
+$$
+  \begin{aligned}
+    1 \unit{length~(匹)} &= 40 \unit{rules~(尺)} \\
+    1 \unit{rod~(丈)} &= 10 \unit{rules~(尺)},
+  \end{aligned}
+$$
+--
+and, for capacity,
+--
+$$
+  \begin{aligned}
+    1 \unit{barrel~(斛)} &= 10 \unit{pecks~(斗)} \\
+    1 \unit{peck~(斗)} &= 10 \unit{quarts~(升)} \\
+    1 \unit{quart~(升)} &= 10 \unit{gills~(合)}.
+  \end{aligned}
+$$
+
+
+##{#translation} Translation
+
+--
+Chinese source text: \a[66], \b[161], \c[52262], \d[79]. <br>
+%%version-d-default
+--
+
+
+%%surrounds
+
+
+%%cite
+
+
+%%footer
