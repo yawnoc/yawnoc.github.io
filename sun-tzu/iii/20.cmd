c800f2d578c5b83a9411d1c312dfda2ee08399c2
Conway <yawnoc@pm.me>
2022-11-20 15:10:16 +0800

    Create /sun-tzu/iii/20 (skeleton)

---
+++ sun-tzu/iii/20.cmd
@@ -0,0 +1,78 @@
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
+
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~III \S20"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~III" 《孫子算經卷下》 \S20
+* %date-created --> 2022-11-20
+* %date-modified --> 2022-11-20
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
+
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~III](./)
+* %%surrounds-previous --> [<-- \S19](19)
+* %%surrounds-current --> \S20
+* %%surrounds-next -->
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
+
+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !iii
+-{.breadcrumbed} !!20
+- !cite
+^^^^
+
+# .《孫子算經卷下》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~III" <br>
+  \S20. Unit conversion~(1)
+
+%%noscript-equations
+
+
+%%surrounds
+
+--
+This section gives a worked example of unit conversion.
+--
+--
+The relevant unit conversion for weight is
+--
+$$
+  1 \unit{catty~(斤)} = 16 \unit{taels~(兩)}.
+$$
+--
+See [Vol.~I \S2 (Units of weight)](/sun-tzu/i/2).
+--
+
+
+##{#translation} Translation
+
+--
+Chinese source text: \a[70], \b[161], \c[52274], \d[82]. <br>
+%%version-d-default
+--
+
+
+%%surrounds
+
+
+%%cite
+
+
+%%footer
