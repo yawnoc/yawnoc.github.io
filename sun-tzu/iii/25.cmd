809f3aca7db8cda9f4c260c2bf6494da4af6b650
Conway <yawnoc@pm.me>
2022-11-26 10:33:54 +0800

    Create /sun-tzu/iii/25 (skeleton)

---
+++ sun-tzu/iii/25.cmd
@@ -0,0 +1,70 @@
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
+
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~III \S25"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~III" 《孫子算經卷下》 \S25
+* %date-created --> 2022-11-26
+* %date-modified --> 2022-11-26
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
+
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~III](./)
+* %%surrounds-previous --> [<-- \S24](24)
+* %%surrounds-current --> \S25
+* %%surrounds-next -->
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
+
+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !iii
+-{.breadcrumbed} !!25
+- !cite
+^^^^
+
+# .《孫子算經卷下》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~III" <br>
+  \S25. Utilising similar triangles
+
+%%noscript-equations
+
+
+%%surrounds
+
+--
+This section gives a word problem where similarity of triangles is used
+to determine an unknown length.
+--
+
+
+##{#translation} Translation
+
+--
+Chinese source text: \a[72], \b[162], \c[52289], \d[85]. <br>
+%%version-d-default
+--
+
+
+%%surrounds
+
+
+%%cite
+
+
+%%footer
