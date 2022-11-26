7ba17698cd011c1daef594cae040e7472a27f366
Conway <yawnoc@pm.me>
2022-11-26 15:57:15 +0800

    Prefer supplying [be the] for 客幾何
    rather than [there be]

--- sun-tzu/iii/17.cmd
+++ sun-tzu/iii/17.cmd
@@ -14,7 +14,7 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 * %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~III \S17"'
 * %title --> "Sun Tz(uu)'s Computational Classic: Volume~III" 《孫子算經卷下》 \S17
 * %date-created --> 2022-11-13
-* %date-modified --> 2022-11-20
+* %date-modified --> 2022-11-26
 - concluding_replacements:
     #.yawnoc.typography
     #.romanisation.special-characters
@@ -71,13 +71,13 @@ Chinese source text: \a[68], \b[161], \c[52265], \d[80]. <br>
 <<
   津吏曰、客幾何。婦人曰、二人共飯、三人共羹、四人共肉、凡用桮六十五。不知客幾何。
 \\
-  .[And the] officer of .[the] ford saith, How many guests .[be there]?
+  .[And the] officer of .[the] ford saith, How many .[be the] guests?
   .[And the] woman saith,
     Two people together .[for] rice,
     three people together .[for] soup,
     .[and] four people together .[for] flesh,
   .[in] total use cups sixty-five.
-  Know .[we] not how many guests .[there be]?
+  Know .[we] not how many .[be the] guests?
 >>
 =={.translation-annotations}
 * \c[52265] erroneously has .用聣 for .用桮.
