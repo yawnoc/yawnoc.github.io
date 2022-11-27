b8862a041147d1a22fde6c40eed27416c0901fa2
Conway <yawnoc@pm.me>
2022-11-27 21:53:29 +0800

    Create /sun-tzu/iii/32 (skeleton)

---
+++ sun-tzu/iii/32.cmd
@@ -0,0 +1,70 @@
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/equations.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+< /resources/rules/sun-tzu.cmdr
+
+OrdinaryDictionaryReplacement: #.footer-properties-override
+- queue_position: AFTER #.yawnoc.footer
+
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"Sun Tz(uu)'s Computational Classic: Volume~III \S32"'
+* %title --> "Sun Tz(uu)'s Computational Classic: Volume~III" 《孫子算經卷下》 \S32
+* %date-created --> 2022-11-27
+* %date-modified --> 2022-11-27
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
+
+OrdinaryDictionaryReplacement: #.surrounds-navigation
+- queue_position: AFTER #.yawnoc.properties-override
+* %%surrounds-up --> [^ Volume~III](./)
+* %%surrounds-previous --> [<-- \S31](31)
+* %%surrounds-current --> \S32
+* %%surrounds-next -->
+- concluding_replacements: #.sun-tzu.surrounds-navigation-arrows
+
+%%%
+
+
+^^^^
+- !home
+- !top
+- !sun-tzu
+-{.breadcrumbed} !iii
+-{.breadcrumbed} !!32
+- !cite
+^^^^
+
+# .《孫子算經卷下》 "(Sun Tz(uu)|孫子)'s Computational Classic: Volume~III" <br>
+  \S32. Division utilising a rate~(4)
+
+%%noscript-equations
+
+
+%%surrounds
+
+--
+This section gives a word problem
+where division by a rate is used to determine an amount.
+--
+
+
+##{#translation} Translation
+
+--
+Chinese source text: \a[80], \b[163], \c[52311], \d[92]. <br>
+%%version-d-default
+--
+
+
+%%surrounds
+
+
+%%cite
+
+
+%%footer
