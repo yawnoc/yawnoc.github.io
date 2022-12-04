2d516246248f187b552f97631090cbc4659ea6b3
Conway <yawnoc@pm.me>
2022-12-04 15:30:27 +0800

    Create /analects/ (index)

---
+++ analects/index.cmd
@@ -0,0 +1,78 @@
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+* %cite-title --> '"[The] Analects"'
+* %title --> "[The] Analects" 《論語》
+* %description -->
+    Work in progress for an English translation the Confucian Analects, 《論語》.
+* %date-created --> 2022-12-04
+* %date-modified --> 2022-12-04
+
+%%%
+
+
+^^^^
+- !home
+- !top
+<## TODO: - !!analects ##>
+- !cite
+^^^^
+
+
+# .《論語》 ".[The] Analects"
+
+--
+Work in progress for an English translation the Confucian ''Analects''.
+--
+
+
+##{#contents} Contents of translation
+
+;;;;{-class}
+1. [.《學而第一》 ".[To] Learn And, .[the] First"](1-learn-and)
+;;;;
+
+||{.marker}
+(Incomplete; work in progress.)
+||
+
+
+##{#conways-preface} Conway's Preface
+
+--
+.《論語》, ".[The] Analects", colloquially known as "Confucius Say",
+is a collection of (alleged) sayings of Confucius and others.
+Though not initially of exceeding prominence,
+it eventually became a central text of Confucianism.
+A detailed discussion about its history, authorship, and significance
+I defer to [its Wikipedia article].
+--
+--
+The ''Analects'' have been interpreted to death throughout the ages,
+[as pointed out by James Legge].
+Scholars have written countless .註 "annotations" and .疏 "subcommentary"
+(which are annotations of the annotations).
+It is of course a slippery slope
+between critical interpretation of a text (exegesis)
+and projection of the interpreter's viewpoint into it (eisegesis).
+--
+--
+This translation is an attempt to render the ''Analects''
+with as few supplied words as possible,
+that it might keep nigh unto exegesis.
+When it feels like the translation is vague or not saying very much,
+this is a reflection of the terseness of the original Chinese.
+--
+
+[its Wikipedia article]: https://en.wikipedia.org/wiki/Analects
+[as pointed out by James Legge]: https://archive.org/details/chineseclassicsw0001unse/page/n37/mode/1up?view=theater
+
+
+%%cite
+
+
+%%footer
