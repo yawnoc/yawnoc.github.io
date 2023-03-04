8256da984c68b3dc24b8faeff01a90164686251a
Conway <yawnoc@pm.me>
2023-03-04 17:00:07 +0800

    Convert sun-tzu/iii/27 to translation tables

--- sun-tzu/iii/27.cmd
+++ sun-tzu/iii/27.cmd
@@ -9,13 +9,14 @@
 
 OrdinaryDictionaryReplacement: #.footer-properties-override
 - queue_position: AFTER #.yawnoc.footer
+* %copyright-prior-years --> get_year@%date-created--
 
 OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 - queue_position: AFTER #.yawnoc.properties-override
 * %cite-title --> '"Sun~Tz(uu)'s Computational Classic: Volume~III \S27"'
 * %title --> "Sun~Tz(uu)'s Computational Classic: Volume~III" 《孫子算經卷下》 \S27
 * %date-created --> 2022-11-26
-* %date-modified --> 2022-12-06
+* %date-modified --> 2023-03-xx
 - concluding_replacements:
     #.yawnoc.typography
     #.romanisation.special-characters
@@ -60,6 +61,7 @@ Chinese source text: \a[74], \b[162], \c[52295], \d[87]. <br>
 %%version-d-default
 --
 
+@@@@
 <<
   今有獸六首四足、禽四首二足。上有七十六首、下有四十六足。
 \\
@@ -68,7 +70,7 @@ Chinese source text: \a[74], \b[162], \c[52295], \d[87]. <br>
   Above be there seventy-six heads;
   below be there forty-six feet.
 >>
-=={.translation-annotations}
+==
 * \b[162] erroneously has .禽二首 for .禽四首.
 ==
 
@@ -77,7 +79,7 @@ Chinese source text: \a[74], \b[162], \c[52295], \d[87]. <br>
 \\
   .[We] ask, how many each .[be the] fowls .[and the] beasts?
 >>
-=={.translation-annotations}
+==
 * \c[52295] erroneously has .篱 for .禽.
 
 * In modern notation, we have the system
@@ -103,7 +105,7 @@ Chinese source text: \a[74], \b[162], \c[52295], \d[87]. <br>
   double .[the] feet with .[which] subtract of .[the] heads;
   .[the] remainder, halving it, is .[the] beasts.
 >>
-=={.translation-annotations}
+==
 * \c[52295] erroneously has .之即禽 before .即獸.
 
 * In modern notation,
@@ -122,7 +124,7 @@ Chinese source text: \a[74], \b[162], \c[52295], \d[87]. <br>
   Subtract of .[the] feet by four times .[the] beasts;
   .[the] remainder, halving it, is .[the] fowls.
 >>
-=={.translation-annotations}
+==
 * \c[52295] is erroneously missing .之即禽.
 
 * In modern notation, the feet equation rearranges to give
@@ -130,6 +132,7 @@ Chinese source text: \a[74], \b[162], \c[52295], \d[87]. <br>
     f = \frac{46 - 4b}{2} = \frac{46 - 4 \times 8}{2} = 7.
   $$
 ==
+@@@@
 
 
 %%surrounds
