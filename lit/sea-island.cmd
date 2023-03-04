5e7b30ad1b45a97dbfea42d86cc8da730acedfee
Conway <yawnoc@pm.me>
2023-03-04 11:32:16 +0800

    Make sea-island %date-modified indeterminate

--- lit/sea-island.cmd
+++ lit/sea-island.cmd
@@ -17,7 +17,7 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
     An annotated English translation of the first problem in Haidao Suanjing, \
     《海島算經》, `The Sea Island Computational Classic`.
 * %date-created --> 2019-04-20
-* %date-modified --> 2022-12-26
+* %date-modified --> 2023-03-xx
 - concluding_replacements:
     #.yawnoc.double-quotes-in-attributes
     #backslash-continuations
