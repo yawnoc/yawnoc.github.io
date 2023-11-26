22f89c395fb1e09b061cef95565e2f2fc0408526
Conway <yawnoc@pm.me>
2023-11-26 18:20:59 +0800

    Fix /sun-tzu/iii/20 missing 黃金 literal annotation

--- sun-tzu/iii/20.cmd
+++ sun-tzu/iii/20.cmd
@@ -16,7 +16,7 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 * %cite-title --> '"Sun~Tz(uu)'s Computational Classic: Volume~III \S20"'
 * %title --> "Sun~Tz(uu)'s Computational Classic: Volume~III" 《孫子算經卷下》 \S20
 * %date-created --> 2022-11-20
-* %date-modified --> 2023-04-29
+* %date-modified --> 2023-11-26
 - concluding_replacements:
     #.yawnoc.typography
     #.romanisation.special-characters
@@ -77,6 +77,9 @@ Chinese source text: \a[70], \b[161], \c[52274], \d[82]. <br>
   Suppose there be gold one catty, worth coins ten myriad.
   .[We] ask, how much .[be each] tael worth?
 >>
+==
+* .黃金： gold; \lit yellow gold
+==
 
 <<
   答曰、六千二百五十錢。
