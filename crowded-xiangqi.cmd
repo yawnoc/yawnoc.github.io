0cf8dbfc1604ea11689f438f0ea1a1a2a13b02a3
Conway <yawnoc@pm.me>
2024-06-23 22:40:54 +0800

    Update Crowded for Fairy-Stockfish configuration

--- crowded-xiangqi.cmd
+++ crowded-xiangqi.cmd
@@ -4,7 +4,7 @@
 
 OrdinaryDictionaryReplacement: #.footer-properties-override
 - queue_position: AFTER #.yawnoc.footer
-* %copyright-prior-years --> get_year@%date-created--
+* %copyright-prior-years --> 'get_year@%date-created--2022, '
 
 OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 - queue_position: AFTER #.yawnoc.properties-override
@@ -13,7 +13,7 @@ OrdinaryDictionaryReplacement: #.boilerplate-properties-override
 * %title --> Crowded Xiangqi
 * %description --> Conway's half-board variant of Chinese Chess.
 * %date-created --> 2020-09-06
-* %date-modified --> 2022-06-07
+* %date-modified --> 2024-06-23
 
 RegexDictionaryReplacement: #.piece-letters
 - queue_position: BEFORE #inline-semantics
@@ -71,13 +71,45 @@ general ({k}ing), chariot ({r}ook), horse (k{n}ight), {c}annon,
 {a}dvisor, elephant ({b}ishop), and {p}awn respectively.
 --
 
+
+##{#fairy-config} Fairy-Stockfish configuration
+
 --
-I have not named this variant "half-board" lest it be confused
+Crowded Xiangqi can be configured in [Fairy-Stockfish]
+by loading a `variants.ini` file containing the following:
+--
+
+``
+# Chinese Chess on half the board (5 × 9 intersections)
+# Pawns (soldiers) are set to display as archbishops (via pieceToCharTable)
+# so that WinBoard/XBoard won't disappear them on the last rank.
+# https://github.com/fairy-stockfish/Fairy-Stockfish/issues/180#issuecomment-692910150
+[crowdedxiangqi:xiangqi]
+pieceToCharTable = .N.R.ABP.K.C...........n.r.abp.k.c..........
+maxRank = 5
+startFen = rnbakabnr/pcp1p1pcp/9/PCP1P1PCP/RNBAKABNR w - - 0 1
+mobilityRegionBlackKing = d3 e3 f3 d4 e4 f4 d5 e5 f5
+mobilityRegionBlackFers = d3 e3 f3 d4 e4 f4 d5 e5 f5
+mobilityRegionBlackElephant = *1 *2 *3 *4 *5
+soldierPromotionRank = 4
+``
+
+--
+See [Variant configuration in the Fairy-Stockfish wiki][variant-config].
+--
+
+
+##{#naming} Naming
+
+--
+I have not named this variant "half-board" due to confusion
 with [Banqi (.半棋)], which is a very different game
 where the pieces are shuffled (so there is a strong element of luck)
 and placed in the squares rather than at the intersections.
 --
 
+[Fairy-Stockfish]: https://github.com/fairy-stockfish/Fairy-Stockfish
+[variant-config]: https://github.com/fairy-stockfish/Fairy-Stockfish/wiki/Variant-configuration
 [Banqi (.半棋)]: https://en.wikipedia.org/wiki/Banqi
 
 
