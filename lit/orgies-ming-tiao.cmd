8714b5efa4fdf0e128f2954b10c8ed145fd76b94
Conway <yawnoc@pm.me>
2025-09-27 00:05:32 +0800

    Write Ming-t'iao lead

---
+++ lit/orgies-ming-tiao.cmd
@@ -0,0 +1,67 @@
+< /resources/rules/yawnoc.cmdr
+< /resources/rules/rendering.cmdr
+< /resources/rules/language.cmdr
+< /resources/rules/translation.cmdr
+< /resources/rules/romanisation.cmdr
+
+OrdinaryDictionaryReplacement: #.boilerplate-properties-override
+- queue_position: AFTER #.yawnoc.properties-override
+- apply_mode: SEQUENTIAL
+* %cite-title --> %title
+* %title --> On John Green's orgies in Ming-t(')iao
+* %description -->
+    Examining John Green's claim that the Shujing references orgies in Ming-t(')iao.
+* %date-created --> 2025-09-27
+* %date-modified --> 2025-09-27
+- concluding_replacements:
+    #.yawnoc.typography
+    #.romanisation.special-characters
+    #backslash-continuations
+
+%%%
+
+
+# On John Green's orgies in (Ming-t(')iao|鳴條)
+
+
+^^^^
+- !home
+- !top
+- !translation
+- !cite
+^^^^
+
+--
+Recently I was re-enjoying John Green's CrashCourse world history.
+--
+--
+At [3:55 of the episode on Chinese history and the Mandate of Heaven][episode],
+he says:
+--
+
+[episode]: https://www.youtube.com/watch?v=ylWORyToTo4&t=3m55s
+
+
+""""
+--
+The (Shujing|書經) is pretty specific
+about what caused the (Xia|夏) kings to lose the Mandate, by the way, explaining,
+"The attack on (Xia|夏) may be traced to the orgies in (Mingtiao|鳴條)."
+--
+--
+Sadly the (Shujing|書經) is woefully short on details of these orgies,
+but orgies are the kind of behavior that is not expected of a ruler,
+and therefore Heaven saw fit to come in, remove the Mandate,
+and allow the (Shang|商) to take power.
+--
+""""
+--
+This got me intrigued: what is the word in the original Chinese
+that has been rendered as "orgies"?
+--
+
+
+%%cite
+
+
+%%footer
